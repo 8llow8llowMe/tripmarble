0ce01c4b835e4aefbc14ac0d6a61cf4bf20416c4
신동근 <tlsehdrms124@naver.com>
2025-05-12 17:58:43 +0900

    [BE] feat: Resource Server에서 사용하는 JWT로부터 사용자 정보를 추출하는 Authentication 개발

---
+++ BackEnd/Module/security-module/src/main/java/com/followfollowme/tripmarble/security/resourceserver/jwt/JwtToMemberConverter.java
@@ -0,0 +1,32 @@
+package com.followfollowme.tripmarble.security.resourceserver.jwt;
+
+import com.followfollowme.tripmarble.security.common.dto.MemberLoginActive;
+import com.followfollowme.tripmarble.security.common.enums.SecurityRole;
+import com.followfollowme.tripmarble.security.common.jwt.JwtAuthentication;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+public class JwtToMemberConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+    
+    @Override
+    public AbstractAuthenticationToken convert(Jwt jwt) {
+        // 클레임 추출
+        long memberId = Long.parseLong(jwt.getSubject());
+        SecurityRole role = SecurityRole.valueOf(jwt.getClaimAsString("role"));
+
+        MemberLoginActive principal = MemberLoginActive.builder()
+            .memberId(memberId)
+            .role(role)
+            .build();
+
+        Collection<? extends GrantedAuthority> authorities =
+            List.of(new SimpleGrantedAuthority(role.name()));
+
+        return new JwtAuthentication(principal, "", authorities);
+    }
+}
