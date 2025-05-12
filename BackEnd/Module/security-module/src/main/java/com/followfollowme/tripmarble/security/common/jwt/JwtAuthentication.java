85cf20cccfedba4561756a1ff503d60a52bff18c
신동근 <tlsehdrms124@naver.com>
2025-05-12 17:55:28 +0900

    [BE] feat: 로그인된 사용자 정보를 담는 커스텀 인증 객체 정의

---
+++ BackEnd/Module/security-module/src/main/java/com/followfollowme/tripmarble/security/common/jwt/JwtAuthentication.java
@@ -0,0 +1,32 @@
+package com.followfollowme.tripmarble.security.common.jwt;
+
+import com.followfollowme.tripmarble.security.common.dto.MemberLoginActive;
+import java.util.Collection;
+import lombok.Getter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+@Getter
+public class JwtAuthentication extends AbstractAuthenticationToken {
+
+    private final MemberLoginActive principal;
+    private final Object credentials;
+
+    public JwtAuthentication(MemberLoginActive principal, Object credentials,
+        Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.principal = principal;
+        this.credentials = credentials;
+        super.setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+}
