4194a87f58d0bc39d1abbe335447a6ed3aece207
신동근 <tlsehdrms124@naver.com>
2025-05-13 15:12:56 +0900

    [BE] fix: 불필요한 파일 삭제 및 파일명 수정

--- BackEnd/Module/common-module/src/main/java/com/followfollowme/tripmarble/common/config/JasyptPropertiesConfig.java
+++ BackEnd/Module/common-module/src/main/java/com/followfollowme/tripmarble/common/config/JasyptPropertiesConfig.java
@@ -6,5 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(JasyptProperties.class)
-public class CommonModuleConfig {
+public class CommonPropertiesConfig {
+
 }
--- BackEnd/Module/security-module/src/main/java/com/followfollowme/tripmarble/security/common/enums/SecurityRole.java
+++
@@ -1,9 +0,0 @@
-package com.followfollowme.tripmarble.security.common.enums;
-
-public enum SecurityRole {
-    USER, MANAGER, ADMIN;
-
-    public static SecurityRole from(String name) {
-        return SecurityRole.valueOf(name.toUpperCase());
-    }
-}
