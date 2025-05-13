444076446dfcf3a338c6f3f5e2563232d22bb2a3
신동근 <tlsehdrms124@naver.com>
2025-05-13 16:31:43 +0900

    [BE] feat: Auth 서비스 내 모듈별 가져올 프로퍼티 설정 Import하는 설정 파일 정의

---
+++ BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/AuthServicePropertiesConfig.java
@@ -0,0 +1,17 @@
+package com.followfollowme.tripmarble.global.config;
+
+import com.followfollowme.tripmarble.common.config.JasyptPropertiesConfig;
+import com.followfollowme.tripmarble.persistence.config.SnowflakePropertiesConfig;
+import com.followfollowme.tripmarble.security.auth.config.JwtAuthPropertiesConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({
+    JasyptPropertiesConfig.class,
+    JwtAuthPropertiesConfig.class,
+    SnowflakePropertiesConfig.class
+})
+public class AuthServicePropertiesConfig {
+
+}
