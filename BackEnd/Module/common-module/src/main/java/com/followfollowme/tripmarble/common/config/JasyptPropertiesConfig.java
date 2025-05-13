239584e09ebb4a62227be6b9c4ff12c5042f295a
신동근 <tlsehdrms124@naver.com>
2025-05-13 15:16:55 +0900

    [BE] Refactor: Jaypt 프로퍼티 설정 파일이란것을 나타내기 위해 명칭 리팩토링

--- BackEnd/Module/common-module/src/main/java/com/followfollowme/tripmarble/common/config/JasyptPropertiesConfig.java
+++ BackEnd/Module/common-module/src/main/java/com/followfollowme/tripmarble/common/config/JasyptPropertiesConfig.java
@@ -6,6 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(JasyptProperties.class)
-public class CommonPropertiesConfig {
+public class JasyptPropertiesConfig {
 
 }
