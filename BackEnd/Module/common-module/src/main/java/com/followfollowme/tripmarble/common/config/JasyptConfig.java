d46869f1198502059cd5aa5300aafd03593dc528
신동근 <tlsehdrms124@naver.com>
2025-05-13 15:16:06 +0900

    [BE] feat: Jasypt 설정 정의

---
+++ BackEnd/Module/common-module/src/main/java/com/followfollowme/tripmarble/common/config/JasyptConfig.java
@@ -0,0 +1,34 @@
+package com.followfollowme.tripmarble.common.config;
+
+import com.followfollowme.tripmarble.common.property.JasyptProperties;
+import lombok.RequiredArgsConstructor;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JasyptConfig {
+
+    private final JasyptProperties jasyptProperties;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(jasyptProperties.key());
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
