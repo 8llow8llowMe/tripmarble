b9d53f59195c7d63315ffcb36fa6642e143f6e14
신동근 <tlsehdrms124@naver.com>
2025-05-16 00:08:40 +0900

    [BE] feat: 나의 회원 정보 가져오기 Response DTO 정의 및 기능, API 개발

--- BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/core/member/application/port/in/MemberUseCase.java
+++ BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/core/member/application/port/in/MemberUseCase.java
@@ -1,8 +1,11 @@
 package com.followfollowme.tripmarble.core.member.application.port.in;
 
+import com.followfollowme.tripmarble.core.member.adapter.in.web.dto.MemberMyInfoResponse;
 import com.followfollowme.tripmarble.core.member.application.command.MemberSignupCommand;
 
 public interface MemberUseCase {
 
     void signupMember(MemberSignupCommand command);
+
+    MemberMyInfoResponse getMyInfoMember(Long memberId);
 }
