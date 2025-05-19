8933f922f90f763f7ff62e664b3caba45a3776b5
신동근 <tlsehdrms124@naver.com>
2025-05-19 11:07:01 +0900

    [BE] feat: Service Discovery 초기 세팅 작업

---
+++ BackEnd/Cloud/service-discovery/src/test/java/com/followfollowme/tripmarble/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.followfollowme.tripmarble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
