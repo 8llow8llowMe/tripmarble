4a0fe6cdf34bb731e66fb50da552ddc00d35876e
신동근 <tlsehdrms124@naver.com>
2025-05-19 16:33:51 +0900

    [BE] feat: OAUTH 2.0 관련 프로퍼티 바인딩 설정 정의

---
+++ BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/OAuthPropertiesConfig.java
@@ -0,0 +1,15 @@
+package com.followfollowme.tripmarble.global.config;
+
+import com.followfollowme.tripmarble.core.auth.adapter.out.persistence.external.oauth.vendor.kakao.properties.KakaoOAuthProperties;
+import com.followfollowme.tripmarble.core.auth.adapter.out.persistence.external.oauth.vendor.naver.NaverOAuthProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties({
+    KakaoOAuthProperties.class,
+    NaverOAuthProperties.class
+})
+public class OAuthPropertiesConfig {
+
+}
