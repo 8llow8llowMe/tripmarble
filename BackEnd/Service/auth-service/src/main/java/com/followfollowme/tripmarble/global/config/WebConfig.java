d80ef2ce3efffa4e09b24f259df2b8ba4c923010
신동근 <tlsehdrms124@naver.com>
2025-05-19 16:37:36 +0900

    [BE] feat: Web MVC 설정 추가 (소셜 로그인 제공업체 관련 컨버터 추가)

---
+++ BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.followfollowme.tripmarble.global.config;
+
+import com.followfollowme.tripmarble.core.auth.adapter.out.persistence.external.oauth.vendor.enums.OAuthProviderConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new OAuthProviderConverter());
+    }
+}
