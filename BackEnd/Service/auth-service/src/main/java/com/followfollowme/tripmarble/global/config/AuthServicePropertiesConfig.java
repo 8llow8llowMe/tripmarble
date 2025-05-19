e8e017800d9860b8ae016cd43819d09144c7f847
신동근 <tlsehdrms124@naver.com>
2025-05-19 09:57:19 +0900

    [BE] feat: 프로퍼티 설정에 Redis 프로터피 설정 Import

--- BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/AuthServicePropertiesConfig.java
+++ BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/AuthServicePropertiesConfig.java
@@ -2,6 +2,7 @@ package com.followfollowme.tripmarble.global.config;
 
 import com.followfollowme.tripmarble.common.config.JasyptPropertiesConfig;
 import com.followfollowme.tripmarble.persistence.config.SnowflakePropertiesConfig;
+import com.followfollowme.tripmarble.redis.config.RedisPropertiesConfig;
 import com.followfollowme.tripmarble.security.auth.config.JwtAuthPropertiesConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Import;
 @Import({
     JasyptPropertiesConfig.class,
     JwtAuthPropertiesConfig.class,
-    SnowflakePropertiesConfig.class
+    SnowflakePropertiesConfig.class,
+    RedisPropertiesConfig.class
 })
 public class AuthServicePropertiesConfig {
 
