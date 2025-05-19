a3710b9a60c30818b9485632c29b02d4acdc1e79
신동근 <tlsehdrms124@naver.com>
2025-05-19 17:56:54 +0900

    [BE] Chore: 클래스명 변경 (오타 수정)

--- BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/core/auth/application/service/CredentialLoginProcessor.java
+++ BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/core/auth/application/service/CredentialLoginProcessor.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CrendentialLoginProcessor {
+public class CredentialLoginProcessor {
 
     private final MemberRepositoryPort memberRepositoryPort;
     private final PasswordEncoder passwordEncoder;
