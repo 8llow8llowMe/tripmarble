7f1f21e42bdd775e6f6ae5547379f89fcbdfa055
신동근 <tlsehdrms124@naver.com>
2025-05-19 17:43:16 +0900

    [BE] feat: 퍼사드 패턴 적용에 따른 자격 증명 관련 일반 로그인 비즈니스 로직을 담은 서비스 클래스 정의

---
+++ BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/core/auth/application/service/CrendentialLoginProcessor.java
@@ -0,0 +1,29 @@
+package com.followfollowme.tripmarble.core.auth.application.service;
+
+import com.followfollowme.tripmarble.core.auth.adapter.in.web.dto.AuthLoginResponse;
+import com.followfollowme.tripmarble.core.auth.application.command.AuthLoginCommand;
+import com.followfollowme.tripmarble.core.member.application.port.out.MemberRepositoryPort;
+import com.followfollowme.tripmarble.core.member.domain.model.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CrendentialLoginProcessor {
+
+    private final MemberRepositoryPort memberRepositoryPort;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    public AuthLoginResponse login(AuthLoginCommand command) {
+        Member member = memberRepositoryPort.findByEmail(command.email())
+            .orElseThrow(() -> new IllegalArgumentException("회원정보가 존재하지 않습니다."));
+
+        if (!passwordEncoder.matches(command.password(), member.password())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return tokenService.issueTokens(member.id(), member.role());
+    }
+}
