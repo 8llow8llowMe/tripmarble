0cc76a3db7c10fe81729f99a12111ad7e4e354b6
신동근 <tlsehdrms124@naver.com>
2025-05-19 14:03:45 +0900

    [BE] feat: jwt 검증 관련 프로퍼티 정의 및 바인딩 설정 정의

---
+++ BackEnd/Cloud/api-gateway/src/main/java/com/followfollowme/tripmarble/apigateway/config/JwtVerificationPropertiesConfig.java
@@ -0,0 +1,11 @@
+package com.followfollowme.tripmarble.apigateway.config;
+
+import com.followfollowme.tripmarble.apigateway.jwt.JwtVerificationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(JwtVerificationProperties.class)
+public class JwtVerificationPropertiesConfig {
+
+}
