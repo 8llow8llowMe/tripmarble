9e8f997da2cd7ced541f0779dd183cd9b0c4c5bf
신동근 <tlsehdrms124@naver.com>
2025-05-19 09:52:37 +0900

    [BE] feat: Redis 프로퍼티 및 프로퍼티 설정 추가

---
+++ BackEnd/Module/redis-module/src/main/java/com/followfollowme/tripmarble/redis/config/RedisPropertiesConfig.java
@@ -0,0 +1,11 @@
+package com.followfollowme.tripmarble.redis.config;
+
+import com.followfollowme.tripmarble.redis.properties.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(RedisProperties.class)
+public class RedisPropertiesConfig {
+
+}
