2c7807a21c334cf1782da01122089c941c1916a0
신동근 <tlsehdrms124@naver.com>
2025-05-19 16:33:25 +0900

    [BE] feat: 카카오, 네이버 소셜 관련 프로퍼티 정의

---
+++ BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/core/auth/adapter/out/persistence/external/oauth/vendor/kakao/properties/KakaoOAuthProperties.java
@@ -0,0 +1,13 @@
+package com.followfollowme.tripmarble.core.auth.adapter.out.persistence.external.oauth.vendor.kakao.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "oauth.kakao")
+public record KakaoOAuthProperties(
+    String redirectUri,
+    String clientId,
+    String clientSecret,
+    String[] scope
+) {
+
+}
---
+++ BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/core/auth/adapter/out/persistence/external/oauth/vendor/naver/NaverOAuthProperties.java
@@ -0,0 +1,13 @@
+package com.followfollowme.tripmarble.core.auth.adapter.out.persistence.external.oauth.vendor.naver;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "oauth.naver")
+public record NaverOAuthProperties(
+    String redirectUri,
+    String clientId,
+    String clientSecret,
+    String state
+) {
+
+}
