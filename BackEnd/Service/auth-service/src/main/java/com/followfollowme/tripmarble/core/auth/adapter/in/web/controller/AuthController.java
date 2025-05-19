edf27947716c878144cd4b28ed974ab12767d08e
신동근 <tlsehdrms124@naver.com>
2025-05-19 17:54:52 +0900

    [BE] feat: 소셜 로그인 관련 Auth Code 발급 및 해당 코드로 소셜 로그인 API 개발

--- BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/core/auth/adapter/in/web/controller/AuthController.java
+++ BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/core/auth/adapter/in/web/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.followfollowme.tripmarble.core.auth.adapter.in.web.controller;
 import com.followfollowme.tripmarble.common.dto.Response;
 import com.followfollowme.tripmarble.core.auth.adapter.in.web.dto.AuthLoginRequest;
 import com.followfollowme.tripmarble.core.auth.adapter.in.web.dto.AuthLoginResponse;
+import com.followfollowme.tripmarble.core.auth.adapter.out.persistence.external.oauth.vendor.enums.OAuthProvider;
 import com.followfollowme.tripmarble.core.auth.application.command.AuthLoginCommand;
 import com.followfollowme.tripmarble.core.auth.application.port.in.AuthUseCase;
 import com.followfollowme.tripmarble.security.common.dto.MemberLoginActive;
@@ -10,9 +11,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,5 +39,19 @@ public class AuthController {
         @AuthenticationPrincipal MemberLoginActive loginActive) {
         authUseCase.logout(loginActive.id());
         return ResponseEntity.ok().body(Response.success());
+    }
+
+    @GetMapping("/{provider}/authorize")
+    public ResponseEntity<Response<String>> generateOAuthAuthorizationUrl(
+        @PathVariable OAuthProvider provider) {
+        String redirectUrl = authUseCase.generateOAuthAuthorizationUrl(provider);
+        return ResponseEntity.ok().body(Response.success(redirectUrl));
+    }
+
+    @PostMapping("/{provider}/login")
+    public ResponseEntity<Response<AuthLoginResponse>> loginWithOAuthCode(
+        @PathVariable OAuthProvider provider, @RequestParam("code") String authCode) {
+        AuthLoginResponse loginResponse = authUseCase.loginWithOAuthCode(provider, authCode);
+        return ResponseEntity.ok().body(Response.success(loginResponse));
     }
 }
