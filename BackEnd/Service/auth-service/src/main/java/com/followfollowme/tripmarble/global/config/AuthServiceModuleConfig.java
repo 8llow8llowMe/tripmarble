3021bde46e9d4f61a56d1ee6a9c5219927ed6452
신동근 <tlsehdrms124@naver.com>
2025-05-19 09:57:45 +0900

    [BE] feat: 모듈 설정에 Redis 설정 Import

--- BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/AuthServiceModuleConfig.java
+++ BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/AuthServiceModuleConfig.java
@@ -3,6 +3,7 @@ package com.followfollowme.tripmarble.global.config;
 import com.followfollowme.tripmarble.common.config.JasyptConfig;
 import com.followfollowme.tripmarble.persistence.config.JpaAuditConfig;
 import com.followfollowme.tripmarble.persistence.config.SnowflakeConfig;
+import com.followfollowme.tripmarble.redis.config.RedisConfig;
 import com.followfollowme.tripmarble.security.auth.config.AuthSecurityConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -12,7 +13,8 @@ import org.springframework.context.annotation.Import;
     JasyptConfig.class,
     JpaAuditConfig.class,
     SnowflakeConfig.class,
-    AuthSecurityConfig.class
+    AuthSecurityConfig.class,
+    RedisConfig.class
 })
 public class AuthServiceModuleConfig {
 
