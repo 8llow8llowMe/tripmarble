1651134cbb08f529b03bd2bbb95acbb98e6cb62a
신동근 <tlsehdrms124@naver.com>
2025-05-20 17:50:32 +0900

    [BE] feat: 인증/인가 관련 API 설명 작성

--- BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/core/auth/adapter/in/web/controller/AuthController.java
+++ BackEnd/Service/auth-service/src/main/java/com/followfollowme/tripmarble/core/auth/adapter/in/web/controller/AuthController.java
@@ -7,6 +7,8 @@ import com.followfollowme.tripmarble.core.auth.adapter.out.persistence.external.
 import com.followfollowme.tripmarble.core.auth.application.command.AuthLoginCommand;
 import com.followfollowme.tripmarble.core.auth.application.port.in.AuthUseCase;
 import com.followfollowme.tripmarble.security.common.dto.MemberLoginActive;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -22,10 +24,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
+@Tag(name = "인증/인가", description = "인증/인가 관련 API 입니다.")
 public class AuthController {
 
     private final AuthUseCase authUseCase;
 
+    @Operation(
+        summary = "일반 로그인",
+        description = "이메일과 비밀번호를 입력하여 로그인을 하는 기능입니다."
+    )
     @PostMapping("/login")
     public ResponseEntity<Response<AuthLoginResponse>> login(
         @RequestBody AuthLoginRequest request) {
@@ -33,6 +40,10 @@ public class AuthController {
         return ResponseEntity.ok().body(Response.success(loginResponse));
     }
 
+    @Operation(
+        summary = "로그아웃",
+        description = "로그인 한 회원을 로그아웃 하는 기능입니다."
+    )
     @PostMapping("/logout")
     @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     public ResponseEntity<Response<Void>> logout(
@@ -41,6 +52,11 @@ public class AuthController {
         return ResponseEntity.ok().body(Response.success());
     }
 
+    @Operation(
+        summary = "소셜 로그인 제공업체를 통한 인증코드 가져오기",
+        description = "소셜 로그인을 하기 전 인증코드를 가져와 "
+            + "소셜 제공업체(EX. 카카오, 네이버 등)에 가입된 회원정보를 가져오는 기능입니다."
+    )
     @GetMapping("/{provider}/authorize")
     public ResponseEntity<Response<String>> generateOAuthAuthorizationUrl(
         @PathVariable OAuthProvider provider) {
@@ -48,6 +64,11 @@ public class AuthController {
         return ResponseEntity.ok().body(Response.success(redirectUrl));
     }
 
+    @Operation(
+        summary = "소셜 로그인",
+        description = "인증코드를 통해 소셜 로그인을 하는 기능입니다. "
+            + "해당 서비스에 회원 정보가 없는 경우 회원가입 후 로그인을 하는 기능입니다."
+    )
     @PostMapping("/{provider}/login")
     public ResponseEntity<Response<AuthLoginResponse>> loginWithOAuthCode(
         @PathVariable OAuthProvider provider, @RequestParam("code") String authCode) {
