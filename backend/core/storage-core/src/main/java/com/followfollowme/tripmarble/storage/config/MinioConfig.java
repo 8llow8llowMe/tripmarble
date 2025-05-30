597a5da8bd636b9d2a44daacd6e025b5f717ed4d
신동근 <tlsehdrms124@naver.com>
2025-05-30 17:30:06 +0900

    [BE] feat: MinIO 설정 클래스 정의

---
+++ backend/core/storage-core/src/main/java/com/followfollowme/tripmarble/storage/config/MinioConfig.java
@@ -0,0 +1,22 @@
+package com.followfollowme.tripmarble.storage.config;
+
+import com.followfollowme.tripmarble.storage.properties.MinioProperties;
+import io.minio.MinioClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class MinioConfig {
+
+    private final MinioProperties minioProperties;
+    
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+            .endpoint(minioProperties.url())
+            .credentials(minioProperties.accessKey(), minioProperties.secretKey())
+            .build();
+    }
+}
