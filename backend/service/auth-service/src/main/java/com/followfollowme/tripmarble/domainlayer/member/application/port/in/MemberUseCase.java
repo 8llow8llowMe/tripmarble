113d53b2d78e458c02e3fbe477a91351b5d11496
신동근 <tlsehdrms124@naver.com>
2025-06-02 11:33:20 +0900

    [BE] feat: 회원 관련 비즈니스 로직 프로필 이미지 업로드 추가 (인터페이스)

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/port/in/MemberUseCase.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/port/in/MemberUseCase.java
@@ -1,11 +1,17 @@
 package com.followfollowme.tripmarble.domainlayer.member.application.port.in;
 
 import com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.dto.MemberMyInfoResponse;
+import com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.dto.MemberProfileUploadResponse;
 import com.followfollowme.tripmarble.domainlayer.member.application.command.MemberSignupCommand;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface MemberUseCase {
 
     void signup(MemberSignupCommand command);
 
-    MemberMyInfoResponse getMyInfo(Long memberId);
+    MemberMyInfoResponse getMyInfo(long memberId);
+
+    MemberProfileUploadResponse uploadProfileImage(long memberId, MultipartFile imageFile);
+
+
 }
