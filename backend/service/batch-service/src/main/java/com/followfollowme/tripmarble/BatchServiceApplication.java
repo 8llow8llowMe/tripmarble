d94dccac66c9e63e0d8268e8c3d0831f5f63f63a
신동근 <tlsehdrms124@naver.com>
2025-06-02 15:02:55 +0900

    [BE] feat: 데이터 취합 관련인 batch service 초기 세팅 및 의존성 주입

---
+++ backend/service/batch-service/src/main/java/com/followfollowme/tripmarble/BatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.followfollowme.tripmarble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchServiceApplication.class, args);
+	}
+
+}
