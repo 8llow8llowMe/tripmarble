f15e47d620dbb76034f1cc21f35956545c1e29c0
신동근 <tlsehdrms124@naver.com>
2025-06-02 17:45:58 +0900

    [BE] feat: Tour API 등록 관련 프로퍼티 추가 정의

--- backend/service/batch-service/src/main/java/com/followfollowme/tripmarble/global/infra/tourapi/properties/TourApiRegistryProperties.java
+++ backend/service/batch-service/src/main/java/com/followfollowme/tripmarble/global/infra/tourapi/properties/TourApiRegistryProperties.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "tourapi")
 public record TourApiRegistryProperties(
     String serviceKey, // 공통 서비스 키
+    Map<String, String> defaultParams,
     Map<String, TourApiCategory> categories
 ) {
 
@@ -16,7 +17,8 @@ public record TourApiRegistryProperties(
     }
 
     public record TourApiSpec(
-        String url
+        String url,
+        Map<String, String> params // API별 개별 쿼리 파라미터
     ) {
 
     }
