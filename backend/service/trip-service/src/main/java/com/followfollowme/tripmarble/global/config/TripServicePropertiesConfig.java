5a3d466076a371b1777019b3c0c1f18fbad4f94f
신동근 <tlsehdrms124@naver.com>
2025-06-02 15:34:57 +0900

    [BE] feat: trip service 외부 설정 가져오기 및 프로퍼티 바인딩 설정 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/config/TripServicePropertiesConfig.java
@@ -0,0 +1,13 @@
+package com.followfollowme.tripmarble.global.config;
+
+import com.followfollowme.tripmarble.common.config.JasyptPropertiesConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({
+    JasyptPropertiesConfig.class
+})
+public class TripServicePropertiesConfig {
+
+}
