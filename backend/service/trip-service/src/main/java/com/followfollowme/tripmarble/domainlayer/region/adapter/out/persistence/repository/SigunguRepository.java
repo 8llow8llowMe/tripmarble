ea1116970a3406d1cfcaf517906184ad39082120
신동근 <tlsehdrms124@naver.com>
2025-06-02 15:26:46 +0900

    [BE] feat: 지역, 시군구 관련 레포지토리 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/out/persistence/repository/SigunguRepository.java
@@ -0,0 +1,9 @@
+package com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.repository;
+
+import com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.entity.SigunguEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SigunguRepository extends JpaRepository<SigunguEntity, Long> {
+
+
+}
