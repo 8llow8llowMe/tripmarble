03dfc6b6d094adfa4eeb2ad751262695a3ce6793
신동근 <tlsehdrms124@naver.com>
2025-06-02 11:34:16 +0900

    [BE] feat: 임시 프로필 이미지 업로드 API 개발

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/in/web/controller/MemberController.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/in/web/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.controll
 
 import com.followfollowme.tripmarble.common.dto.Response;
 import com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.dto.MemberMyInfoResponse;
+import com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.dto.MemberProfileUploadResponse;
 import com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.dto.MemberSignupRequest;
 import com.followfollowme.tripmarble.domainlayer.member.application.command.MemberSignupCommand;
 import com.followfollowme.tripmarble.domainlayer.member.application.port.in.MemberUseCase;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,7 +19,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -49,4 +53,19 @@ public class MemberController {
         MemberMyInfoResponse myInfoResponse = memberUseCase.getMyInfo(loginActive.id());
         return ResponseEntity.ok().body(Response.success(myInfoResponse));
     }
+
+    @Operation(
+        summary = "임시 프로필 이미지 업로드",
+        description = "로그인한 사용자가 MinIO에 프로필 이미지를 업로드하고, 이미지 URL을 반환 받는 기능입니다."
+    )
+    @PostMapping(value = "/profile-image/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Response<MemberProfileUploadResponse>> uploadProfileImage(
+        @AuthenticationPrincipal MemberLoginActive loginActive,
+        @RequestPart MultipartFile imageFile) {
+        MemberProfileUploadResponse profileUploadResponse = memberUseCase.uploadProfileImage(
+            loginActive.id(), imageFile);
+        return ResponseEntity.ok().body(Response.success(profileUploadResponse));
+    }
+
 }
