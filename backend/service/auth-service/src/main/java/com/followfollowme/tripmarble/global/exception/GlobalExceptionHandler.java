651aa2f4178212fee76a859e4fbf5e57100d404e
신동근 <tlsehdrms124@naver.com>
2025-06-04 17:49:46 +0900

    [BE] feat: Global Exception 핸들러 정의

---
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.followfollowme.tripmarble.global.exception;
+
+import com.followfollowme.tripmarble.common.dto.Response;
+import com.followfollowme.tripmarble.domainlayer.member.application.exception.MemberException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MemberException.class)
+    public ResponseEntity<Response<Void>> memberException(MemberException e) {
+        log.error("회원 관련 오류: {}", e.getMessage());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+            .body(Response.fail(e.getErrorCode().getCode(), e.getErrorCode().getErrorMessage()));
+    }
+
+}
