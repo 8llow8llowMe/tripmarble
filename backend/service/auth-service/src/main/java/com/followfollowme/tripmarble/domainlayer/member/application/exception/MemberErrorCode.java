c6270ba3da0be973ca67e7ea2353641d62aa3670
신동근 <tlsehdrms124@naver.com>
2025-06-04 17:48:26 +0900

    [BE] feat: 회원 관련 커스텀 에러코드 정의

---
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/exception/MemberErrorCode.java
@@ -0,0 +1,18 @@
+package com.followfollowme.tripmarble.domainlayer.member.application.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberErrorCode {
+
+    EXIST_MEMBER_EMAIL("M001", "이미 가입된 이메일입니다.", HttpStatus.CONFLICT),
+    NOT_FOUND_MEMBER("M002", "존재하지 않는 회원입니다", HttpStatus.NOT_FOUND),
+    NOT_MATCH_PASSWORD("M003", "비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+
+    private final String code;
+    private final String errorMessage;
+    private final HttpStatus httpStatus;
+}
