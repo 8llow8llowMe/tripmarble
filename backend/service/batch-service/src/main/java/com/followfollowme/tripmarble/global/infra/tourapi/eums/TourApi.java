37b91e3d987414fa187d398c2429b0a231121444
신동근 <tlsehdrms124@naver.com>
2025-06-11 00:19:33 +0900

    [BE] feat: Tour API 관련 ENUM 정의

---
+++ backend/service/batch-service/src/main/java/com/followfollowme/tripmarble/global/infra/tourapi/eums/TourApi.java
@@ -0,0 +1,16 @@
+package com.followfollowme.tripmarble.global.infra.tourapi.eums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum TourApi {
+
+    REGION_CODE("ldong-code2", "region-code", "법정동 코드 조회 [지역코드(시도코드) 조회]"),
+    SIGUNGU_CODE("ldong-code2", "sigungu-code", "법정동 코드 조회 [시군구코드 조회]");
+
+    private final String category;
+    private final String name;
+    private final String description;
+}
