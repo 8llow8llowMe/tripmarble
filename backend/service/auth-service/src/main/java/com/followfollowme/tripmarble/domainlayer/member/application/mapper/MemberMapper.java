dd0b5bf1f4038089067687aef71c9403d09396be
신동근 <tlsehdrms124@naver.com>
2025-06-12 00:14:41 +0900

    [BE] feat: 회원 관련 Mapper 정의

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/mapper/MemberMapper.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/mapper/MemberMapper.java
@@ -1,5 +1,19 @@
 package com.followfollowme.tripmarble.domainlayer.member.application.mapper;
 
+import com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.dto.MemberMyInfoResponse;
+import com.followfollowme.tripmarble.domainlayer.member.adapter.out.persistence.entity.MemberEntity;
+import com.followfollowme.tripmarble.domainlayer.member.domain.model.Member;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface MemberMapper {
 
+    // 엔티티 -> 도메인
+    Member toDomainFromEntity(MemberEntity memberEntity);
+
+    // 도메인 -> 엔티티
+    MemberEntity toEntityFromDomain(Member domainMember);
+
+    // 도메인 -> DTO
+    MemberMyInfoResponse toMyInfoResponseFromDomain(Member domainMember);
 }
