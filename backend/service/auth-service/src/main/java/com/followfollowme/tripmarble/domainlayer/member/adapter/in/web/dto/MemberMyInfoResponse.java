f7902040df647a278b7d57c17b1a798bc04f7798
신동근 <tlsehdrms124@naver.com>
2025-06-12 00:15:23 +0900

    [BE] fix: Mapper 적용으로 인한 from static 메서드 삭제

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/in/web/dto/MemberMyInfoResponse.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/in/web/dto/MemberMyInfoResponse.java
@@ -1,7 +1,6 @@
 package com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.dto;
 
 import com.followfollowme.tripmarble.domainlayer.auth.adapter.out.persistence.external.oauth.vendor.enums.OAuthProvider;
-import com.followfollowme.tripmarble.domainlayer.member.domain.model.Member;
 import com.followfollowme.tripmarble.security.common.enums.SecurityRole;
 import lombok.Builder;
 
@@ -16,15 +15,4 @@ public record MemberMyInfoResponse(
     OAuthProvider provider
 ) {
 
-    public static MemberMyInfoResponse from(Member member) {
-        return MemberMyInfoResponse.builder()
-            .id(member.id())
-            .email(member.email())
-            .name(member.name())
-            .nickname(member.nickname())
-            .profileImage(member.profileImage())
-            .role(member.role())
-            .provider(member.provider())
-            .build();
-    }
 }
