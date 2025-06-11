40b81edd237f7799e375fbb7d0f4c5a373b4de77
신동근 <tlsehdrms124@naver.com>
2025-06-11 23:29:08 +0900

    [BE] feat: 대표 여행지 불러오기 API 개발

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/in/web/controller/RepresentativeRegionWebController.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/in/web/controller/RepresentativeRegionWebController.java
@@ -1,5 +1,26 @@
 package com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.controller;
 
+import com.followfollowme.tripmarble.common.dto.Response;
+import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RepresentativeRegionResponse;
+import com.followfollowme.tripmarble.domainlayer.region.application.port.in.RepresentativeRegionWebUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/regions/representative")
 public class RepresentativeRegionWebController {
 
+    private final RepresentativeRegionWebUseCase representativeRegionWebUseCase;
+
+    @GetMapping
+    public ResponseEntity<Response<List<RepresentativeRegionResponse>>> getAllRepresentativeRegions() {
+        List<RepresentativeRegionResponse> representativeRegionResponses = representativeRegionWebUseCase.getAllRepresentativeRegions();
+        return ResponseEntity.ok().body(Response.success(representativeRegionResponses));
+    }
 }
