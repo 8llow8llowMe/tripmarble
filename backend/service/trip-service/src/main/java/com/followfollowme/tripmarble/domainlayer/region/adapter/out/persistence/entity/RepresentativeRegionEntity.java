0a2a4002489d4a48b965f04d03685a933212ed46
신동근 <tlsehdrms124@naver.com>
2025-06-11 23:28:37 +0900

    [BE] feat: 대표 여행지 엔티티 정의

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/out/persistence/entity/RepresentativeRegionEntity.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/out/persistence/entity/RepresentativeRegionEntity.java
@@ -33,17 +33,17 @@ public class RepresentativeRegionEntity {
     @Column(nullable = false, length = 30)
     @Comment("대표 여행지 이름 (ex. 서울, 부산)")
     private String name;
-    
+
     @Comment("썸네일 이미지 URL")
     private String imageUrl;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "region_id")
-    @Comment("시도 연관 (nullable 가능)")
+    @Comment("시도 연관 외래키 (nullable 가능)")
     private RegionEntity region;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sigungu_id")
-    @Comment("시군구 연관 (nullable 가능)")
+    @Comment("시군구 연관 외래키 (nullable 가능)")
     private SigunguEntity sigungu;
 }
