8fa8c028fa03df8ac6b321d6b1119985766ac634
신동근 <tlsehdrms124@naver.com>
2025-06-11 17:46:27 +0900

    [BE] feat: 대표 여행지 관련 JPA Repository 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/out/persistence/repository/RepresentativeRegionRepository.java
@@ -0,0 +1,9 @@
+package com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.repository;
+
+import com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.entity.RepresentativeRegionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepresentativeRegionRepository extends
+    JpaRepository<RepresentativeRegionEntity, Long> {
+
+}
