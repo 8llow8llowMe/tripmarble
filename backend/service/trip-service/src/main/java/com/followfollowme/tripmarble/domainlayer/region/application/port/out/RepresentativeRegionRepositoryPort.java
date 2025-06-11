3a32177a4d50120c8043f3ca7e1999633b4efd57
신동근 <tlsehdrms124@naver.com>
2025-06-11 17:57:03 +0900

    [BE] feat: 대표 여행지 전체 조회 관련 기능 정의 (port)

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/out/RepresentativeRegionRepositoryPort.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/out/RepresentativeRegionRepositoryPort.java
@@ -1,5 +1,9 @@
 package com.followfollowme.tripmarble.domainlayer.region.application.port.out;
 
+import com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.entity.RepresentativeRegionEntity;
+import java.util.List;
+
 public interface RepresentativeRegionRepositoryPort {
 
+    List<RepresentativeRegionEntity> findAll();
 }
