f6cfe8600ad12c3073da1f387864412671e3878f
신동근 <tlsehdrms124@naver.com>
2025-06-11 23:28:56 +0900

    [BE] feat: 대표 여행지 불러오기 관련 비즈니스 로직 개발

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/service/RepresentativeRegionFacade.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/service/RepresentativeRegionFacade.java
@@ -1,5 +1,29 @@
 package com.followfollowme.tripmarble.domainlayer.region.application.service;
 
-public class RepresentativeRegionFacade {
+import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RepresentativeRegionResponse;
+import com.followfollowme.tripmarble.domainlayer.region.application.port.in.RepresentativeRegionWebUseCase;
+import com.followfollowme.tripmarble.domainlayer.region.application.port.out.RepresentativeRegionRepositoryPort;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RepresentativeRegionFacade implements RepresentativeRegionWebUseCase {
+
+    private final RepresentativeRegionRepositoryPort representativeRegionRepositoryPort;
+
+    @Override
+    public List<RepresentativeRegionResponse> getAllRepresentativeRegions() {
+        return representativeRegionRepositoryPort.findAll().stream()
+            .map(representativeRegion -> RepresentativeRegionResponse.builder()
+                .id(representativeRegion.regionId())
+                .name(representativeRegion.name())
+                .imageUrl(representativeRegion.imageUrl())
+                .build())
+            .toList();
+    }
 }
