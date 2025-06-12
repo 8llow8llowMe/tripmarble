606a385c56c3290d701f3a80357f61af98547346
신동근 <tlsehdrms124@naver.com>
2025-06-12 17:58:36 +0900

    [BE] feat: 여행 콘텐츠 타입 초기 스켈레톤 코드 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/out/persistence/repository/TripContentTypeRepository.java
@@ -0,0 +1,8 @@
+package com.followfollowme.tripmarble.domainlayer.trip.adapter.out.persistence.repository;
+
+import com.followfollowme.tripmarble.domainlayer.trip.adapter.out.persistence.entity.TripContentTypeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TripContentTypeRepository extends JpaRepository<TripContentTypeEntity, Long> {
+
+}
