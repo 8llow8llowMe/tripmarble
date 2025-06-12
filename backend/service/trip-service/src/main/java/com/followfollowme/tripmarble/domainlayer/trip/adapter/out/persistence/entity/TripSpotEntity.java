96590027ac4966fb17cac30bcdd49be7f64cc0fe
신동근 <tlsehdrms124@naver.com>
2025-06-12 17:34:17 +0900

    [BE] feat: 여행지 정보 엔티티 정의

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/out/persistence/entity/TripSpotEntity.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/out/persistence/entity/TripSpotEntity.java
@@ -54,30 +54,24 @@ public class TripSpotEntity {
     private String addr2;
 
     @Comment("X좌표 (경도)")
-    @Column(name = "map_x")
     private String mapX;
 
     @Comment("Y좌표 (위도)")
-    @Column(name = "map_y")
     private String mapY;
 
     @Comment("지도 레벨")
     private String mlevel;
 
     @Comment("지역 코드")
-    @Column(name = "area_code")
     private String areaCode;
 
     @Comment("시군구 코드")
-    @Column(name = "sigungu_code")
     private String sigunguCode;
 
     @Comment("법정동 시도 코드")
-    @Column(name = "ldong_regn_cd")
     private String ldongRegnCd;
 
     @Comment("법정동 시군구 코드")
-    @Column(name = "ldong_signgu_cd")
     private String ldongSignguCd;
 
     @Comment("서비스 분류 코드 대분류")
@@ -90,26 +84,20 @@ public class TripSpotEntity {
     private String cat3;
 
     @Comment("분류체계 1Depth")
-    @Column(name = "lcls_systm1")
     private String lclsSystm1;
 
     @Comment("분류체계 2Depth")
-    @Column(name = "lcls_systm2")
     private String lclsSystm2;
 
     @Comment("분류체계 3Depth")
-    @Column(name = "lcls_systm3")
     private String lclsSystm3;
 
     @Comment("대표 이미지 원본")
-    @Column(name = "first_image")
     private String firstImage;
 
     @Comment("대표 이미지 썸네일")
-    @Column(name = "first_image2")
     private String firstImage2;
 
     @Comment("저작권 유형")
-    @Column(name = "cpyrht_div_cd")
     private String cpyrhtDivCd;
 }
