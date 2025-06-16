101fe82fc2396b52273c78dee45163ea155c9aa7
신동근 <tlsehdrms124@naver.com>
2025-06-16 17:37:08 +0900

    [BE] feat: 여행 관광지 정보 Adapter 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/out/persistence/TripSpotRepositoryAdapter.java
@@ -0,0 +1,10 @@
+package com.followfollowme.tripmarble.domainlayer.trip.adapter.out.persistence;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TripSpotRepositoryAdapter {
+
+}
