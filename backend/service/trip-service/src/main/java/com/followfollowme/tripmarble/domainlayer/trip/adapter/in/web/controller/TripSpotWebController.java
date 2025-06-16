4d7ecae726011acb9531e5bf3c2cf2df85fa7a3d
신동근 <tlsehdrms124@naver.com>
2025-06-16 17:32:31 +0900

    [BE] Chore: 해당 컨트롤러 명칭 변경 (클래스명 변경)

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/in/web/controller/TripSpotWebController.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/in/web/controller/TripSpotWebController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/trip-spots")
