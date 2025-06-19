91b13ba01cd1d8e1c25ee7161d0aecdcee324aab
신동근 <tlsehdrms124@naver.com>
2025-06-19 17:57:50 +0900

    [BE] fix: CQRS 패턴 적용 추후에 하는걸로 변경 (일단 비즈니스 로직부터 개발하는걸로)

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/out/persistence/repository/MemberCommandRepository.java
+++
@@ -1,8 +0,0 @@
-package com.followfollowme.tripmarble.domainlayer.member.adapter.out.persistence.repository;
-
-import com.followfollowme.tripmarble.domainlayer.member.adapter.out.persistence.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberCommandRepository extends JpaRepository<MemberEntity, Long> {
-
-}
