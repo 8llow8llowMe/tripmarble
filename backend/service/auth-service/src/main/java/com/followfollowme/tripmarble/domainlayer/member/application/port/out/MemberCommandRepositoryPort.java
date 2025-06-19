5dcca0229e71077a8c6d1abfbe34fb7015c7c88e
신동근 <tlsehdrms124@naver.com>
2025-06-19 17:57:50 +0900

    [BE] fix: CQRS 패턴 적용 추후에 하는걸로 변경 (일단 비즈니스 로직부터 개발하는걸로)

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/port/out/MemberCommandRepositoryPort.java
+++
@@ -1,8 +0,0 @@
-package com.followfollowme.tripmarble.domainlayer.member.application.port.out;
-
-import com.followfollowme.tripmarble.domainlayer.member.domain.model.Member;
-
-public interface MemberCommandRepositoryPort {
-
-    Member save(Member domainMember);
-}
