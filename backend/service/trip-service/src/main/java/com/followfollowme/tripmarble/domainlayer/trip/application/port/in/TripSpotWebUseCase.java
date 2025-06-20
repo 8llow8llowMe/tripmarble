776ef922249d753993df9dd2d1cd02e7913bec70
신동근 <tlsehdrms124@naver.com>
2025-06-20 18:46:36 +0900

    [BE] feat: 여행지 관련 비즈니스 로직 UseCase 정의

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/port/in/TripSpotWebUseCase.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/port/in/TripSpotWebUseCase.java
@@ -1,5 +1,8 @@
 package com.followfollowme.tripmarble.domainlayer.trip.application.port.in;
 
+import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto.TripSpotWithDetailViewResponse;
+
 public interface TripSpotWebUseCase {
 
+    TripSpotWithDetailViewResponse getTripSpotWithDetail(long tripSpotId);
 }
