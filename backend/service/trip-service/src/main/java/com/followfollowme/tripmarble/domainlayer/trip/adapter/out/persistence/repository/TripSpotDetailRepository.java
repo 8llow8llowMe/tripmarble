72f134b847144b315b9e02198f648a578ab5d7e7
신동근 <tlsehdrms124@naver.com>
2025-06-20 18:38:08 +0900

    [BE] feat: 여행지 상세 정보 관련 레포지토리 정의

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/out/persistence/repository/TripSpotDetailRepository.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/out/persistence/repository/TripSpotDetailRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TripSpotDetailRepository extends JpaRepository<TripSpotDetailEntity, Long> {
 
-    Optional<TripSpotDetailEntity> findByContentId(String contentId);
+    Optional<TripSpotDetailEntity> findByContentId(int contentId);
 }
