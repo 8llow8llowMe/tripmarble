cb76ba9e397798de01ace24a67e2056d6b9c485f
신동근 <tlsehdrms124@naver.com>
2025-06-20 18:47:00 +0900

    [BE] feat: 여행지 관련 UseCase 구현체 Facade 패턴 적용 및 여행지 상세 정보 조회 기능 개발

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/service/TripSpotFacade.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/service/TripSpotFacade.java
@@ -1,14 +1,36 @@
 package com.followfollowme.tripmarble.domainlayer.trip.application.service;
 
+import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto.TripSpotWithDetailViewResponse;
 import com.followfollowme.tripmarble.domainlayer.trip.application.port.in.TripSpotInternalUseCase;
 import com.followfollowme.tripmarble.domainlayer.trip.application.port.in.TripSpotWebUseCase;
+import com.followfollowme.tripmarble.domainlayer.trip.application.port.out.TripSpotDetailRepositoryPort;
+import com.followfollowme.tripmarble.domainlayer.trip.application.port.out.TripSpotRepositoryPort;
+import com.followfollowme.tripmarble.domainlayer.trip.domain.model.TripSpot;
+import com.followfollowme.tripmarble.domainlayer.trip.domain.model.TripSpotDetail;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class TripSpotFacade implements TripSpotWebUseCase, TripSpotInternalUseCase {
 
+    private final TripSpotRepositoryPort tripSpotRepositoryPort;
+    private final TripSpotDetailRepositoryPort tripSpotDetailRepositoryPort;
+
+    @Override
+    @Transactional(readOnly = true)
+    public TripSpotWithDetailViewResponse getTripSpotWithDetail(long tripSpotId) {
+        TripSpot tripSpot = tripSpotRepositoryPort.findById(tripSpotId)
+            .orElseThrow(() -> new IllegalArgumentException("해당 여행지 정보를 찾을 수 없습니다: " + tripSpotId));
+
+        TripSpotDetail tripSpotDetail = tripSpotDetailRepositoryPort.findByContentId(
+                tripSpot.contentId())
+            .orElseThrow(() -> new IllegalArgumentException(
+                "해당 여행지 정보를 찾을 수 없습니다: " + tripSpot.contentId()));
+
+        // TODO: 여행지 상세 정보 Response DTO 정의
+        return TripSpotWithDetailViewResponse.builder()
+            .build();
+    }
 }
