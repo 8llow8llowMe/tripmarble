9e28d91d496894fa3965427727a76aede1032d60
신동근 <tlsehdrms124@naver.com>
2025-06-22 14:55:36 +0900

    [BE] feat: 지역 관련 (시도, 시군구) Repository 정의 및 해당 시도에 따른 시군구 목록 조회 기능 개발

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/out/persistence/repository/SigunguRepository.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/out/persistence/repository/SigunguRepository.java
@@ -3,7 +3,9 @@ package com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence
 import com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.entity.SigunguEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SigunguRepository extends JpaRepository<SigunguEntity, Long> {
 
-
+    List<SigunguEntity> findAllByRegionId(long regionId);
 }
