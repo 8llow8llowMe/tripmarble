207d7898403ceed3a2e114cf5953c621fbc8229d
신동근 <tlsehdrms124@naver.com>
2025-06-23 00:13:24 +0900

    [BE] feat: 대표 여행지_시군구 매핑 관련 대표 여행지 ID를 통한 시군구 ID 목록 조회 기능 개발

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/out/RepresentativeRegionRepositoryPort.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/out/RepresentativeRegionRepositoryPort.java
@@ -3,11 +3,8 @@ package com.followfollowme.tripmarble.domainlayer.region.application.port.out;
 import com.followfollowme.tripmarble.domainlayer.region.domain.model.RepresentativeRegion;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface RepresentativeRegionRepositoryPort {
 
     List<RepresentativeRegion> findAll();
-
-    Optional<RepresentativeRegion> findById(long representativeRegionId);
 }
