42b9d65f7ff8b2d4c96169de55b18e0fd1137a30
신동근 <tlsehdrms124@naver.com>
2025-06-23 00:12:12 +0900

    [BE] feat: 대표 여행지_시군구 매핑 엔티티 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/out/persistence/entity/RepresentativeRegionSigunguMappingEntity.java
@@ -0,0 +1,40 @@
+package com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "representative_region_sigungu_mapping")
+public class RepresentativeRegionSigunguMappingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("대표 여행지_시군구_매핑 ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "representative_region_id", nullable = false)
+    @Comment("대표 여행지 ID")
+    private RepresentativeRegionEntity representativeRegion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sigungu_id", nullable = false)
+    @Comment("시군구 ID")
+    private SigunguEntity sigungu;
+}
