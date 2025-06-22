6143eae9a49095183cae7982f27a567cf66269bf
신동근 <tlsehdrms124@naver.com>
2025-06-22 15:30:04 +0900

    [BE] feat: 대표 여행지 관련 MapStruct Mapper 정의

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/mapper/RepresentativeRegionMapper.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/mapper/RepresentativeRegionMapper.java
@@ -3,17 +3,19 @@ package com.followfollowme.tripmarble.domainlayer.region.application.mapper;
 import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RepresentativeRegionResponse;
 import com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.entity.RepresentativeRegionEntity;
 import com.followfollowme.tripmarble.domainlayer.region.domain.model.RepresentativeRegion;
-import java.util.List;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RepresentativeRegionMapper {
 
     // 엔티티 리스트 -> 도메인 리스트
-    List<RepresentativeRegion> toDomainListFromEntityList(
-        List<RepresentativeRegionEntity> entities);
+    List<RepresentativeRegion> toDomainListFromEntityList(List<RepresentativeRegionEntity> entities);
 
     // 도메인 리스트 -> DTO 리스트
-    List<RepresentativeRegionResponse> toResponseListFromDomainList(
-        List<RepresentativeRegion> domains);
+    List<RepresentativeRegionResponse> toResponseListFromDomainList(List<RepresentativeRegion> domains);
+
+    // 엔티티 -> 도메인
+    RepresentativeRegion toDomainFromEntity(RepresentativeRegionEntity entity);
 }
