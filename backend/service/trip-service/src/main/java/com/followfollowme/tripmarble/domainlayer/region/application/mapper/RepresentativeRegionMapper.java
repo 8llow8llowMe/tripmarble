7011203dac8cf738a223b22f45277a4f8a4ceabf
신동근 <tlsehdrms124@naver.com>
2025-06-22 15:31:42 +0900

    [BE] feat: 대표 여행지 관련 Mapstruct Mapper 재정의 (필드 관련)

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/mapper/RepresentativeRegionMapper.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/mapper/RepresentativeRegionMapper.java
@@ -4,18 +4,26 @@ import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.Repre
 import com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.entity.RepresentativeRegionEntity;
 import com.followfollowme.tripmarble.domainlayer.region.domain.model.RepresentativeRegion;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RepresentativeRegionMapper {
 
+    // 엔티티 -> 도메인
+    @Mapping(source = "region.id", target = "regionId")
+    @Mapping(source = "sigungu.id", target = "sigunguId")
+    RepresentativeRegion toDomainFromEntity(RepresentativeRegionEntity entity);
+
     // 엔티티 리스트 -> 도메인 리스트
     List<RepresentativeRegion> toDomainListFromEntityList(List<RepresentativeRegionEntity> entities);
 
+    // 도메인 -> DTO
+    @Mapping(source = "id", target = "representativeRegionId")
+    @Mapping(source = "name", target = "representativeRegionName")
+    RepresentativeRegionResponse toResponseFromDomain(RepresentativeRegion domain);
+
     // 도메인 리스트 -> DTO 리스트
     List<RepresentativeRegionResponse> toResponseListFromDomainList(List<RepresentativeRegion> domains);
-
-    // 엔티티 -> 도메인
-    RepresentativeRegion toDomainFromEntity(RepresentativeRegionEntity entity);
 }
