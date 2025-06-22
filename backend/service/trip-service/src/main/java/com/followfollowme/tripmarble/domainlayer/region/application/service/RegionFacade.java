643fbccac951670b6d220a9e30c6b02e26a7586b
신동근 <tlsehdrms124@naver.com>
2025-06-22 14:57:17 +0900

    [BE] feat: 지역 관련 (시도, 시군구) UseCase 정의 및 Facade 패턴 적용하여 서비스단 구현

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/service/RegionFacade.java
@@ -0,0 +1,39 @@
+package com.followfollowme.tripmarble.domainlayer.region.application.service;
+
+import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RegionResponse;
+import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.SigunguResponse;
+import com.followfollowme.tripmarble.domainlayer.region.application.mapper.RegionMapper;
+import com.followfollowme.tripmarble.domainlayer.region.application.mapper.SigunguMapper;
+import com.followfollowme.tripmarble.domainlayer.region.application.port.in.RegionWebUseCase;
+import com.followfollowme.tripmarble.domainlayer.region.application.port.out.RegionRepositoryPort;
+import com.followfollowme.tripmarble.domainlayer.region.application.port.out.SigunguRepositoryPort;
+import com.followfollowme.tripmarble.domainlayer.region.domain.model.Region;
+import com.followfollowme.tripmarble.domainlayer.region.domain.model.Sigungu;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RegionFacade implements RegionWebUseCase {
+
+    private final RegionRepositoryPort regionRepositoryPort;
+    private final SigunguRepositoryPort sigunguRepositoryPort;
+    private final RegionMapper regionMapper;
+    private final SigunguMapper sigunguMapper;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<RegionResponse> getAllRegions() {
+        List<Region> regions = regionRepositoryPort.findAll();
+        return regionMapper.toResponseListFromDomainList(regions);
+    }
+
+    @Override
+    public List<SigunguResponse> getSigungusByRegionId(long regionId) {
+        List<Sigungu> sigungus = sigunguRepositoryPort.findAllByRegionId(regionId);
+        return sigunguMapper.toResponseListFromDomainList(sigungus);
+    }
+}
