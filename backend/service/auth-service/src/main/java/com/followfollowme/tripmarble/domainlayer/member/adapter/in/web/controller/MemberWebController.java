a64439b4ac1328d061d5c89cff224db508c7a6d1
신동근 <tlsehdrms124@naver.com>
2025-06-22 14:51:53 +0900

    [BE] chore: API 설명 수정

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/in/web/controller/MemberWebController.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/in/web/controller/MemberWebController.java
@@ -43,8 +43,8 @@ public class MemberWebController {
     }
 
     @Operation(
-        summary = "나의 회원정보 가져오기",
-        description = "로그인한 나의 회원정보를 가져오는 기능입니다."
+        summary = "나의 회원정보 조회",
+        description = "로그인한 나의 회원정보를 조회 기능입니다."
     )
     @GetMapping("/me")
     @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
