95e76334dd982a04f5f48e1685bb43d9e8a854b2
신동근 <tlsehdrms124@naver.com>
2025-06-22 14:58:45 +0900

    [BE] feat: 대표 여행지 Repository Port 정의 (인터페이스)

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/out/RepresentativeRegionRepositoryPort.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/out/RepresentativeRegionRepositoryPort.java
@@ -3,8 +3,11 @@ package com.followfollowme.tripmarble.domainlayer.region.application.port.out;
 import com.followfollowme.tripmarble.domainlayer.region.domain.model.RepresentativeRegion;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RepresentativeRegionRepositoryPort {
 
     List<RepresentativeRegion> findAll();
+
+    Optional<RepresentativeRegion> findById(long representativeRegionId);
 }
