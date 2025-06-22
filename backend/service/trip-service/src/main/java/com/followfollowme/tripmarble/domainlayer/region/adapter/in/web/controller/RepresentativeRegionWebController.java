74ace84875c00b319ea96069d10cb154f5bb0a3a
신동근 <tlsehdrms124@naver.com>
2025-06-23 00:19:19 +0900

    [BE] feat: 대표 여행지 전체 목록 조회 API 개발

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/in/web/controller/RepresentativeRegionWebController.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/in/web/controller/RepresentativeRegionWebController.java
@@ -3,6 +3,8 @@ package com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.controll
 import com.followfollowme.tripmarble.common.dto.Response;
 import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RepresentativeRegionResponse;
 import com.followfollowme.tripmarble.domainlayer.region.application.port.in.RepresentativeRegionWebUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +16,15 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/regions/representative")
+@Tag(name = "대표 여행지", description = "대표 여행지 관련 API 입니다.")
 public class RepresentativeRegionWebController {
 
     private final RepresentativeRegionWebUseCase representativeRegionWebUseCase;
 
+    @Operation(
+        summary = "대표 여행지 전체 목록 조회",
+        description = "대표 여행지 전체 목록을 조회 기능입니다."
+    )
     @GetMapping
     public ResponseEntity<Response<List<RepresentativeRegionResponse>>> getAllRepresentativeRegions() {
         List<RepresentativeRegionResponse> representativeRegionResponses = representativeRegionWebUseCase.getAllRepresentativeRegions();
