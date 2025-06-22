e4d9448fcebcede470668ccce54004e45ba8ff6c
신동근 <tlsehdrms124@naver.com>
2025-06-23 00:16:45 +0900

    [BE] feat: 대표 여행지 Repository 관련 시군구 코드 목록을 통한 시군구 엔티티 목록 조회 기능 갭라

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/out/persistence/repository/TripSpotRepository.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/out/persistence/repository/TripSpotRepository.java
@@ -3,6 +3,9 @@ package com.followfollowme.tripmarble.domainlayer.trip.adapter.out.persistence.r
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.out.persistence.entity.TripSpotEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TripSpotRepository extends JpaRepository<TripSpotEntity, Long> {
-    
+
+    List<TripSpotEntity> findAllBySigunguCodeIn(List<Integer> sigunguCodes);
 }
