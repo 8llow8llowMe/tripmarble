b6bfb2665619830a10fa56744d5b8d336ef8f8ea
신동근 <tlsehdrms124@naver.com>
2025-06-22 14:56:04 +0900

    [BE] feat: 지역 관련 (시도, 시군구) Repository Port 인터페이스 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/out/SigunguRepositoryPort.java
@@ -0,0 +1,10 @@
+package com.followfollowme.tripmarble.domainlayer.region.application.port.out;
+
+import com.followfollowme.tripmarble.domainlayer.region.domain.model.Sigungu;
+
+import java.util.List;
+
+public interface SigunguRepositoryPort {
+
+    List<Sigungu> findAllByRegionId(long regionId);
+}
