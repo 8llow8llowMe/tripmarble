fc0f9ec8e77423262f484dd4da574d5ce422353e
신동근 <tlsehdrms124@naver.com>
2025-06-23 00:19:07 +0900

    [BE] feat: 대표 여행지에 따른 여행지 목록 조회 API 개발

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/in/web/controller/TripSpotWebController.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/in/web/controller/TripSpotWebController.java
@@ -1,6 +1,7 @@
 package com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.controller;
 
 import com.followfollowme.tripmarble.common.dto.Response;
+import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto.TripSpotSimpleResponse;
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto.TripSpotWithDetailViewResponse;
 import com.followfollowme.tripmarble.domainlayer.trip.application.port.in.TripSpotWebUseCase;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/trip-spots")
@@ -19,6 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class TripSpotWebController {
 
     private final TripSpotWebUseCase tripSpotWebUseCase;
+
+    @Operation(
+        summary = "대표 여행지에 따른 여행지 목록 조회",
+        description = "해당 대표 여행지에 속한 여행지 목록을 조회하는 기능입니다."
+    )
+    @GetMapping("/by-representative-region/{representativeRegionId}")
+    public ResponseEntity<Response<List<TripSpotSimpleResponse>>> getTripSpotsByRepresentativeRegionId(
+        @PathVariable long representativeRegionId) {
+        List<TripSpotSimpleResponse> tripSpotSimpleResponses =
+            tripSpotWebUseCase.getTripSpotsByRepresentativeRegionId(representativeRegionId);
+        return ResponseEntity.ok().body(Response.success(tripSpotSimpleResponses));
+    }
 
     @Operation(
         summary = "여행지 상세 정보 조회",
