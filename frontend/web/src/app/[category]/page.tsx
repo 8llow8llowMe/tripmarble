104b4d6ef2bdae49f17cd71e0dd64adce51f46d5
choiseongho <chltjdgh3@naver.com>
2025-06-23 23:23:44 +0900

    [FE/web] feat: 여행지 목록 페이지 제작

--- frontend/web/src/app/[category]/page.tsx
+++ frontend/web/src/app/[category]/page.tsx
@@ -4,13 +4,19 @@ import { notFound, useParams } from "next/navigation";
 import { convertingToString } from "@/utils/common";
 import SearchPage from "@/app/[category]/(search)/SearchPage";
 import SpotsPage from "@/app/[category]/(spots)/SpotsPage";
+<<<<<<< HEAD
 import LoginPage from "@/app/[category]/(user)/page";
+=======
+>>>>>>> ad9ee0b ([FE/web] feat: 여행지 목록 페이지 제작)
 
 const categoryComponentMap: Record<string, React.ComponentType> = {
   search: SearchPage,
   // plan: PlanPage,
   spots: SpotsPage,
+<<<<<<< HEAD
   login: LoginPage,
+=======
+>>>>>>> ad9ee0b ([FE/web] feat: 여행지 목록 페이지 제작)
 };
 
 export default function CategoryPage() {
