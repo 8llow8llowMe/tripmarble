f6553b38c82a822a0813a151d3b42b97a264b8fd
신동근 <tlsehdrms124@naver.com>
2025-06-23 21:13:21 +0900

    [BE] fix: rebase 관련 충돌 해결

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/in/web/dto/TripSpotWithDetailViewResponse.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/in/web/dto/TripSpotWithDetailViewResponse.java
@@ -5,18 +5,7 @@ import lombok.Builder;
 @Builder
 public record TripSpotWithDetailViewResponse(
     long tripSpotId,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 89feead ([BE] feat: 각 서비스당 Map Struct 적용, Trip Service 관련 개발 (지역, 여행 도메인 관련))
     String contentTypeName, // TODO: contentTypeId 이용해서 관련 매핑
-=======
-    String contentTypeName,
->>>>>>> e29d80b ([BE] feat: 각 서비스당 Map Struct 적용, Trip Service 관련 개발 (지역, 여행 도메인 관련))
-=======
-    String contentTypeName, // TODO: contentTypeId 이용해서 관련 매핑
->>>>>>> 7a4c3d8 ([BE] chore: 간격 수정)
     String homepageUrl,
     String overview
 ) {
