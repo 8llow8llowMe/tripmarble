cc88a9c60bdb2cfcfec585f7b23e6bf502e2b3a4
신동근 <tlsehdrms124@naver.com>
2025-06-24 00:05:58 +0900

    [BE] feat: 여행 관광지 JDBC Port 인터페이스 정의

---
+++ backend/service/batch-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/port/out/TripSpotJdbcPort.java
@@ -0,0 +1,10 @@
+package com.followfollowme.tripmarble.domainlayer.trip.application.port.out;
+
+import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.batch.dto.TripSpotItem;
+
+import java.util.List;
+
+public interface TripSpotJdbcPort {
+
+    void batchInsertTripSpots(List<TripSpotItem> tripSpotItems);
+}
