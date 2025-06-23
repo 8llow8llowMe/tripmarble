93359ccc255c11f37116090148d00caac470182b
choiseongho <chltjdgh3@naver.com>
2025-06-24 00:31:39 +0900

    [FE/web] feat: 로그인 화면 퍼블리싱

--- frontend/web/src/app/[category]/page.tsx
+++ frontend/web/src/app/[category]/page.tsx
@@ -4,11 +4,13 @@ import { notFound, useParams } from "next/navigation";
 import { convertingToString } from "@/utils/common";
 import SearchPage from "@/app/[category]/(search)/SearchPage";
 import SpotsPage from "@/app/[category]/(spots)/SpotsPage";
+import LoginPage from "@/app/[category]/(user)/page";
 
 const categoryComponentMap: Record<string, React.ComponentType> = {
   search: SearchPage,
   // plan: PlanPage,
   spots: SpotsPage,
+  login: LoginPage,
 };
 
 export default function CategoryPage() {
