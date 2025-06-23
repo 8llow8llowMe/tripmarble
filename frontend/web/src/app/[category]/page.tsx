6735dc7a687425da3d2decb688c38a13d08421e3
choiseongho <chltjdgh3@naver.com>
2025-06-24 00:31:39 +0900

    [FE/web] feat: 로그인 화면 퍼블리싱

--- frontend/web/src/app/[category]/page.tsx
+++ frontend/web/src/app/[category]/page.tsx
@@ -5,18 +5,26 @@ import { convertingToString } from "@/utils/common";
 import SearchPage from "@/app/[category]/(search)/SearchPage";
 import SpotsPage from "@/app/[category]/(spots)/SpotsPage";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import LoginPage from "@/app/[category]/(user)/page";
 =======
 >>>>>>> ad9ee0b ([FE/web] feat: 여행지 목록 페이지 제작)
+=======
+import LoginPage from "@/app/[category]/(user)/page";
+>>>>>>> 93359cc ([FE/web] feat: 로그인 화면 퍼블리싱)
 
 const categoryComponentMap: Record<string, React.ComponentType> = {
   search: SearchPage,
   // plan: PlanPage,
   spots: SpotsPage,
 <<<<<<< HEAD
+<<<<<<< HEAD
   login: LoginPage,
 =======
 >>>>>>> ad9ee0b ([FE/web] feat: 여행지 목록 페이지 제작)
+=======
+  login: LoginPage,
+>>>>>>> 93359cc ([FE/web] feat: 로그인 화면 퍼블리싱)
 };
 
 export default function CategoryPage() {
