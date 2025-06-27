8dfd1d98f7ec20b781e86c1ebac773a38ccf090d
신동근 <tlsehdrms124@naver.com>
2025-06-27 19:11:46 +0900

    [BE] feat: 여행지 정보 Repository에 custom Repository 상속 추가

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/out/persistence/repository/TripSpotRepository.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/out/persistence/repository/TripSpotRepository.java
@@ -1,11 +1,9 @@
 package com.followfollowme.tripmarble.domainlayer.trip.adapter.out.persistence.repository;
 
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.out.persistence.entity.TripSpotEntity;
+import com.followfollowme.tripmarble.domainlayer.trip.adapter.out.persistence.repository.custom.TripSpotCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+public interface TripSpotRepository extends JpaRepository<TripSpotEntity, Long>, TripSpotCustomRepository {
 
-public interface TripSpotRepository extends JpaRepository<TripSpotEntity, Long> {
-
-    List<TripSpotEntity> findAllByLdongSignguCdIn(List<Integer> ldongSignguCodes);
 }
