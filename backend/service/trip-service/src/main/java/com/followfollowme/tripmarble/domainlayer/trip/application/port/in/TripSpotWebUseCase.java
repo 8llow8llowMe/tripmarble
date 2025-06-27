1f9645dff8c4127ea57084d801d15fdb11101a29
신동근 <tlsehdrms124@naver.com>
2025-06-27 19:13:10 +0900

    [BE] feat: 대표 여행지에 따른 여행지 목록 조회 기능 no offset 기반 (무한 스크롤 방식) 개발

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/port/in/TripSpotWebUseCase.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/port/in/TripSpotWebUseCase.java
@@ -2,12 +2,13 @@ package com.followfollowme.tripmarble.domainlayer.trip.application.port.in;
 
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto.TripSpotSimpleResponse;
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto.TripSpotWithDetailViewResponse;
-
-import java.util.List;
+import com.followfollowme.tripmarble.persistence.dto.SliceResponse;
 
 public interface TripSpotWebUseCase {
 
-    List<TripSpotSimpleResponse> getTripSpotsByRepresentativeRegionId(long representativeRegionId);
+    SliceResponse<TripSpotSimpleResponse> getTripSpotsByRepresentativeRegionId(long representativeRegionId,
+        long lastTripSpotId,
+        int size);
 
     TripSpotWithDetailViewResponse getTripSpotWithDetail(long tripSpotId);
 }
