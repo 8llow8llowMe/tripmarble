100f8036939bc7ad00b1f81adcf81e28a96f57db
신동근 <tlsehdrms124@naver.com>
2025-06-30 16:07:12 +0900

    [BE] chore: 여행지 정보 및 상세 정보 관련 비즈니스 로직을 담은 Batch 서비스단 클래스명 변경

--- backend/service/batch-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/service/TripSpotBatchFacade.java
+++ backend/service/batch-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/service/TripSpotBatchFacade.java
@@ -1,7 +1,9 @@
 package com.followfollowme.tripmarble.domainlayer.trip.application.service;
 
+import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.batch.dto.TripSpotDetailItem;
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.batch.dto.TripSpotItem;
 import com.followfollowme.tripmarble.domainlayer.trip.application.port.in.TripSpotBatchUseCase;
+import com.followfollowme.tripmarble.domainlayer.trip.application.port.out.TripSpotDetailJdbcPort;
 import com.followfollowme.tripmarble.domainlayer.trip.application.port.out.TripSpotJdbcPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,12 +12,23 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class TripSpotBatchService implements TripSpotBatchUseCase {
+public class TripSpotBatchFacade implements TripSpotBatchUseCase {
 
     private final TripSpotJdbcPort tripSpotJdbcPort;
+    private final TripSpotDetailJdbcPort tripSpotDetailJdbcPort;
 
     @Override
     public void registerTripSpots(List<TripSpotItem> tripSpotItems) {
         tripSpotJdbcPort.batchInsertTripSpots(tripSpotItems);
+    }
+
+    @Override
+    public List<Integer> fetchAllContentIds() {
+        return tripSpotJdbcPort.findAllContentIds();
+    }
+
+    @Override
+    public void registerTripSpotDetails(List<TripSpotDetailItem> tripSpotDetailItems) {
+        tripSpotDetailJdbcPort.batchInsertTripSpotDetails(tripSpotDetailItems);
     }
 }
