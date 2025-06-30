d9bfcc1978538d591d08df5f2e5d74a7ef51e2d1
신동근 <tlsehdrms124@naver.com>
2025-06-30 16:51:56 +0900

    [BE] feat: 여행 관련 커스텀 에러코드 정의 및 커스텀 Exception 처리, Controller Advice정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/exception/TripServiceGlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.followfollowme.tripmarble.global.exception;
+
+import com.followfollowme.tripmarble.common.dto.Response;
+import com.followfollowme.tripmarble.domainlayer.trip.application.exception.TripException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class TripServiceGlobalExceptionHandler {
+
+    @ExceptionHandler(TripException.class)
+    public ResponseEntity<Response<Void>> tripException(TripException e) {
+        log.error("여행 관련 오류: {}", e.getMessage());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+            .body(Response.fail(e.getErrorCode().getCode(), e.getMessage()));
+    }
+}
