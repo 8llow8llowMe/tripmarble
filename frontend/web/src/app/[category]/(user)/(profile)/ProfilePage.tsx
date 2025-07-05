4eb41ca127c3c1c5b871adc202e6d34e5e79f7d4
seonghoho <chltjdgh3@naver.com>
2025-07-06 02:43:11 +0900

    [FE/web] fix: merge rebase 오류 수정

--- frontend/web/src/app/[category]/(user)/(profile)/ProfilePage.tsx
+++ frontend/web/src/app/[category]/(user)/(profile)/ProfilePage.tsx
@@ -11,21 +11,7 @@ export default function ProfilePage() {
     <div className={styles.profileWrapper}>
       <div className={styles.profileHeader}>
         <div className={styles.avatar}>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f975d6c ([FE/web] feat: profile 페이지 퍼블리싱)
-          <Image src={Logo} alt="User Avatar" width={150} height={150} />
-=======
           <Image src={Logo} alt="User Avatar" width={200} height={200} />
->>>>>>> dfeb421 ([FE/web] feat: profile 페이지 퍼블리싱)
-<<<<<<< HEAD
-=======
-          <Image src={Logo} alt="User Avatar" width={200} height={200} />
->>>>>>> 6556e65 ([FE/web] feat: profile 페이지 퍼블리싱)
-=======
->>>>>>> f975d6c ([FE/web] feat: profile 페이지 퍼블리싱)
         </div>
         <div className={styles.profileInfo}>
           <h2 className={styles.username}>트립마블</h2>
@@ -38,38 +24,14 @@ export default function ProfilePage() {
         <HorizontalList
           title="나의 게임 기록"
           items={spotsData}
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
           baseHref="/spots"
-=======
->>>>>>> dfeb421 ([FE/web] feat: profile 페이지 퍼블리싱)
-=======
->>>>>>> 6556e65 ([FE/web] feat: profile 페이지 퍼블리싱)
-=======
-          baseHref="/spots"
-=======
->>>>>>> dfeb421 ([FE/web] feat: profile 페이지 퍼블리싱)
->>>>>>> f975d6c ([FE/web] feat: profile 페이지 퍼블리싱)
           itemWidth={300}
           itemHeight={180}
         />
         <HorizontalList
           title="내가 쓴 리뷰"
           items={spotsData}
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
           baseHref="/spots"
-=======
->>>>>>> dfeb421 ([FE/web] feat: profile 페이지 퍼블리싱)
-=======
->>>>>>> 6556e65 ([FE/web] feat: profile 페이지 퍼블리싱)
-=======
-          baseHref="/spots"
-=======
->>>>>>> dfeb421 ([FE/web] feat: profile 페이지 퍼블리싱)
->>>>>>> f975d6c ([FE/web] feat: profile 페이지 퍼블리싱)
           itemWidth={300}
           itemHeight={180}
         />
