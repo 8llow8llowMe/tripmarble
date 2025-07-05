4969c0b04761b743d2cac8ee487ea208ecc6a2be
seonghoho <chltjdgh3@naver.com>
2025-07-05 13:00:43 +0900

    [FE/web] feat: profile 페이지 퍼블리싱

--- frontend/web/src/app/[category]/(user)/(profile)/ProfilePage.tsx
+++ frontend/web/src/app/[category]/(user)/(profile)/ProfilePage.tsx
@@ -12,10 +12,14 @@ export default function ProfilePage() {
       <div className={styles.profileHeader}>
         <div className={styles.avatar}>
 <<<<<<< HEAD
+<<<<<<< HEAD
           <Image src={Logo} alt="User Avatar" width={150} height={150} />
 =======
           <Image src={Logo} alt="User Avatar" width={200} height={200} />
 >>>>>>> dfeb421 ([FE/web] feat: profile 페이지 퍼블리싱)
+=======
+          <Image src={Logo} alt="User Avatar" width={200} height={200} />
+>>>>>>> 6556e65 ([FE/web] feat: profile 페이지 퍼블리싱)
         </div>
         <div className={styles.profileInfo}>
           <h2 className={styles.username}>트립마블</h2>
@@ -29,9 +33,12 @@ export default function ProfilePage() {
           title="나의 게임 기록"
           items={spotsData}
 <<<<<<< HEAD
+<<<<<<< HEAD
           baseHref="/spots"
 =======
 >>>>>>> dfeb421 ([FE/web] feat: profile 페이지 퍼블리싱)
+=======
+>>>>>>> 6556e65 ([FE/web] feat: profile 페이지 퍼블리싱)
           itemWidth={300}
           itemHeight={180}
         />
@@ -39,9 +46,12 @@ export default function ProfilePage() {
           title="내가 쓴 리뷰"
           items={spotsData}
 <<<<<<< HEAD
+<<<<<<< HEAD
           baseHref="/spots"
 =======
 >>>>>>> dfeb421 ([FE/web] feat: profile 페이지 퍼블리싱)
+=======
+>>>>>>> 6556e65 ([FE/web] feat: profile 페이지 퍼블리싱)
           itemWidth={300}
           itemHeight={180}
         />
