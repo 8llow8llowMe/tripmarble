98580b9b0a229ba281600c799eb56142c239f126
신동근 <tlsehdrms124@naver.com>
2025-07-10 19:32:22 +0900

    [BE] feat: Trip Game Service 초기 세팅

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/TripGameServiceApplication.java
@@ -0,0 +1,15 @@
+package com.followfollowme.tripmarble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class TripGameServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripGameServiceApplication.class, args);
+    }
+
+}
