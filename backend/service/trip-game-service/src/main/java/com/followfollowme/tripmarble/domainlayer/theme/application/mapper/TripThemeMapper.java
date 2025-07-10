da2771189ebd50fce3879686f8e751e066f0c806
신동근 <tlsehdrms124@naver.com>
2025-07-10 19:52:41 +0900

    [BE] feat: 여행 테마 관련 Mapper 정의

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/application/mapper/TripThemeMapper.java
@@ -0,0 +1,17 @@
+package com.followfollowme.tripmarble.domainlayer.theme.application.mapper;
+
+import com.followfollowme.tripmarble.domainlayer.theme.adapter.in.web.dto.TripThemeResponse;
+import com.followfollowme.tripmarble.domainlayer.theme.adapter.out.persistence.entity.TripThemeEntity;
+import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
+import java.util.List;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TripThemeMapper {
+
+    // 엔티티 리스트 -> 도메인 리스트
+    List<TripTheme> toDomainListFromEntityList(List<TripThemeEntity> entities);
+
+    // 도메인 리스트 -> DTO 리스트
+    List<TripThemeResponse> toResponseListFromDomainList(List<TripTheme> domains);
+}
