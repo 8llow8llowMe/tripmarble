54b6c60c60e27973e27e86afba27326bd6014ce8
신동근 <tlsehdrms124@naver.com>
2025-07-10 19:33:19 +0900

    [BE] feat: Trip Service 유레카 클라이언트 설정 추가

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/TripServiceApplication.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/TripServiceApplication.java
@@ -2,12 +2,14 @@ package com.followfollowme.tripmarble;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TripServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TripServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TripServiceApplication.class, args);
+    }
 
 }
