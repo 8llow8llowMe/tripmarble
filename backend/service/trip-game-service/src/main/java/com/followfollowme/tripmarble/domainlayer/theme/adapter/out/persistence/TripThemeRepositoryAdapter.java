24e64bc07e2b260c787ea3218cba4a34b4b55a39
신동근 <tlsehdrms124@naver.com>
2025-07-10 19:55:50 +0900

    [BE] feat: Map Struct를 이용하여 여행 테마 목록 조회 관련 기능 개발 및 매핑

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/adapter/out/persistence/TripThemeRepositoryAdapter.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/adapter/out/persistence/TripThemeRepositoryAdapter.java
@@ -1,6 +1,8 @@
 package com.followfollowme.tripmarble.domainlayer.theme.adapter.out.persistence;
 
+import com.followfollowme.tripmarble.domainlayer.theme.adapter.out.persistence.entity.TripThemeEntity;
 import com.followfollowme.tripmarble.domainlayer.theme.adapter.out.persistence.repository.TripThemeRepository;
+import com.followfollowme.tripmarble.domainlayer.theme.application.mapper.TripThemeMapper;
 import com.followfollowme.tripmarble.domainlayer.theme.application.port.out.TripThemeRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
 import java.util.List;
@@ -12,9 +14,11 @@ import org.springframework.stereotype.Component;
 public class TripThemeRepositoryAdapter implements TripThemeRepositoryPort {
 
     private final TripThemeRepository tripThemeRepository;
+    private final TripThemeMapper tripThemeMapper;
 
     @Override
     public List<TripTheme> findAll() {
-        return null;
+        List<TripThemeEntity> entities = tripThemeRepository.findAll();
+        return tripThemeMapper.toDomainListFromEntityList(entities);
     }
 }
