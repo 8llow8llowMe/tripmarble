d0f630de7634778bc1550a87193521dd49221c03
신동근 <tlsehdrms124@naver.com>
2025-07-11 19:13:39 +0900

    [BE] feat: 여행 계획 (게임) 생성 비즈니스 로직 개발

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/in/TripGameWebUseCase.java
@@ -0,0 +1,9 @@
+package com.followfollowme.tripmarble.domainlayer.game.application.port.in;
+
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameCreateResponse;
+import com.followfollowme.tripmarble.domainlayer.game.application.command.TripGameCreateCommand;
+
+public interface TripGameWebUseCase {
+
+    TripGameCreateResponse crateTripGame(TripGameCreateCommand command);
+}
