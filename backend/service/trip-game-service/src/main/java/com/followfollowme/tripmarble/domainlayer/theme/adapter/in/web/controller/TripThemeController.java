9406aa64ddc98647143af2d059d72bb668188ed5
신동근 <tlsehdrms124@naver.com>
2025-07-11 16:51:55 +0900

    [BE] feat: 여행 테마 목록 조회 API 개발

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/adapter/in/web/controller/TripThemeController.java
@@ -0,0 +1,32 @@
+package com.followfollowme.tripmarble.domainlayer.theme.adapter.in.web.controller;
+
+import com.followfollowme.tripmarble.common.dto.Response;
+import com.followfollowme.tripmarble.domainlayer.theme.adapter.in.web.dto.TripThemeResponse;
+import com.followfollowme.tripmarble.domainlayer.theme.application.port.in.TripThemeWebUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/trip-themes")
+@Tag(name = "여행 테마", description = "여행 테마 관련 클라이언트에 제공하는 API 입니다.")
+public class TripThemeController {
+
+    private final TripThemeWebUseCase tripThemeWebUseCase;
+
+    @Operation(
+        summary = "여행 테마 목록 조회",
+        description = "여행 테마 목록을 조회하는 기능입니다."
+    )
+    @GetMapping
+    public ResponseEntity<Response<List<TripThemeResponse>>> getAllTripThemes() {
+        List<TripThemeResponse> tripThemeResponses = tripThemeWebUseCase.getAllTripThemes();
+        return ResponseEntity.ok().body(Response.success(tripThemeResponses));
+    }
+}
