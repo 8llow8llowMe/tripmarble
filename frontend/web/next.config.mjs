f23e8c58ed6f9b12ec72ae70979f653b05ab50c3
seonghoho <chltjdgh3@naver.com>
2025-07-12 23:52:23 +0900

    [FE/web] feat: dotenv 설정 추가

--- frontend/web/next.config.mjs
+++ frontend/web/next.config.mjs
@@ -1,4 +1,20 @@
+import dotenv from "dotenv";
+
+const envPath =
+  process.env.NODE_ENV === "development"
+    ? "./env/.env-frontend-dev"
+    : "./env/.env-frontend-prod";
+
+dotenv.config({ path: envPath });
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: Object.keys(process.env)
+    .filter((key) => key.startsWith("NEXT_PUBLIC_"))
+    .reduce((env, key) => {
+      env[key] = process.env[key];
+      return env;
+    }, {}),
+};
 
 export default nextConfig;
