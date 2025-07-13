8405dcb7d3d897a935e679a7c1878b20ea9eb594
seonghoho <chltjdgh3@naver.com>
2025-07-13 20:28:58 +0900

    [FE/web] feat: 대표여행지 목록 페이지 api 작업 및 임시 더미데이터 표시

--- frontend/web/src/app/[category]/(spots)/[id]/page.tsx
+++ frontend/web/src/app/[category]/(spots)/[id]/page.tsx
@@ -1,12 +1,151 @@
 "use client";
-
-import { useParams } from "next/navigation";
+import Image from "next/image";
+import { seoul2 } from "@/assets/images/places";
+// style
+import styles from "./Spot.module.scss";
+// import { useParams } from "next/navigation";
+// import { useEffect, useRef } from "react";
+// import { useTripSpotsByRepresentativeRegion } from "@/hooks/queries/useTrips";
 
 const SpotDetailPage = () => {
-  const params = useParams();
-  const spotId = params.id;
+  // const { id: spotId } = useParams();
+  const dummyData = [
+    {
+      tripSpotId: 50693,
+      contentId: 2609624,
+      tripSpotName: "[백년가게]온천입구기장곰장어",
+      thumbnailImageUrl: "",
+    },
+    {
+      tripSpotId: 50692,
+      contentId: 2563357,
+      tripSpotName: "[백년가게]옥미아구찜",
+      thumbnailImageUrl: "",
+    },
+    {
+      tripSpotId: 50691,
+      contentId: 2605849,
+      tripSpotName: "[백년가게]언양한우불고기",
+      thumbnailImageUrl:
+        "http://tong.visitkorea.or.kr/cms/resource/68/2606468_image2_1.JPG",
+    },
+    {
+      tripSpotId: 50689,
+      contentId: 2609601,
+      tripSpotName: "[백년가게]신흥장어",
+      thumbnailImageUrl:
+        "http://tong.visitkorea.or.kr/cms/resource/90/2609790_image2_1.JPG",
+    },
+    {
+      tripSpotId: 50687,
+      contentId: 2609183,
+      tripSpotName: "[백년가게]신포순대",
+      thumbnailImageUrl: "",
+    },
+    {
+      tripSpotId: 50685,
+      contentId: 2608933,
+      tripSpotName: "[백년가게]신안촌",
+      thumbnailImageUrl:
+        "http://tong.visitkorea.or.kr/cms/resource/26/2609026_image2_1.JPG",
+    },
+    {
+      tripSpotId: 50684,
+      contentId: 2616274,
+      tripSpotName: "[백년가게]신동양",
+      thumbnailImageUrl:
+        "http://tong.visitkorea.or.kr/cms/resource/30/2616330_image2_1.jpg",
+    },
+    {
+      tripSpotId: 50683,
+      contentId: 2563226,
+      tripSpotName: "[백년가게]스미센",
+      thumbnailImageUrl:
+        "http://tong.visitkorea.or.kr/cms/resource/81/2563281_image2_1.jpg",
+    },
+    {
+      tripSpotId: 50682,
+      contentId: 2563237,
+      tripSpotName: "[백년가게]쉐라미",
+      thumbnailImageUrl:
+        "http://tong.visitkorea.or.kr/cms/resource/26/2563326_image2_1.jpg",
+    },
+    {
+      tripSpotId: 50679,
+      contentId: 2609645,
+      tripSpotName: "[백년가게]선동보리밥",
+      thumbnailImageUrl:
+        "http://tong.visitkorea.or.kr/cms/resource/12/2609812_image2_1.jpg",
+    },
+  ];
+  // const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+  //   useTripSpotsByRepresentativeRegion(String(spotId));
 
-  return <div>여행지 ID: {spotId}</div>;
+  // const observerRef = useRef<HTMLDivElement>(null);
+
+  // useEffect(() => {
+  //   if (
+  //     !observerRef.current ||
+  //     !data?.pages[data.pages.length - 1].data.dataBody.hasNext
+  //   )
+  //     return;
+
+  //   const observer = new IntersectionObserver(([entry]) => {
+  //     if (
+  //       entry.isIntersecting &&
+  //       data?.pages[data.pages.length - 1].data.dataBody.hasNext
+  //     ) {
+  //       fetchNextPage();
+  //     }
+  //   });
+
+  //   observer.observe(observerRef.current);
+  //   return () => observer.disconnect();
+  // }, [data?.pages, fetchNextPage]);
+
+  return (
+    <>
+      <section className={styles.spotHeader}>
+        <div className={styles.mainImageWrapper}>
+          <Image
+            src={seoul2}
+            alt="spot-main-image"
+            fill
+            priority
+            sizes="(max-width: 768px) 100vw, 250px"
+            className={styles.mainImage}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className={styles.regionContent}>
+          <h2>대한민국 지역</h2>
+          <p>해당 지역에 대한 설명을 여기에 표시</p>
+        </div>
+      </section>
+
+      <section className={styles.contentGrid}>
+        {/* {data?.pages.flatMap((page) =>
+          page.data.dataBody.contents.map((spot) => ( */}
+        {dummyData.map((spot) => (
+          <div key={spot.tripSpotId} className={styles.contentCard}>
+            <div className={styles.cardImage}>
+              <Image
+                src={spot.thumbnailImageUrl || "/images/no-image.png"}
+                alt={spot.tripSpotName}
+                fill
+                priority
+                sizes="(max-width: 768px) 100vw, 250px"
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+            <p className={styles.cardTitle}>{spot.tripSpotName}</p>
+          </div>
+        ))}
+        {/* <div ref={observerRef} />
+        {isFetchingNextPage && <p>불러오는 중...</p>} */}
+      </section>
+    </>
+  );
 };
 
 export default SpotDetailPage;
--- frontend/web/src/app/[category]/(spots)/layout.tsx
+++ frontend/web/src/app/[category]/(spots)/layout.tsx
@@ -1,9 +1,4 @@
 import { ReactNode } from "react";
 export default function SpotsLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <h1>여행지 상세</h1>
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
