62be9a79144748f2782e142def88f41d6cf178af
seonghoho <chltjdgh3@naver.com>
2025-07-14 23:07:37 +0900

    [FE/web] fix: 앱 처음 로딩 시 회원 정보 저장하도록 적용

--- frontend/web/src/components/layout/ClientLayout.tsx
+++ frontend/web/src/components/layout/ClientLayout.tsx
@@ -1,11 +1,24 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { usePathname } from "next/navigation";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { fetchMe } from "@/store/user";
 
 export default function ClientLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   const hasHeader = pathname !== "/";
+
+  // user 값 store에서 읽기
+  const user = useAppSelector((state) => state.user.user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    // user가 없고, 에러도 아닐 때만 fetchMe 실행
+    if (!user) {
+      dispatch(fetchMe());
+    }
+  }, [user, dispatch]);
 
   return (
     <div className={`layoutWrapper ${hasHeader ? " hasHeader" : ""}`}>
