7635b9e4b87112e54dfd31a5d4837357ba6fec02
신동근 <tlsehdrms124@naver.com>
2025-07-14 22:31:47 +0900

    [BE] feat: 여행 게임(계획) 생성 기능에 방장 관련 로직 추가

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameFacade.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameFacade.java
@@ -3,8 +3,11 @@ package com.followfollowme.tripmarble.domainlayer.game.application.service;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameCreateResponse;
 import com.followfollowme.tripmarble.domainlayer.game.application.command.TripGameCreateCommand;
 import com.followfollowme.tripmarble.domainlayer.game.application.port.in.TripGameWebUseCase;
+import com.followfollowme.tripmarble.domainlayer.game.application.port.out.TripGameMemberRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.game.application.port.out.TripGameRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameMember;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Status;
 import com.followfollowme.tripmarble.domainlayer.theme.application.port.out.TripThemeRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
 import com.followfollowme.tripmarble.persistence.util.SnowflakeIdGenerator;
@@ -18,14 +21,17 @@ public class TripGameFacade implements TripGameWebUseCase {
 
     private final TripGameRepositoryPort tripGameRepositoryPort;
     private final TripThemeRepositoryPort tripThemeRepositoryPort;
+    private final TripGameMemberRepositoryPort tripGameMemberRepositoryPort;
     private final SnowflakeIdGenerator snowflakeIdGenerator;
 
     @Override
     @Transactional
     public TripGameCreateResponse crateTripGame(TripGameCreateCommand command) {
+        // 1. 여행 게임(계획) 생성
         TripGame tripGame = TripGame.builder()
             .id(snowflakeIdGenerator.generateId())
             .title(command.title())
+            .status(Status.WAITING)
             .difficulty(command.difficulty())
             .startedAt(command.startedAt())
             .endedAt(command.endedAt())
@@ -33,20 +39,37 @@ public class TripGameFacade implements TripGameWebUseCase {
             .tripThemeId(command.tripThemeId())
             .build();
 
-        TripGame saved = tripGameRepositoryPort.save(tripGame);
+        TripGame savedTripGame = tripGameRepositoryPort.save(tripGame);
 
-        TripTheme tripTheme = tripThemeRepositoryPort.findById(saved.tripThemeId())
+        // 2. 방장(자기 자신)을 게임 참여자로 등록
+        TripGameMember tripGameMember = TripGameMember.builder()
+            .id(snowflakeIdGenerator.generateId())
+            .tripGameId(savedTripGame.id())
+            .memberId(command.memberId())
+            .isReady(false)
+            .isHost(true)
+            .build();
+
+        TripGameMember savedTripGameMember = tripGameMemberRepositoryPort.save(tripGameMember);
+
+        // 3. 테마 정보 조회
+        TripTheme tripTheme = tripThemeRepositoryPort.findById(savedTripGame.tripThemeId())
             .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 여행 테마입니다."));
 
         // TODO: 내부 서비스 통신에 의해 대표 여행지 이름 조회
 
         return TripGameCreateResponse.builder()
-            .tripGameId(saved.tripThemeId())
-            .difficulty(saved.difficulty())
-            .startedAt(saved.startedAt())
-            .endedAt(saved.endedAt())
+            .tripGameId(savedTripGame.tripThemeId())
+            .status(savedTripGame.status())
+            .statusMessage(savedTripGame.status().getDescription())
+            .difficulty(savedTripGame.difficulty())
+            .difficultyMessage(savedTripGame.difficulty().getDescription())
+            .startedAt(savedTripGame.startedAt())
+            .endedAt(savedTripGame.endedAt())
             .tripThemeName(tripTheme.name())
             .representativeRegionName(null) // 추후 내부 통신 추가
+            .isHost(savedTripGameMember.isHost())
+            .isReady(savedTripGameMember.isReady())
             .build();
     }
 }
