53f5f6b85e647a7dd3eabbbc4a18beb78520f22f
seonghoho <chltjdgh3@naver.com>
2025-07-14 23:07:57 +0900

    [FE/web] feat: 로그아웃 로직 추가

--- frontend/web/src/app/[category]/(user)/(profile)/ProfilePage.tsx
+++ frontend/web/src/app/[category]/(user)/(profile)/ProfilePage.tsx
@@ -1,12 +1,38 @@
 "use client";
 
-import Image from "next/image";
+import { Cookies } from "react-cookie";
+import { useRouter } from "next/navigation";
+// style
 import styles from "./Profile.module.scss";
+// component
 import HorizontalList from "@/components/common/HorizontalList/HorizontalList";
+// data
 import { spotsData } from "@/constants/spots";
+// image
+import Image from "next/image";
 import { Logo } from "@/assets/images";
+// store
+import { useAppDispatch } from "@/store/hooks";
+// api
+import { useLogout } from "@/hooks/queries/useUsers";
+import { logout } from "@/store/user/userSlice";
 
 export default function ProfilePage() {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const { mutate: logoutMutate } = useLogout();
+
+  const handleLogout = () => {
+    logoutMutate(undefined, {
+      onSuccess: () => {
+        const cookies = new Cookies();
+        cookies.remove("accessToken", { path: "/" });
+        dispatch(logout());
+        router.push("/");
+      },
+    });
+  };
+
   return (
     <div className={styles.profileWrapper}>
       <div className={styles.profileHeader}>
@@ -17,6 +43,9 @@ export default function ProfilePage() {
           <h2 className={styles.username}>트립마블</h2>
           <p className={styles.socialInfo}>카카오 로그인</p>
           <button className={styles.editButton}>프로필 수정</button>
+          <div onClick={handleLogout} className={styles.logoutButton}>
+            로그아웃
+          </div>
         </div>
       </div>
 
