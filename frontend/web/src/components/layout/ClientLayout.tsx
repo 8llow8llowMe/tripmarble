26ccea497a56f06260de9966cf61ed6a03e863a1
seonghoho <chltjdgh3@naver.com>
2025-07-14 23:25:19 +0900

    [FE/web] fix: accessToken이 존재할 경우에만 user 정보 api 호출

--- frontend/web/src/components/layout/ClientLayout.tsx
+++ frontend/web/src/components/layout/ClientLayout.tsx
@@ -4,21 +4,24 @@ import { ReactNode, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { fetchMe } from "@/store/user";
+import { useCookies } from "react-cookie";
 
 export default function ClientLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   const hasHeader = pathname !== "/";
 
+  const [cookies] = useCookies(["accessToken"]);
+  const accessToken = cookies.accessToken;
   // user 값 store에서 읽기
   const user = useAppSelector((state) => state.user.user);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     // user가 없고, 에러도 아닐 때만 fetchMe 실행
-    if (!user) {
+    if (accessToken && !user) {
       dispatch(fetchMe());
     }
-  }, [user, dispatch]);
+  }, [accessToken, user, dispatch]);
 
   return (
     <div className={`layoutWrapper ${hasHeader ? " hasHeader" : ""}`}>
