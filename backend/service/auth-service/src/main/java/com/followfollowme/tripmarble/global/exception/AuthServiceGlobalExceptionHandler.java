6dd225ec8c03032e5302343917bc646c2e8124c2
신동근 <tlsehdrms124@naver.com>
2025-07-14 17:29:25 +0900

    [BE] feat: auth service 관련 글로벌 Exception Handler에 Validation 관련 처리 개발

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/global/exception/AuthServiceGlobalExceptionHandler.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/global/exception/AuthServiceGlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.followfollowme.tripmarble.global.exception;
 
 import com.followfollowme.tripmarble.common.dto.Response;
+import com.followfollowme.tripmarble.common.exception.ValidationErrorCode;
 import com.followfollowme.tripmarble.domainlayer.member.application.exception.MemberException;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,4 +22,20 @@ public class AuthServiceGlobalExceptionHandler {
             .body(Response.fail(e.getErrorCode().getCode(), e.getMessage()));
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Response<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName + "Error", message);
+        });
+
+        ValidationErrorCode errorCode = ValidationErrorCode.VALIDATION_ERROR;
+        log.warn("Validation 오류: {}", errors);
+
+        return ResponseEntity.status(errorCode.getHttpStatus())
+            .body(Response.fail(errorCode.getCode(), errors));
+    }
 }
