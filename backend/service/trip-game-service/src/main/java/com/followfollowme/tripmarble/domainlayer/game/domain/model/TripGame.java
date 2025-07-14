3588a4f8e1a6d5612943fb424082576b7bba19ec
신동근 <tlsehdrms124@naver.com>
2025-07-14 22:28:54 +0900

    [BE] feat: 여행 게임(계획) 도메인에 상태 필드 추가

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/domain/model/TripGame.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/domain/model/TripGame.java
@@ -1,13 +1,16 @@
 package com.followfollowme.tripmarble.domainlayer.game.domain.model;
 
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Difficulty;
-import java.time.LocalDate;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Status;
 import lombok.Builder;
+
+import java.time.LocalDate;
 
 @Builder
 public record TripGame(
     long id,
     String title,
+    Status status,
     Difficulty difficulty,
     LocalDate startedAt,
     LocalDate endedAt,
