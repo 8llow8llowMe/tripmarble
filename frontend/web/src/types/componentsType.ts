5405abe3c29c1bdabf3b77f285263a4d293b9f39
seonghoho <seonghoho@codnut.com>
2025-07-17 12:00:45 +0900

    [FE/web] fix: type 분리 및 수정

---
+++ frontend/web/src/types/componentsType.ts
@@ -0,0 +1,14 @@
+import { StaticImageData } from "next/image";
+
+export type HorizontalListProps<T> = {
+  title?: string;
+  items: {
+    id: number;
+    name: string;
+    imgUrl: string | StaticImageData;
+  }[];
+  baseHref: string;
+  itemWidth?: number;
+  itemHeight?: number;
+  gap?: number;
+};
--- frontend/web/src/types/tripsType.ts
+++ frontend/web/src/types/tripsType.ts
@@ -5,14 +5,53 @@ export interface TripSpot {
   thumbnailImageUrl: string;
 }
 
+export interface dataHeader {
+  success: boolean;
+  resultCode: string | null;
+  resultMessage: string | null;
+}
+
+export interface ApiResponse<T> {
+  dataHeader: dataHeader;
+  dataBody: T;
+}
+
+// 여행 콘텐트 타입 (관광 타입) 목록 조회
+export interface TripContentTypesResponse {
+  contentTypeId: string;
+  contentTypeName: string;
+}
+
+// 대표 여행지 목록 전체 조회
+export interface RegionsRepresentativeResponse {
+  representativeRegionId: number;
+  representativeRegionName: string;
+  imageUrl: string | null;
+  regionId: number | null;
+  sigunguId: number | null;
+}
+
+// 대표 여행지에 따른 여행지 목록 조회 (무한 스크롤 방식)
 export interface TripSpotsResponse {
-  dataHeader: {
-    success: boolean;
-    resultCode: string | null;
-    resultMessage: string | null;
-  };
+  dataHeader: dataHeader;
   dataBody: {
     contents: TripSpot[];
     hasNext: boolean;
   };
+}
+
+// 여행지 상세 정보 조회
+export interface TripSpotDetailResponse {
+  tripSpotId: number;
+  tripSpotName: string;
+  contentTypeName: string;
+  description: string;
+  homepageUrl: string;
+  phoneNumber: string;
+  address: string;
+  addressDetail: string;
+  longitude: number;
+  latitude: number;
+  imageUrl: string;
+  thumbnailImageUrl: string;
 }
