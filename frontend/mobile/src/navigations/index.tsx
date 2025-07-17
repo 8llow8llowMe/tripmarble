cfde4f2abceba0ef5262b0007ded2e155fc82209
eestringz <tmdgus1761@gmail.com>
2025-07-17 17:24:11 +0900

    feat: 초기 스플래시 화면 렌더링 및 로그인 여부에 따른 navigation 분기 처리

--- frontend/mobile/src/navigations/index.tsx
+++ frontend/mobile/src/navigations/index.tsx
@@ -1,10 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabNavigator from './BottomTabNavigator';
+import React, { useEffect, useState } from 'react';
+import SplashScreen from '@/screens/Auth/SplashScreen';
+import AuthNavigator from '@/navigations/AuthNavigator';
 
 export default function RootNavigation() {
+  const [isLoading, setIsLoading] = useState(true); // 스플래시/로딩
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 여부
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+      // TODO: 로그인 여부 확인
+    }, 1500);
+  }, []);
+
+  if (isLoading) return <SplashScreen />; // 로딩 중에는 스플래시 보여주기
+
   return (
     <NavigationContainer>
-      <BottomTabNavigator />
+      {isLoggedIn ? <BottomTabNavigator /> : <AuthNavigator />}
     </NavigationContainer>
   );
 }
