87b806db2868c4edd79a659ac6462f80bb6318bf
eestringz <tmdgus1761@gmail.com>
2025-07-17 17:23:19 +0900

    feat: auth navigation 설계

---
+++ frontend/mobile/src/navigations/AuthNavigator.tsx
@@ -0,0 +1,15 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '@/screens/Auth/LoginScreen';
+import SignUpScreen from '@/screens/Auth/SignUpScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AuthNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="SignUp" component={SignUpScreen} />
+      {/* TODO: ID/PW 찾기 등 추가 */}
+    </Stack.Navigator>
+  );
+}
