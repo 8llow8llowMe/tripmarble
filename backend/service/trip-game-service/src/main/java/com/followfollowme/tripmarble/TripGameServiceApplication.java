d00a40203f597a9502d96151199f4021dfb07931
신동근 <tlsehdrms124@naver.com>
2025-07-18 20:02:50 +0900

    [BE] feat: trip game service Feign Client 사용할 수 있게끔 해당 어노테이션 적용

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/TripGameServiceApplication.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/TripGameServiceApplication.java
@@ -3,7 +3,9 @@ package com.followfollowme.tripmarble;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {
     "com.followfollowme.tripmarble.domainlayer",
