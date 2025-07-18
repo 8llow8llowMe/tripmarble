8cef1ac4fa19af1c92194abd2955f31400a24e45
신동근 <tlsehdrms124@naver.com>
2025-07-18 19:12:53 +0900

    [BE] fix: Response DTO 클래스명 수정에 따른 Mapper 수정 및 정의

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/mapper/RepresentativeRegionMapper.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/mapper/RepresentativeRegionMapper.java
@@ -1,6 +1,7 @@
 package com.followfollowme.tripmarble.domainlayer.region.application.mapper;
 
-import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RepresentativeRegionResponse;
+import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RepresentativeRegionDetailResponse;
+import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RepresentativeRegionSummaryResponse;
 import com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.entity.RepresentativeRegionEntity;
 import com.followfollowme.tripmarble.domainlayer.region.domain.model.RepresentativeRegion;
 import java.util.List;
@@ -11,8 +12,6 @@ import org.mapstruct.Mapping;
 public interface RepresentativeRegionMapper {
 
     // 엔티티 -> 도메인
-    @Mapping(source = "region.id", target = "regionId")
-    @Mapping(source = "sigungu.id", target = "sigunguId")
     RepresentativeRegion toDomainFromEntity(RepresentativeRegionEntity entity);
 
     // 엔티티 리스트 -> 도메인 리스트
@@ -21,8 +20,12 @@ public interface RepresentativeRegionMapper {
     // 도메인 -> DTO
     @Mapping(source = "id", target = "representativeRegionId")
     @Mapping(source = "name", target = "representativeRegionName")
-    RepresentativeRegionResponse toResponseFromDomain(RepresentativeRegion domain);
+    RepresentativeRegionSummaryResponse toSummaryResponseFromDomain(RepresentativeRegion domain);
+
+    @Mapping(source = "id", target = "representativeRegionId")
+    @Mapping(source = "name", target = "representativeRegionName")
+    RepresentativeRegionDetailResponse toDetailResponseFromDomain(RepresentativeRegion domain);
 
     // 도메인 리스트 -> DTO 리스트
-    List<RepresentativeRegionResponse> toResponseListFromDomainList(List<RepresentativeRegion> domains);
+    List<RepresentativeRegionSummaryResponse> toSummaryResponseListFromDomainList(List<RepresentativeRegion> domains);
 }
