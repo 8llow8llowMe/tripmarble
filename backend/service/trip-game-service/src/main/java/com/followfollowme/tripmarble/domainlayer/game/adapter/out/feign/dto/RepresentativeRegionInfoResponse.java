b653e9b1fadc2d17b7670b53f164ded3c7f79950
신동근 <tlsehdrms124@naver.com>
2025-07-18 20:00:44 +0900

    [BE] feat: 내부 서비스 통신에 의해 대표 여행지 정보 조회 Response DTO 정의

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/dto/RepresentativeRegionInfoResponse.java
@@ -0,0 +1,8 @@
+package com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.dto;
+
+public record RepresentativeRegionInfoResponse(
+    long representativeRegionId,
+    String representativeRegionName
+) {
+
+}
