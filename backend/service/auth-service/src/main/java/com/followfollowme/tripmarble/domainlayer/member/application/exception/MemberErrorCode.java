1bb9eeb6a4e821320298510883b4425cbee78274
신동근 <tlsehdrms124@naver.com>
2025-07-18 18:27:21 +0900

    [BE] fix: 회원 커스텀 에러코드 수정 (직관적이게끔)

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/exception/MemberErrorCode.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/exception/MemberErrorCode.java
@@ -8,10 +8,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum MemberErrorCode {
 
-    EXIST_MEMBER_EMAIL("MEM001", "이미 가입된 이메일 (%s)입니다.", HttpStatus.CONFLICT),
-    NOT_FOUND_MEMBER("MEM002", "존재하지 않는 회원입니다", HttpStatus.NOT_FOUND),
-    NOT_MATCH_PASSWORD("MEM003", "비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
-    UPLOAD_PROFILE_IMAGE_FAILED("MEM004", "프로필 이미지 업로드에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    EXIST_MEMBER_EMAIL("MEMBER_001", "이미 가입된 이메일 (%s)입니다.", HttpStatus.CONFLICT),
+    NOT_FOUND_MEMBER("MEMBER_002", "존재하지 않는 회원입니다", HttpStatus.NOT_FOUND),
+    NOT_MATCH_PASSWORD("MEMBER_003", "비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
+    UPLOAD_PROFILE_IMAGE_FAILED("MEMBER_004", "프로필 이미지 업로드에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final String code;
     private final String errorMessage;
