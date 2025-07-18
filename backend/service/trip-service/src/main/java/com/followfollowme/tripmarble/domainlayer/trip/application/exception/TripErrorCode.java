3db634c11f68681e57b841401dbceaf534d24db7
신동근 <tlsehdrms124@naver.com>
2025-07-18 18:26:54 +0900

    [BE] fix: 여행 관련 커스텀 에러코드 수정 (직관적이게끔)

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/exception/TripErrorCode.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/exception/TripErrorCode.java
@@ -8,9 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum TripErrorCode {
 
-    TRIP_SPOT_NOT_FOUND("T001", "해당 여행지 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    TRIP_CONTENT_TYPE_NOT_FOUND("T002", "해당 여행지 콘텐츠 타입을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    TRIP_SPOT_DETAIL_NOT_FOUND("T003", "해당 여행지 상세 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    TRIP_SPOT_NOT_FOUND("TRIP_001", "해당 여행지 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    TRIP_CONTENT_TYPE_NOT_FOUND("TRIP_002", "해당 여행지 콘텐츠 타입을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    TRIP_SPOT_DETAIL_NOT_FOUND("TRIP_003", "해당 여행지 상세 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String code;
     private final String errorMessage;
