85002ec476e7a93939fdfa8c476421ab06b378ed
신동근 <tlsehdrms124@naver.com>
2025-07-18 18:10:05 +0900

    [BE] feat: 대표 여행지_시군구 매핑 테이블 데이터 정리

--- backend/service/trip-service/src/main/resources/db/insert_representative_region_sigungu_mapping.sql
+++ backend/service/trip-service/src/main/resources/db/insert_representative_region_sigungu_mapping.sql
@@ -1,27 +1,132 @@
 -- 서울 매핑
 INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
-VALUES (1, 1),
-       (1, 2),
-       (1, 3),
-       (1, 4),
-       (1, 5),
-       (1, 6),
-       (1, 7),
-       (1, 8),
-       (1, 9),
-       (1, 10),
-       (1, 11),
-       (1, 12),
-       (1, 13),
-       (1, 14),
-       (1, 15),
-       (1, 16),
-       (1, 17),
-       (1, 18),
-       (1, 19),
-       (1, 20),
-       (1, 21),
-       (1, 22),
-       (1, 23),
-       (1, 24),
-       (1, 25);
+VALUES (1, 1),  -- 종로구
+       (1, 2),  -- 중구
+       (1, 3),  -- 용산구
+       (1, 4),  -- 성동구
+       (1, 5),  -- 광진구
+       (1, 6),  -- 동대문구
+       (1, 7),  -- 중랑구
+       (1, 8),  -- 성북구
+       (1, 9),  -- 강북구
+       (1, 10), -- 도봉구
+       (1, 11), -- 노원구
+       (1, 12), -- 은평구
+       (1, 13), -- 서대문구
+       (1, 14), -- 마포구
+       (1, 15), -- 양천구
+       (1, 16), -- 강서구
+       (1, 17), -- 구로구
+       (1, 18), -- 금천구
+       (1, 19), -- 영등포구
+       (1, 20), -- 동작구
+       (1, 21), -- 관악구
+       (1, 22), -- 서초구
+       (1, 23), -- 강남구
+       (1, 24), -- 송파구
+       (1, 25) -- 강동구
+;
+
+-- 대전 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (2, 66), -- 동구
+       (2, 67), -- 중구
+       (2, 68), -- 서구
+       (2, 69), -- 유성구
+       (2, 70) -- 대덕구
+;
+
+-- 여수 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (3, 160) -- 여수시
+;
+
+-- 대구 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (4, 42), -- 중구
+       (4, 43), -- 동구
+       (4, 44), -- 서구
+       (4, 45), -- 남구
+       (4, 46), -- 북구
+       (4, 47), -- 수성구
+       (4, 48), -- 달서구
+       (4, 49), -- 달성군
+       (4, 50) -- 군위군
+;
+
+-- 부산 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (5, 26), -- 중구
+       (5, 27), -- 서구
+       (5, 28), -- 동구
+       (5, 29), -- 영도구
+       (5, 30), -- 부산진구
+       (5, 31), -- 동래구
+       (5, 32), -- 남구
+       (5, 33), -- 북구
+       (5, 34), -- 해운대구
+       (5, 35), -- 사하구
+       (5, 36), -- 금정구
+       (5, 37), -- 강서구
+       (5, 38), -- 연제구
+       (5, 39), -- 수영구
+       (5, 40), -- 사상구
+       (5, 41) -- 기장군
+;
+
+-- 강릉 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (6, 232) -- 강릉시
+;
+
+-- 양양 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (7, 247) -- 양양군
+;
+
+-- 경주 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (8, 184) -- 경주시
+;
+
+-- 전주 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (9, 248), -- 전주시
+       (9, 249), -- 전주시 완산구
+       (9, 250) -- 전주시 덕진구
+;
+
+-- 제주 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (10, 228), -- 제주시
+       (10, 229) -- 서귀포시
+;
+
+-- 목포 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (11, 159) -- 목포시
+;
+
+-- 속초 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (12, 235) -- 속초시
+;
+
+-- 춘천 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (13, 230) -- 춘천시
+;
+
+-- 가평 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (14, 125) -- 가평군
+;
+
+-- 광주 매핑
+INSERT INTO representative_region_sigungu_mapping (representative_region_id, sigungu_id)
+VALUES (15, 61), -- 동구
+       (15, 62), -- 서구
+       (15, 63), -- 남구
+       (15, 64), -- 북구
+       (15, 65) -- 광산구
+;
