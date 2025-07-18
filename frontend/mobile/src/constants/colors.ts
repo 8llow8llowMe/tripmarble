06474730eaaa6c2ab33076c695e8b4d653195891
eestringz <tmdgus1761@gmail.com>
2025-07-18 16:11:56 +0900

    feat: 폰트/컬러 상수 파일 작성

---
+++ frontend/mobile/src/constants/colors.ts
@@ -0,0 +1,79 @@
+export const palette = {
+  black: '#000',
+  white: '#fff', // primaryWhite
+  error: 'red', // errorText
+  Neutral300: '#A8B1BD', // dateColor, countColor, disabledText
+  Neutral800: '#0F1B2A', // regularText
+  orange100: '#FDEAD7', // recentListBackground
+  orange300: '#F7B27A', // mainInput, focusInputShadow
+  orange400: '#F38744', // mainButton, orangeText
+  orange500: '#EF6820', // mainColor
+  rose200: '#FECCD6',
+  rose500: '#F63D68', // removeText, countNumber
+  gray50: '#F8F9FB', // placeholderBack
+  gray100: '#EEF1F6', // bottomSheetBackground,
+  gray150: '#E4E8EE', // focusInputShadow
+  gray200: '#CDD3DD', // inputBorder, toastMessage
+  gray300: '#A8B1BD', // dateColor
+  gray400: '#8D99A8', // grayText, placeholderText
+  gray500: '#7D8998', // textGray
+  gray550: '#5F6B7A', // countColor
+  gray600: '#414D5C', // subText, labelText, receivedMessageText
+  gray800: '#0F1B2A', // recentListBackground
+  blue300: '#5984FF', // searchText, linkText, readCount
+  blue400: '#36BFFA', // mentionText
+
+  bottomSheetBackground: '#F3F4F8',
+  receivedMessageBackground: '#F3F4F8',
+  confirmCheck: '#DCFAE6',
+  completeText: '#12B76A',
+  alertError: '#FEE4E2',
+  currentCheck: '#29CC6A',
+  toastBackground: '#192534',
+  confirmButtonBackground: '#47CD89',
+  alertButtonBackground: '#F97066',
+  snackBarShadow: '#262b43',
+  transparent: 'transparent',
+  contentColor: '#E4E8EE',
+  placeHolder: 'rgba(255, 255, 255, 0.6)',
+  borderBottomColor: 'rgba(228, 232, 238, 0.20)',
+  reportText: '#F97066',
+
+  purple300: '#BDB4FE',
+  yellow300: '#FEC84B',
+  pink300: '#FAA7E0',
+  green300: '#6CE9A6',
+  rose300: '#FEA3B4',
+  blueLight300: '#7CD4FD',
+  blue200: '#89A8FF',
+  red300: '#FDA29B',
+};
+
+// 자주 사용하는 color 지정
+export const colors = {
+  primaryWhite: '#FFFFFF',
+  mainColor: '#EF6820',
+
+  regularText: palette.Neutral800,
+  subText: '#414D5C',
+  grayText: '#8D99A8',
+  orangeText: '#F38744',
+  errorText: 'red',
+  searchText: palette.blue300,
+  labelText: '#414D5C',
+  placeholderText: '#8D99A8',
+  completeText: '#12B76A',
+  removeText: '#F63D68',
+  linkText: palette.blue300,
+  mentionText: palette.blue400,
+
+  recentListBackground: '#0F1B2A',
+  placeholderBack: palette.gray50,
+  dateColor: '#A8B1BD',
+  countColor: '#A8B1BD',
+  transparent: 'transparent',
+
+  textGray: '#7D8998',
+  countNumber: '#F63D68',
+  inputBorder: '#CDD3DD',
+};
