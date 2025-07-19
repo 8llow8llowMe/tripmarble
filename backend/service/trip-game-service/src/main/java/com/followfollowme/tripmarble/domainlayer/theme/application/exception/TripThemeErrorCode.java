dff974ac6faec6f2a9529a5aae031451fce5e099
신동근 <tlsehdrms124@naver.com>
2025-07-19 13:26:17 +0900

    [BE] feat: 여행 테마 관련 커스텀 에러코드 정의

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/application/exception/TripThemeErrorCode.java
@@ -0,0 +1,16 @@
+package com.followfollowme.tripmarble.domainlayer.theme.application.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum TripThemeErrorCode {
+
+    TRIP_THEME_NOT_FOUND("TRIP_THEME_001", "존재하지 않는 여행 테마 입니다.", HttpStatus.NOT_FOUND);
+
+    private final String code;
+    private final String errorMessage;
+    private final HttpStatus httpStatus;
+}
