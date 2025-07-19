8fd4dc5db5926323468407c819bd6e6cacd78a69
신동근 <tlsehdrms124@naver.com>
2025-07-19 13:28:32 +0900

    [BE] fix: 수정된 Mapper에 의해 해당 여행 게임(계획) 저장 부분 수정

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/out/TripGameRepositoryPort.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/out/TripGameRepositoryPort.java
@@ -1,8 +1,9 @@
 package com.followfollowme.tripmarble.domainlayer.game.application.port.out;
 
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
+import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
 
 public interface TripGameRepositoryPort {
 
-    TripGame save(TripGame domain);
+    TripGame save(TripGame tripGame, TripTheme tripTheme);
 }
