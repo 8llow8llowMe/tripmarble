d731f06f84ddde82c404739978bb1f03c60c5a96
신동근 <tlsehdrms124@naver.com>
2025-07-19 15:34:21 +0900

    [BE] fix: 여행 게임 (계획) 참여자 Map Struct 수정 (매핑 관련)

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/mapper/TripGameMemberMapper.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/mapper/TripGameMemberMapper.java
@@ -1,15 +1,22 @@
 package com.followfollowme.tripmarble.domainlayer.game.application.mapper;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.out.persistence.entity.TripGameMemberEntity;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameMember;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = TripGameMapper.class)
 public interface TripGameMemberMapper {
 
     // 엔티티 -> 도메인
+    @Mapping(target = "tripGameId", source = "tripGame.id")
     TripGameMember toDomainFromEntity(TripGameMemberEntity entity);
 
-    // 도메인 -> 엔티티
-    TripGameMemberEntity toEntityFromDomain(TripGameMember domain);
+    @Mapping(target = "tripGame", source = "tripGame")
+    @Mapping(target = "id", source = "domain.id")
+    @Mapping(target = "memberId", source = "domain.memberId")
+    @Mapping(target = "isReady", source = "domain.isReady")
+    @Mapping(target = "isHost", source = "domain.isHost")
+    TripGameMemberEntity toEntityFromDomain(TripGameMember domain, TripGame tripGame);
 }
