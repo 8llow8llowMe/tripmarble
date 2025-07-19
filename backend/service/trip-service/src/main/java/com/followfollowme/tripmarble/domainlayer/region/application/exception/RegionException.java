fe63de435593bf9a05d4173248fe5385ca4f70a9
신동근 <tlsehdrms124@naver.com>
2025-07-19 11:10:41 +0900

    [BE] feat: 지역 및 대표 여행지 관련 커스텀 에러코드에 따른 커스텀 Exception 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/exception/RegionException.java
@@ -0,0 +1,14 @@
+package com.followfollowme.tripmarble.domainlayer.region.application.exception;
+
+import lombok.Getter;
+
+@Getter
+public class RegionException extends RuntimeException {
+
+    private final RegionErrorCode errorCode;
+
+    public RegionException(RegionErrorCode errorCode) {
+        super(errorCode.getErrorMessage());
+        this.errorCode = errorCode;
+    }
+}
