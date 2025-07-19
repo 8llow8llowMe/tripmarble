71fcca1c85ffcad1004005426d467f9bc59644e2
신동근 <tlsehdrms124@naver.com>
2025-07-19 16:37:25 +0900

    [BE] feat: 여행 게임 (계획) 생성 관련 Info DTO 정의

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/info/TripGameCreateInfo.java
@@ -0,0 +1,28 @@
+package com.followfollowme.tripmarble.domainlayer.game.application.info;
+
+import com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.dto.RepresentativeRegionInfoResponse;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameMember;
+import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record TripGameCreateInfo(
+    TripGame tripGame,
+    TripGameMember tripGameMember,
+    List<TripTheme> tripThemes,
+    RepresentativeRegionInfoResponse representativeRegionInfo
+) {
+    public static TripGameCreateInfo of(
+        TripGame game, TripGameMember member, List<TripTheme> themes, RepresentativeRegionInfoResponse info
+    ) {
+        return TripGameCreateInfo.builder()
+            .tripGame(game)
+            .tripGameMember(member)
+            .tripThemes(themes)
+            .representativeRegionInfo(info)
+            .build();
+    }
+}
