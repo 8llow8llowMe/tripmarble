58967de1fb0f00c799d645689a782a807fd4793b
신동근 <tlsehdrms124@naver.com>
2025-07-19 11:10:19 +0900

    [BE] feat: 지역 및 대표 여행지 관련 커스텀 에러 코드 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/exception/RegionErrorCode.java
@@ -0,0 +1,16 @@
+package com.followfollowme.tripmarble.domainlayer.region.application.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum RegionErrorCode {
+
+    REPRESENTATIVE_REGION_NOT_FOUND("REGION_001", "해당 대표 여행지가 존재하지 않습니다.", HttpStatus.NOT_FOUND);
+
+    private final String code;
+    private final String errorMessage;
+    private final HttpStatus httpStatus;
+}
