074e1eeac3cfa94150144e2ba533dfee6a3cc1eb
신동근 <tlsehdrms124@naver.com>
2025-07-19 11:31:02 +0900

    [BE] feat: 여행 게임 서비스 관련 Swagger 설정 정의

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/global/config/TripGameServiceSwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.followfollowme.tripmarble.global.config;
+
+import com.followfollowme.tripmarble.common.support.SwaggerSecurityCommon;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(SwaggerSecurityCommon.class)
+@OpenAPIDefinition(
+    info = @Info(
+        title = "트립마블(TripMarble) 여행 게임(계획) 서비스 관련 API 명세서",
+        description = "Trip Game Service 전용",
+        version = "v1"
+    )
+)
+public class TripGameServiceSwaggerConfig {
+}
