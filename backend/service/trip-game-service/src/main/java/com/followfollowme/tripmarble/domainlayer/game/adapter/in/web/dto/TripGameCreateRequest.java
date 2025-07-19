c275488587341ea2d2bd8a1efd7d32f7bf60b273
신동근 <tlsehdrms124@naver.com>
2025-07-19 16:35:59 +0900

    [BE] feat: 여행 게임 (계획) 생성 Request, Response DTO 정의

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/in/web/dto/TripGameCreateRequest.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/in/web/dto/TripGameCreateRequest.java
@@ -2,7 +2,9 @@ package com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto;
 
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Difficulty;
 import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
+import java.util.List;
 
 @Schema(description = "여행 게임 생성 요청 DTO")
 public record TripGameCreateRequest(
@@ -19,11 +21,11 @@ public record TripGameCreateRequest(
     @Schema(description = "여행 종료일", example = "2025-07-20")
     LocalDate endedAt,
 
-    @Schema(description = "대표 여행지 아이디", example = "101")
+    @Schema(description = "대표 여행지 아이디", example = "10")
     long representativeRegionId,
 
-    @Schema(description = "여행 테마 아이디", example = "5")
-    long tripThemeId
+    @Schema(description = "여행 테마 아이디 목록", example = "[1, 2, 3, 4]")
+    List<Long> tripThemeIds
 ) {
 
 }
