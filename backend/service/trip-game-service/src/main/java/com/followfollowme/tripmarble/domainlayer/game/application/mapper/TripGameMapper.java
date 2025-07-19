cb7b15d1dc3c6200cc082328fa69010440504490
신동근 <tlsehdrms124@naver.com>
2025-07-19 15:33:33 +0900

    [BE] fix: 여행 게임 (계획) Map Struct 수정 (원복)

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/mapper/TripGameMapper.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/mapper/TripGameMapper.java
@@ -3,25 +3,14 @@ package com.followfollowme.tripmarble.domainlayer.game.application.mapper;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.out.persistence.entity.TripGameEntity;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
 import com.followfollowme.tripmarble.domainlayer.theme.application.mapper.TripThemeMapper;
-import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = TripThemeMapper.class)
 public interface TripGameMapper {
 
     // 엔티티 -> 도메인
-    @Mapping(target = "tripThemeId", source = "tripTheme.id")
     TripGame toDomainFromEntity(TripGameEntity entity);
 
     // 도메인 -> 엔티티
-    @Mapping(target = "id", source = "domain.id")
-    @Mapping(target = "title", source = "domain.title")
-    @Mapping(target = "status", source = "domain.status")
-    @Mapping(target = "difficulty", source = "domain.difficulty")
-    @Mapping(target = "startedAt", source = "domain.startedAt")
-    @Mapping(target = "endedAt", source = "domain.endedAt")
-    @Mapping(target = "representativeRegionId", source = "domain.representativeRegionId")
-    @Mapping(target = "tripTheme", source = "tripTheme")
-    TripGameEntity toEntityFromDomain(TripGame domain, TripTheme tripTheme);
+    TripGameEntity toEntityFromDomain(TripGame domain);
 }
