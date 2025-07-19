f7c73154833f0e8549e3d7e8fd9048321ce2c9a3
신동근 <tlsehdrms124@naver.com>
2025-07-19 15:32:57 +0900

    [BE] feat: 여행 게임_테마 매핑 Jpa Repository 정의

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/persistence/repository/TripGameThemeMappingRepository.java
@@ -0,0 +1,8 @@
+package com.followfollowme.tripmarble.domainlayer.game.adapter.out.persistence.repository;
+
+import com.followfollowme.tripmarble.domainlayer.game.adapter.out.persistence.entity.TripGameThemeMappingEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TripGameThemeMappingRepository extends JpaRepository<TripGameThemeMappingEntity, Long> {
+
+}
