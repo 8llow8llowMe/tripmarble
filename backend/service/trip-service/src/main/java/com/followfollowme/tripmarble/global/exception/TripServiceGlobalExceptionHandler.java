eb7e1292a0faf76f0508261b1576fc1f72a872d0
신동근 <tlsehdrms124@naver.com>
2025-07-19 11:15:36 +0900

    [BE] feat: 지역 및 대표 여행지 관련 커스텀 Exception Handler 처리

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/exception/TripServiceGlobalExceptionHandler.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/exception/TripServiceGlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.followfollowme.tripmarble.global.exception;
 
 import com.followfollowme.tripmarble.common.dto.Response;
+import com.followfollowme.tripmarble.domainlayer.region.application.exception.RegionException;
 import com.followfollowme.tripmarble.domainlayer.trip.application.exception.TripException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,13 @@ public class TripServiceGlobalExceptionHandler {
     @ExceptionHandler(TripException.class)
     public ResponseEntity<Response<Void>> tripException(TripException e) {
         log.error("여행 관련 오류: {}", e.getMessage());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+            .body(Response.fail(e.getErrorCode().getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(RegionException.class)
+    public ResponseEntity<Response<Void>> regionException(RegionException e) {
+        log.error("지역 및 대표 여행지 관련 오류: {}", e.getMessage());
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
             .body(Response.fail(e.getErrorCode().getCode(), e.getMessage()));
     }
