fe83678b10acae3a47503ad3c2de82a525333c0e
신동근 <tlsehdrms124@naver.com>
2025-07-19 13:25:59 +0900

    [BE] feat: Trip Game Service Swagger 설정 정의

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/global/config/TripGameServiceSwaggerConfig.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/global/config/TripGameServiceSwaggerConfig.java
@@ -1,19 +1,36 @@
 package com.followfollowme.tripmarble.global.config;
 
+import com.followfollowme.tripmarble.common.property.SwaggerProperties;
 import com.followfollowme.tripmarble.common.support.SwaggerSecurityCommon;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 @Import(SwaggerSecurityCommon.class)
-@OpenAPIDefinition(
-    info = @Info(
-        title = "트립마블(TripMarble) 여행 게임(계획) 서비스 관련 API 명세서",
-        description = "Trip Game Service 전용",
-        version = "v1"
-    )
-)
 public class TripGameServiceSwaggerConfig {
+
+    private final SwaggerProperties properties;
+
+    @Bean
+    public OpenAPI tripGameOpenAPI(Components components, SecurityRequirement securityRequirement) {
+        return new OpenAPI()
+            .components(components)
+            .addSecurityItem(securityRequirement)
+            .info(new Info()
+                .title("트립마블(TripMarble) 여행 게임(계획) 서비스 관련 API 명세서")
+                .description("Trip Game Service 전용")
+                .version("v1")
+            )
+            .servers(List.of(new Server().url(properties.serverUrl())));
+    }
 }
