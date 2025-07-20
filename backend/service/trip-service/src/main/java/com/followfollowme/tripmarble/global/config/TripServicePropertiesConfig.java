a1850565f5df6902c6247fe41e6bf18ffe4b36d8
신동근 <tlsehdrms124@naver.com>
2025-07-20 22:38:31 +0900

    [BE] feat: Trip Service 관련 프로퍼티 바인딩 설정 클래스 추가 (Swagger)

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/config/TripServicePropertiesConfig.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/config/TripServicePropertiesConfig.java
@@ -1,12 +1,14 @@
 package com.followfollowme.tripmarble.global.config;
 
 import com.followfollowme.tripmarble.common.config.JasyptPropertiesConfig;
+import com.followfollowme.tripmarble.common.config.SwaggerPropertiesConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-    JasyptPropertiesConfig.class
+    JasyptPropertiesConfig.class,
+    SwaggerPropertiesConfig.class
 })
 public class TripServicePropertiesConfig {
 
