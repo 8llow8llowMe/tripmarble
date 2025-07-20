6645c061fa98eb309fc36ed2accbf67dfa5ca8d5
신동근 <tlsehdrms124@naver.com>
2025-07-20 22:35:50 +0900

    [BE] fix: Auth Service 관련 Swagger 설정 수정

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/AuthServiceSwaggerConfig.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/AuthServiceSwaggerConfig.java
@@ -1,20 +1,37 @@
 package com.followfollowme.tripmarble.global.config;
 
+import com.followfollowme.tripmarble.common.property.SwaggerProperties;
 import com.followfollowme.tripmarble.common.support.SwaggerSecurityCommon;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
 
 @Configuration
+@Profile("!prod")
+@RequiredArgsConstructor
 @Import(SwaggerSecurityCommon.class)
-@OpenAPIDefinition(
-    info = @Info(
-        title = "트립마블(TripMarble) 회원 및 인증/인가 서비스 관련 API 명세서",
-        description = "Auth Service 전용",
-        version = "v1"
-    )
-)
 public class AuthServiceSwaggerConfig {
 
+    private final SwaggerProperties properties;
+
+    @Bean
+    public OpenAPI authOpenAPI(Components components, SecurityRequirement securityRequirement) {
+        return new OpenAPI()
+            .components(components)
+            .addSecurityItem(securityRequirement)
+            .info(new io.swagger.v3.oas.models.info.Info()
+                .title("트립마블(TripMarble) 회원 및 인증/인가 서비스 관련 API 명세서")
+                .description("Auth Service 전용")
+                .version("v1")
+            )
+            .servers(List.of(new Server().url(properties.serverUrl())));
+    }
 }
