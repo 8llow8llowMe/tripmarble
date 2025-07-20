06358a07f8ef9d6b88086ade460330644360e71f
신동근 <tlsehdrms124@naver.com>
2025-07-20 20:51:49 +0900

    [BE] feat: 여행 테마 ID 목록으로 여행 테마 조회 기능 개발 (Adapter, Port)

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/application/port/out/TripThemeRepositoryPort.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/application/port/out/TripThemeRepositoryPort.java
@@ -1,6 +1,7 @@
 package com.followfollowme.tripmarble.domainlayer.theme.application.port.out;
 
 import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -9,4 +10,6 @@ public interface TripThemeRepositoryPort {
     List<TripTheme> findAll();
 
     Optional<TripTheme> findById(Long tripThemeId);
+
+    List<TripTheme> findByIdIn(List<Long> tripThemeIds);
 }
