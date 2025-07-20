3813f9e7fb63dbc5ea28d6f522d988d0b7d0f65c
신동근 <tlsehdrms124@naver.com>
2025-07-20 21:31:49 +0900

    [BE] feat: 여행 게임 관련 난이도 목록 조회 기능 개발

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/in/TripGameWebUseCase.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/in/TripGameWebUseCase.java
@@ -1,9 +1,14 @@
 package com.followfollowme.tripmarble.domainlayer.game.application.port.in;
 
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.DifficultyResponse;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameCreateResponse;
 import com.followfollowme.tripmarble.domainlayer.game.application.command.TripGameCreateCommand;
 
+import java.util.List;
+
 public interface TripGameWebUseCase {
+
+    List<DifficultyResponse> getAllDifficulties();
 
     TripGameCreateResponse crateTripGame(TripGameCreateCommand command);
 }
--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameFacade.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameFacade.java
@@ -1,19 +1,34 @@
 package com.followfollowme.tripmarble.domainlayer.game.application.service;
 
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.DifficultyResponse;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameCreateResponse;
 import com.followfollowme.tripmarble.domainlayer.game.application.command.TripGameCreateCommand;
 import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameCreateInfo;
 import com.followfollowme.tripmarble.domainlayer.game.application.port.in.TripGameWebUseCase;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Difficulty;
 import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TripGameFacade implements TripGameWebUseCase {
 
     private final TripGameCreateService tripGameCreateService;
+
+    @Override
+    public List<DifficultyResponse> getAllDifficulties() {
+        return Arrays.stream(Difficulty.values())
+            .map(difficulty -> DifficultyResponse.builder()
+                .code(difficulty.name())
+                .description(difficulty.getDescription())
+                .build())
+            .toList();
+    }
 
     @Override
     @Transactional
