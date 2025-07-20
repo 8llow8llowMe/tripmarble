d31eded054de38d88ff946cf7f7361e55137ad27
신동근 <tlsehdrms124@naver.com>
2025-07-20 20:51:49 +0900

    [BE] feat: 여행 테마 ID 목록으로 여행 테마 조회 기능 개발 (Adapter, Port)

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/adapter/out/persistence/TripThemeRepositoryAdapter.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/adapter/out/persistence/TripThemeRepositoryAdapter.java
@@ -5,10 +5,11 @@ import com.followfollowme.tripmarble.domainlayer.theme.adapter.out.persistence.r
 import com.followfollowme.tripmarble.domainlayer.theme.application.mapper.TripThemeMapper;
 import com.followfollowme.tripmarble.domainlayer.theme.application.port.out.TripThemeRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
-import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -27,5 +28,11 @@ public class TripThemeRepositoryAdapter implements TripThemeRepositoryPort {
     public Optional<TripTheme> findById(Long tripThemeId) {
         return tripThemeRepository.findById(tripThemeId)
             .map(tripThemeMapper::toDomainFromEntity);
+    }
+
+    @Override
+    public List<TripTheme> findByIdIn(List<Long> tripThemeIds) {
+        List<TripThemeEntity> entities = tripThemeRepository.findByIdIn(tripThemeIds);
+        return tripThemeMapper.toDomainListFromEntityList(entities);
     }
 }
