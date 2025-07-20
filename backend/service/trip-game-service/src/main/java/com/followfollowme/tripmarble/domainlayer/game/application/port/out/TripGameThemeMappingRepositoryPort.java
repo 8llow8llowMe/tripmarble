6bbf6e716b0754860e162058bf21f02fc29e9d7b
신동근 <tlsehdrms124@naver.com>
2025-07-20 20:52:38 +0900

    [BE] feat: 여행 게임_테마 saveAll 관련 기능 개발 (Adapter, Port)

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/out/TripGameThemeMappingRepositoryPort.java
@@ -0,0 +1,12 @@
+package com.followfollowme.tripmarble.domainlayer.game.application.port.out;
+
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameThemeMapping;
+import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
+
+import java.util.List;
+
+public interface TripGameThemeMappingRepositoryPort {
+
+    List<TripGameThemeMapping> saveAll(List<TripGameThemeMapping> mappings, TripGame tripGame, List<TripTheme> tripThemes);
+}
