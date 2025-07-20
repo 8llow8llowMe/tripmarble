dd3271762c63a39e931acb6c9c9c6abc6b1ba095
신동근 <tlsehdrms124@naver.com>
2025-07-20 20:50:45 +0900

    [BE] feat: 여행 게임_테마 매핑 MapStruct Mapper 정의 및 기능 개발

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/mapper/TripGameThemeMappingMapper.java
@@ -0,0 +1,43 @@
+package com.followfollowme.tripmarble.domainlayer.game.application.mapper;
+
+import com.followfollowme.tripmarble.domainlayer.game.adapter.out.persistence.entity.TripGameThemeMappingEntity;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameThemeMapping;
+import com.followfollowme.tripmarble.domainlayer.theme.application.mapper.TripThemeMapper;
+import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring", uses = {TripGameMapper.class, TripThemeMapper.class})
+public interface TripGameThemeMappingMapper {
+
+    // 엔티티 -> 도메인
+    @Mapping(target = "tripGameId", source = "tripGame.id")
+    @Mapping(target = "tripThemeId", source = "tripTheme.id")
+    TripGameThemeMapping toDomainFromEntity(TripGameThemeMappingEntity entity);
+
+    // 도메인 -> 엔티티
+    @Mapping(target = "id", source = "domain.id")
+    @Mapping(target = "tripGame", source = "tripGame")
+    @Mapping(target = "tripTheme", source = "tripTheme")
+    TripGameThemeMappingEntity toEntityFromDomain(TripGameThemeMapping domain, TripGame tripGame, TripTheme tripTheme);
+
+    default List<TripGameThemeMappingEntity> toEntitiesFromDomainList(
+        List<TripGameThemeMapping> domains, TripGame tripGame, List<TripTheme> tripThemes
+    ) {
+        Map<Long, TripTheme> themeMap = tripThemes.stream()
+            .collect(Collectors.toMap(TripTheme::id, Function.identity()));
+
+        return domains.stream()
+            .map(domain -> {
+                TripTheme tripTheme = themeMap.get(domain.tripThemeId());
+                return toEntityFromDomain(domain, tripGame, tripTheme);
+            })
+            .toList();
+    }
+}
