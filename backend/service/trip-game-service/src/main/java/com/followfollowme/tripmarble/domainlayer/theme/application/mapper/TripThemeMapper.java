6f1912ca4acb412acb2c15620f393741a47f29f7
신동근 <tlsehdrms124@naver.com>
2025-07-20 21:29:04 +0900

    [BE] fix: 여행 테마 응답 관련해서 도메인 -> 응답 DTO 매핑 수정

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/application/mapper/TripThemeMapper.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/application/mapper/TripThemeMapper.java
@@ -3,8 +3,10 @@ package com.followfollowme.tripmarble.domainlayer.theme.application.mapper;
 import com.followfollowme.tripmarble.domainlayer.theme.adapter.in.web.dto.TripThemeResponse;
 import com.followfollowme.tripmarble.domainlayer.theme.adapter.out.persistence.entity.TripThemeEntity;
 import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
-import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TripThemeMapper {
@@ -14,6 +16,11 @@ public interface TripThemeMapper {
 
     // 엔티티 리스트 -> 도메인 리스트
     List<TripTheme> toDomainListFromEntityList(List<TripThemeEntity> entities);
+
+    // 도메인 -> DTO
+    @Mapping(target = "tripThemeId", source = "id")
+    @Mapping(target = "tripThemeName", source = "name")
+    TripThemeResponse toResponseFromDomain(TripTheme domain);
 
     // 도메인 리스트 -> DTO 리스트
     List<TripThemeResponse> toResponseListFromDomainList(List<TripTheme> domains);
