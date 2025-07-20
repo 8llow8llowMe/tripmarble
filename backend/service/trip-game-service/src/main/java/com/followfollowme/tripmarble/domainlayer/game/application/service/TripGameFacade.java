9d20e430958607abe65255bccb4b43db74cf0425
신동근 <tlsehdrms124@naver.com>
2025-07-20 20:54:30 +0900

    [BE] refactor: 여행 계획(생성) 관련 기능 리팩토링

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameFacade.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameFacade.java
@@ -1,20 +1,10 @@
 package com.followfollowme.tripmarble.domainlayer.game.application.service;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameCreateResponse;
-import com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.dto.RepresentativeRegionInfoResponse;
 import com.followfollowme.tripmarble.domainlayer.game.application.command.TripGameCreateCommand;
+import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameCreateInfo;
 import com.followfollowme.tripmarble.domainlayer.game.application.port.in.TripGameWebUseCase;
-import com.followfollowme.tripmarble.domainlayer.game.application.port.out.RepresentativeRegionClientPort;
-import com.followfollowme.tripmarble.domainlayer.game.application.port.out.TripGameMemberRepositoryPort;
-import com.followfollowme.tripmarble.domainlayer.game.application.port.out.TripGameRepositoryPort;
-import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
-import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameMember;
-import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Status;
-import com.followfollowme.tripmarble.domainlayer.theme.application.exception.TripThemeErrorCode;
-import com.followfollowme.tripmarble.domainlayer.theme.application.exception.TripThemeException;
-import com.followfollowme.tripmarble.domainlayer.theme.application.port.out.TripThemeRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
-import com.followfollowme.tripmarble.persistence.util.SnowflakeIdGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,62 +13,27 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class TripGameFacade implements TripGameWebUseCase {
 
-    private final TripGameRepositoryPort tripGameRepositoryPort;
-    private final TripThemeRepositoryPort tripThemeRepositoryPort;
-    private final TripGameMemberRepositoryPort tripGameMemberRepositoryPort;
-    private final SnowflakeIdGenerator snowflakeIdGenerator;
-    private final RepresentativeRegionClientPort representativeRegionClientPort;
-
+    private final TripGameCreateService tripGameCreateService;
 
     @Override
     @Transactional
     public TripGameCreateResponse crateTripGame(TripGameCreateCommand command) {
-        // 1. 여행 테마 조회
-        TripTheme tripTheme = tripThemeRepositoryPort.findById(command.tripThemeId())
-            .orElseThrow(() -> new TripThemeException(TripThemeErrorCode.TRIP_THEME_NOT_FOUND));
-
-        // 1. 여행 게임(계획) 도메인 생성
-        TripGame tripGame = TripGame.builder()
-            .id(snowflakeIdGenerator.generateId())
-            .title(command.title())
-            .status(Status.WAITING)
-            .difficulty(command.difficulty())
-            .startedAt(command.startedAt())
-            .endedAt(command.endedAt())
-            .representativeRegionId(command.representativeRegionId())
-            .tripThemeId(command.tripThemeId())
-            .build();
-
-        // 3. 여행 게임 (계획) 저장 (tripTheme도 같이 전달)
-        TripGame savedTripGame = tripGameRepositoryPort.save(tripGame, tripTheme);
-
-        // 4. 방장(자기 자신)을 게임 참여자로 등록
-        TripGameMember tripGameMember = TripGameMember.builder()
-            .id(snowflakeIdGenerator.generateId())
-            .tripGameId(savedTripGame.id())
-            .memberId(command.memberId())
-            .isReady(false)
-            .isHost(true)
-            .build();
-
-        TripGameMember savedTripGameMember = tripGameMemberRepositoryPort.save(tripGameMember);
-
-        // 5. 내부 서비스 통신에 의해 대표 여행지 이름 조회
-        RepresentativeRegionInfoResponse representativeRegionInfoResponse =
-            representativeRegionClientPort.getRepresentativeRegionInfo(command.representativeRegionId());
+        TripGameCreateInfo info = tripGameCreateService.createTripGame(command);
 
         return TripGameCreateResponse.builder()
-            .tripGameId(savedTripGame.tripThemeId())
-            .status(savedTripGame.status())
-            .statusMessage(savedTripGame.status().getDescription())
-            .difficulty(savedTripGame.difficulty())
-            .difficultyMessage(savedTripGame.difficulty().getDescription())
-            .startedAt(savedTripGame.startedAt())
-            .endedAt(savedTripGame.endedAt())
-            .tripThemeName(tripTheme.name())
-            .representativeRegionName(representativeRegionInfoResponse.representativeRegionName())
-            .isHost(savedTripGameMember.isHost())
-            .isReady(savedTripGameMember.isReady())
+            .tripGameId(info.tripGame().id())
+            .status(info.tripGame().status())
+            .statusMessage(info.tripGame().status().getDescription())
+            .difficulty(info.tripGame().difficulty())
+            .difficultyMessage(info.tripGame().difficulty().getDescription())
+            .startedAt(info.tripGame().startedAt())
+            .endedAt(info.tripGame().endedAt())
+            .tripThemeNames(info.tripThemes().stream()
+                .map(TripTheme::name)
+                .toList())
+            .representativeRegionName(info.representativeRegionInfo().representativeRegionName())
+            .isHost(info.tripGameMember().isHost())
+            .isReady(info.tripGameMember().isReady())
             .build();
     }
 }
