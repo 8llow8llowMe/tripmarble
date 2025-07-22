5ef821a9df90b472df9c365b7b0113ab706d356f
신동근 <tlsehdrms124@naver.com>
2025-07-22 23:59:01 +0900

    [BE] feat: 여행 콘텐츠 타입 (관광 타입) Response DTO Swagger 적용

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/in/web/dto/TripContentTypeResponse.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/in/web/dto/TripContentTypeResponse.java
@@ -1,10 +1,16 @@
 package com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "여행 콘텐츠 타입 (관광 타입) 목록 조회 응답 DTO")
 public record TripContentTypeResponse(
+
+    @Schema(description = "TourAPI 콘텐츠 타입 아이디", example = "12")
     String contentTypeId,
+
+    @Schema(description = "여행 콘텐츠 타입 (관광 타입) 명칭", example = "관광지")
     String contentTypeName
 ) {
 
