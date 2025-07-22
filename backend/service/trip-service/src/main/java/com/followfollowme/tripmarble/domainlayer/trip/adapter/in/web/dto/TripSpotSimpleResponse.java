b9b1a846f93fdd550763fae6828fc30c9109cc6c
신동근 <tlsehdrms124@naver.com>
2025-07-22 23:59:34 +0900

    [BE] feat: 대표 여행지에 따른 여행지 관련 Response DTO Swagger 설명 추가

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/in/web/dto/TripSpotSimpleResponse.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/in/web/dto/TripSpotSimpleResponse.java
@@ -1,12 +1,22 @@
 package com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "대표 여행지에 따른 여행지 목록 조회 응답 DTO")
 public record TripSpotSimpleResponse(
+
+    @Schema(description = "여행지 정보 아이디", example = "1")
     long tripSpotId,
+
+    @Schema(description = "여행 콘텐츠 아이디", example = "2820640")
     int contentId,
+
+    @Schema(description = "여행지 정보 이름 (제목)", example = "가림상회")
     String tripSpotName,
-    String thumbnailImageUrl
+
+    @Schema(description = "여행지 대표 이미지 원본 URL", example = "http://tong.visitkorea.or.kr/~~~.jpg")
+    String originalImageUrl
 ) {
 }
