0eee55fdd0e98b48dac83bb9afad59a98131a15b
eestingz <tmdgus1761@gmail.com>
2025-07-22 14:48:07 +0900

    feat: key 변수 관련 상수 파일 작성

---
+++ frontend/mobile/src/constants/keys.ts
@@ -0,0 +1,10 @@
+const STORAGE_KEY = {
+  //AUTH
+  REMEMBER_ID: 'rememberId',
+  ACCESS_TOKEN: 'accessToken',
+  USER_ID: 'userId',
+};
+
+const QUERY_KEY = {};
+
+export { STORAGE_KEY, QUERY_KEY };
