8f7ef981c753091c0e2f51f100c36c4d4781183b
신동근 <tlsehdrms124@naver.com>
2025-07-25 18:25:59 +0900

    [BE] feat: 인증/인가 관련 커스텀 에러코드 정의

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/application/exception/AuthErrorCode.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/application/exception/AuthErrorCode.java
@@ -1,4 +1,17 @@
 package com.followfollowme.tripmarble.domainlayer.auth.application.exception;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
 public enum AuthErrorCode {
+
+    INVALID_VERIFICATION_CODE("AUTH_001", "인증코드가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
+    NOT_FOUND_VERIFICATION_CODE("AUTH_002", "인증코드가 존재하지 않습니다. 이메일 인증코드 재요청을 클릭해주세요.", HttpStatus.NOT_FOUND);
+
+    private final String code;
+    private final String errorMessage;
+    private final HttpStatus httpStatus;
 }
