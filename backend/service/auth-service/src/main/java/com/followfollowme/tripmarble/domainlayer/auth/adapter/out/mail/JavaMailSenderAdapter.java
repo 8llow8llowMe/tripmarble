c4787c499581414e04aa256a2838861d9600d7f1
신동근 <tlsehdrms124@naver.com>
2025-07-25 19:03:00 +0900

    [BE] fix: SENDER_EMAIL 부분 동적으로 주입받아서 쓸 수 있게끔 프로퍼티로 관리

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/adapter/out/mail/JavaMailSenderAdapter.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/adapter/out/mail/JavaMailSenderAdapter.java
@@ -1,6 +1,7 @@
 package com.followfollowme.tripmarble.domainlayer.auth.adapter.out.mail;
 
 import com.followfollowme.tripmarble.domainlayer.auth.application.port.out.MailSendPort;
+import com.followfollowme.tripmarble.global.properties.MailProperties;
 import jakarta.mail.Message.RecipientType;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
@@ -14,16 +15,16 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class JavaMailSenderAdapter implements MailSendPort {
 
-    private static final String SENDER_EMAIL = "no-reply@tripmarble.com";
     private static final String SENDER_NAME = "TripMarble";
 
     private final JavaMailSender javaMailSender;
+    private final MailProperties mailProperties;
 
     @Override
     public void send(String to, String subject, String htmlBody) {
         try {
             MimeMessage message = javaMailSender.createMimeMessage();
-            message.setFrom(new InternetAddress(SENDER_EMAIL, SENDER_NAME));
+            message.setFrom(new InternetAddress(mailProperties.username(), SENDER_NAME));
             message.addRecipients(RecipientType.TO, to);
             message.setSubject(subject);
             message.setText(htmlBody, "utf-8", "html");
