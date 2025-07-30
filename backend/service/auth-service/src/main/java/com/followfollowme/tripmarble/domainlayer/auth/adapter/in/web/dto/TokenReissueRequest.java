95e6ed938da1a3e17a545918ba67b9fb2756b912
신동근 <tlsehdrms124@naver.com>
2025-07-30 16:23:01 +0900

    [BE] feat: 토큰 재발급 요청 DTO 정의

---
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/adapter/in/web/dto/TokenReissueRequest.java
@@ -0,0 +1,12 @@
+package com.followfollowme.tripmarble.domainlayer.auth.adapter.in.web.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "토큰 재발급 요청 DTO")
+public record TokenReissueRequest(
+
+    @Schema(description = "회원 아이디", example = "202507110001")
+    long memberId
+) {
+
+}
