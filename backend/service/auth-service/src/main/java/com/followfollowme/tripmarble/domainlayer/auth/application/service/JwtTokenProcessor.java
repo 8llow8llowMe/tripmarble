598e407e58a37ff2387aa9f5a124dc2cb0768e82
신동근 <tlsehdrms124@naver.com>
2025-07-30 16:30:05 +0900

    [BE] feat: 토큰 재발급 관련 기능 개발

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/application/service/JwtTokenProcessor.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/application/service/JwtTokenProcessor.java
@@ -1,12 +1,19 @@
 package com.followfollowme.tripmarble.domainlayer.auth.application.service;
 
 import com.followfollowme.tripmarble.domainlayer.auth.adapter.in.web.dto.AuthLoginResponse;
+import com.followfollowme.tripmarble.domainlayer.auth.adapter.in.web.dto.TokenReissueResponse;
+import com.followfollowme.tripmarble.domainlayer.auth.application.exception.AuthErrorCode;
+import com.followfollowme.tripmarble.domainlayer.auth.application.exception.AuthException;
 import com.followfollowme.tripmarble.domainlayer.auth.application.port.out.JwtTokenStorePort;
+import com.followfollowme.tripmarble.domainlayer.member.application.exception.MemberErrorCode;
+import com.followfollowme.tripmarble.domainlayer.member.application.exception.MemberException;
+import com.followfollowme.tripmarble.domainlayer.member.application.port.out.MemberRepositoryPort;
+import com.followfollowme.tripmarble.domainlayer.member.domain.model.Member;
+import com.followfollowme.tripmarble.redis.exception.RedisInfraException;
 import com.followfollowme.tripmarble.security.auth.jwt.JwtAuthProvider;
 import com.followfollowme.tripmarble.security.common.enums.SecurityRole;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -16,6 +23,7 @@ public class JwtTokenProcessor {
 
     private final JwtAuthProvider jwtAuthProvider;
     private final JwtTokenStorePort jwtTokenStorePort;
+    private final MemberRepositoryPort memberRepositoryPort;
 
     public AuthLoginResponse issueTokens(long memberId, SecurityRole role) {
         String accessToken = jwtAuthProvider.issueAccessToken(memberId, role);
@@ -23,9 +31,10 @@ public class JwtTokenProcessor {
 
         try {
             jwtTokenStorePort.save(memberId, refreshToken);
-        } catch (RedisConnectionFailureException e) {
+        } catch (RedisInfraException e) {
             log.warn("Redis 연결 실패, RefreshToken 저장 안됨: {}", e.getMessage());
         }
+
         return AuthLoginResponse.builder()
             .accessToken(accessToken)
             .memberId(memberId)
@@ -34,5 +43,30 @@ public class JwtTokenProcessor {
 
     public void revoke(long memberId) {
         jwtTokenStorePort.find(memberId).ifPresent(token -> jwtTokenStorePort.delete(memberId));
+    }
+
+    public TokenReissueResponse reissueTokens(long memberId) {
+        try {
+            String refreshToken = jwtTokenStorePort.find(memberId)
+                .orElseThrow(() -> new AuthException(AuthErrorCode.EXPIRED_REFRESH_TOKEN));
+        } catch (RedisInfraException e) {
+            throw new AuthException(AuthErrorCode.TOKEN_REISSUE_FAILURE);
+        }
+
+        Member member = memberRepositoryPort.findById(memberId)
+            .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_MEMBER));
+
+        String newAccessToken = jwtAuthProvider.issueAccessToken(member.id(), member.role());
+        String newRefreshToken = jwtAuthProvider.issueRefreshToken();
+
+        try {
+            jwtTokenStorePort.save(member.id(), newRefreshToken);
+        } catch (RedisInfraException e) {
+            log.warn("Redis 연결 실패, RefreshToken 저장 안됨: {}", e.getMessage());
+        }
+
+        return TokenReissueResponse.builder()
+            .accessToken(newAccessToken)
+            .build();
     }
 }
