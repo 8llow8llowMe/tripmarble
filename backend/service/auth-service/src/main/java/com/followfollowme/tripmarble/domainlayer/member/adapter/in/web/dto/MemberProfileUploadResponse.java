eabe74d84f7b5464b9407dbe609f7b35159936ea
신동근 <tlsehdrms124@naver.com>
2025-07-30 16:22:04 +0900

    [BE] chore: 해당 DTO 설명 직관적으로 수정

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/in/web/dto/MemberProfileUploadResponse.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/in/web/dto/MemberProfileUploadResponse.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-@Schema(description = "프로필 이미지 업로드 응답 DTO")
+@Schema(description = "임시 프로필 이미지 업로드 응답 DTO")
 public record MemberProfileUploadResponse(
 
     @Schema(description = "업로드된 임시 이미지 URL", example = "https://cdn.tripmarble.com/profile_abc123.jpg")
