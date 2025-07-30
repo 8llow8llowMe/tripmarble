b2330c0c5c81ae8d040b1377ab9b10a97edf913a
신동근 <tlsehdrms124@naver.com>
2025-07-30 16:29:32 +0900

    [BE] feat: 인증/인가 관련 커스텀 에러코드에서 리프레쉬 토큰 만료 및 토큰 재발급 처리 관련 오류 정의

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/application/exception/AuthErrorCode.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/application/exception/AuthErrorCode.java
@@ -11,7 +11,9 @@ public enum AuthErrorCode {
     INVALID_VERIFICATION_CODE("AUTH_001", "인증코드가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
     NOT_FOUND_VERIFICATION_CODE("AUTH_002", "인증코드가 존재하지 않습니다. 이메일 인증코드 재요청을 클릭해주세요.", HttpStatus.NOT_FOUND),
     UNSUPPORTED_OAUTH_PROVIDER("AUTH_003", "지원하지 않는 소셜 로그인 제공자입니다. (입력값: %s)", HttpStatus.BAD_REQUEST),
-    UNMATCHED_OAUTH_PROVIDER("AUTH_004", "다른 방식으로 이미 가입된 계정입니다. 기존: %s", HttpStatus.CONFLICT);
+    UNMATCHED_OAUTH_PROVIDER("AUTH_004", "다른 방식으로 이미 가입된 계정입니다. 기존: %s", HttpStatus.CONFLICT),
+    EXPIRED_REFRESH_TOKEN("AUTH_005", "로그인 정보가 만료되었습니다. 다시 로그인해주세요.", HttpStatus.UNAUTHORIZED),
+    TOKEN_REISSUE_FAILURE("AUTH_006", "토큰 재발급 처리 중 오류가 발생하였습니다.", HttpStatus.SERVICE_UNAVAILABLE);
 
     private final String code;
     private final String errorMessage;
