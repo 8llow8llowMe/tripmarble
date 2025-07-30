9296075f7ad8c41e85454722432e1e74c4cd9b7d
신동근 <tlsehdrms124@naver.com>
2025-07-30 16:20:57 +0900

    [BE] fix: Redis 관련 Token Store Adapter 연결 관련 Exception 처리

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/adapter/out/persistence/RedisJwtTokenStoreAdapter.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/adapter/out/persistence/RedisJwtTokenStoreAdapter.java
@@ -1,9 +1,12 @@
 package com.followfollowme.tripmarble.domainlayer.auth.adapter.out.persistence;
 
 import com.followfollowme.tripmarble.domainlayer.auth.application.port.out.JwtTokenStorePort;
+import com.followfollowme.tripmarble.redis.exception.RedisErrorCode;
+import com.followfollowme.tripmarble.redis.exception.RedisInfraException;
 import com.followfollowme.tripmarble.security.auth.jwt.JwtAuthProperties;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -17,20 +20,26 @@ public class RedisJwtTokenStoreAdapter implements JwtTokenStorePort {
 
     @Override
     public void save(long memberId, String token) {
-        String key = KEY_PREFIX + memberId;
-        redisTemplate.opsForValue().set(key, token, jwtAuthProperties.refreshExpiration());
+        try {
+            redisTemplate.opsForValue().set(buildKey(memberId), token, jwtAuthProperties.refreshExpiration());
+        } catch (RedisConnectionFailureException e) {
+            throw new RedisInfraException(RedisErrorCode.CONNECTION_FAILURE);
+        }
+
     }
 
     @Override
     public Optional<String> find(long memberId) {
-        String key = KEY_PREFIX + memberId;
-        String token = redisTemplate.opsForValue().get(key);
+        String token = redisTemplate.opsForValue().get(buildKey(memberId));
         return Optional.ofNullable(token);
     }
 
     @Override
     public void delete(long memberId) {
-        String key = KEY_PREFIX + memberId;
-        redisTemplate.delete(key);
+        redisTemplate.delete(buildKey(memberId));
+    }
+
+    private String buildKey(long memberId) {
+        return KEY_PREFIX + memberId;
     }
 }
