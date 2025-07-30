78590a6df270b6d49c1022f48270165a46ff2943
신동근 <tlsehdrms124@naver.com>
2025-07-30 16:23:28 +0900

    [BE] feat: 토큰 재발급 command 정의

---
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/application/command/TokenReissueCommand.java
@@ -0,0 +1,16 @@
+package com.followfollowme.tripmarble.domainlayer.auth.application.command;
+
+import com.followfollowme.tripmarble.domainlayer.auth.adapter.in.web.dto.TokenReissueRequest;
+import lombok.Builder;
+
+@Builder
+public record TokenReissueCommand(
+    long memberId
+) {
+
+    public static TokenReissueCommand from(TokenReissueRequest request) {
+        return TokenReissueCommand.builder()
+            .memberId(request.memberId())
+            .build();
+    }
+}
