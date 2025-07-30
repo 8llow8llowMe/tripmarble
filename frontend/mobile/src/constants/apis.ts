52c6485c75444ebffc0abefcf9f70f55e3edafca
eestingz <tmdgus1761@gmail.com>
2025-07-30 14:20:38 +0900

    feat: api 엔드포인트 추가 작성

--- frontend/mobile/src/constants/apis.ts
+++ frontend/mobile/src/constants/apis.ts
@@ -1,7 +1,15 @@
 const END_POINTS = {
+  //AUTH
   SIGN_UP: `/members/signup`,
   LOGIN: `/auth/login`,
   LOGOUT: `/auth/logout`,
+  SEND_CODE: `/auth/mail/send-code`,
+  VERIFY_CODE: `/auth/mail/verfiy-code`,
+
+  //USER
+  USER: {
+    INFO: `/members/me`,
+  },
 };
 
 export { END_POINTS };
