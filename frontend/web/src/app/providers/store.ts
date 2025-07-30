97c2d980d6f0c9f6ebbd1192953e8c61aa6fd2a1
seonghoho <chltjdgh3@naver.com>
2025-07-30 23:04:45 +0900

    [FE/web] feat: create game store 추가

--- frontend/web/src/app/providers/store.ts
+++ frontend/web/src/app/providers/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "@/entities/users/model/user/userSlice";
 import formReducer from "@/entities/users/model/form/formSlice";
+import createGameReducer from "@/features/game/create-game/model/createGameSlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       form: formReducer,
       user: userReducer,
+      createGame: createGameReducer,
     },
   });
 };
