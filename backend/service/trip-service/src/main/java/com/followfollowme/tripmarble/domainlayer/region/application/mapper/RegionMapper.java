c742bc30f7a084ed8e57914237ae831c0627c72d
신동근 <tlsehdrms124@naver.com>
2025-08-01 19:09:44 +0900

    [BE] fix: 지역 관련 도메인 -> DTO는 Presenter한테 위임했으므로 불필요한 코드 삭제

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/mapper/RegionMapper.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/mapper/RegionMapper.java
@@ -1,23 +1,16 @@
 package com.followfollowme.tripmarble.domainlayer.region.application.mapper;
 
-import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RegionResponse;
 import com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.entity.RegionEntity;
 import com.followfollowme.tripmarble.domainlayer.region.domain.model.Region;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
 import java.util.List;
+import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface RegionMapper {
 
+    // 엔티티 -> 도메인
+    Region toDomainFromEntity(RegionEntity entity);
+
     // 엔티티 리스트 -> 도메인 리스트
     List<Region> toDomainListFromEntityList(List<RegionEntity> entities);
-
-    // 도메인 -> DTO
-    @Mapping(source = "id", target = "regionId")
-    RegionResponse toResponseFromDomain(Region domain);
-
-    // 도메인 리스트 -> DTO 리스트
-    List<RegionResponse> toResponseListFromDomainList(List<Region> domains);
 }
--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/mapper/RepresentativeRegionMapper.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/mapper/RepresentativeRegionMapper.java
@@ -1,13 +1,9 @@
 package com.followfollowme.tripmarble.domainlayer.region.application.mapper;
 
-import com.followfollowme.tripmarble.domainlayer.region.adapter.in.internal.dto.RepresentativeRegionInfoResponse;
-import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RepresentativeRegionDetailResponse;
-import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RepresentativeRegionSummaryResponse;
 import com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.entity.RepresentativeRegionEntity;
 import com.followfollowme.tripmarble.domainlayer.region.domain.model.RepresentativeRegion;
 import java.util.List;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface RepresentativeRegionMapper {
@@ -17,22 +13,4 @@ public interface RepresentativeRegionMapper {
 
     // 엔티티 리스트 -> 도메인 리스트
     List<RepresentativeRegion> toDomainListFromEntityList(List<RepresentativeRegionEntity> entities);
-
-    // 도메인 -> DTO
-    @Mapping(source = "id", target = "representativeRegionId")
-    @Mapping(source = "name", target = "representativeRegionName")
-    RepresentativeRegionSummaryResponse toSummaryResponseFromDomain(RepresentativeRegion domain);
-
-    @Mapping(source = "id", target = "representativeRegionId")
-    @Mapping(source = "name", target = "representativeRegionName")
-    RepresentativeRegionDetailResponse toDetailResponseFromDomain(RepresentativeRegion domain);
-
-    // 도메인 리스트 -> DTO 리스트
-    List<RepresentativeRegionSummaryResponse> toSummaryResponseListFromDomainList(List<RepresentativeRegion> domains);
-
-    // 내부 서비스 통신 전용
-    // 도메인 -> DTO
-    @Mapping(source = "id", target = "representativeRegionId")
-    @Mapping(source = "name", target = "representativeRegionName")
-    RepresentativeRegionInfoResponse toInfoResponseFromDomain(RepresentativeRegion domain);
 }
