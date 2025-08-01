49cd763f189fbf070814039b05f215ddc87b190c
신동근 <tlsehdrms124@naver.com>
2025-08-01 19:09:44 +0900

    [BE] fix: 지역 관련 도메인 -> DTO는 Presenter한테 위임했으므로 불필요한 코드 삭제

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/mapper/SigunguMapper.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/mapper/SigunguMapper.java
@@ -1,23 +1,18 @@
 package com.followfollowme.tripmarble.domainlayer.region.application.mapper;
 
-import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.SigunguResponse;
 import com.followfollowme.tripmarble.domainlayer.region.adapter.out.persistence.entity.SigunguEntity;
 import com.followfollowme.tripmarble.domainlayer.region.domain.model.Sigungu;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface SigunguMapper {
 
+    // 엔티티 -> 도메인
+    @Mapping(source = "region.id", target = "regionId")
+    Sigungu toDomainFromEntity(SigunguEntity entity);
+
     // 엔티티 리스트 -> 도메인 리스트
     List<Sigungu> toDomainListFromEntityList(List<SigunguEntity> entities);
-
-    // 도메인 -> DTO
-    @Mapping(source = "id", target = "sigunguId")
-    SigunguResponse toResponseFromDomain(Sigungu domain);
-
-    // 도메인 리스트 -> DTO 리스트
-    List<SigunguResponse> toResponseListFromDomainList(List<Sigungu> domains);
 }
