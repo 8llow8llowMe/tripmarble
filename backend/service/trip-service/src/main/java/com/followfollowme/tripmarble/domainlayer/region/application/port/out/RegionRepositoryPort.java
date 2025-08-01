7853c8b25c3b7a268c67b8129de6347e6438d1b5
신동근 <tlsehdrms124@naver.com>
2025-08-01 19:20:21 +0900

    [BE] feat: 지역 (시도) 관련 시도 ID에 따른 지역 조회 관련 기능 개발 (Repository)

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/out/RegionRepositoryPort.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/out/RegionRepositoryPort.java
@@ -1,10 +1,12 @@
 package com.followfollowme.tripmarble.domainlayer.region.application.port.out;
 
 import com.followfollowme.tripmarble.domainlayer.region.domain.model.Region;
-
 import java.util.List;
+import java.util.Optional;
 
 public interface RegionRepositoryPort {
 
     List<Region> findAll();
+
+    Optional<Region> findById(long regionId);
 }
