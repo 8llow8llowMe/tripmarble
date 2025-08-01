bd7ecaafa32fee6e66cb24163b7bcf7c9f3b3b89
신동근 <tlsehdrms124@naver.com>
2025-08-01 19:26:38 +0900

    [BE] feat: 내부 서비스 통신용 랜덤 여행지 조회 기능 개발 (여행 게임 생성 관련)

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/service/TripSpotInternalFacade.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/service/TripSpotInternalFacade.java
@@ -1,10 +1,13 @@
 package com.followfollowme.tripmarble.domainlayer.trip.application.service;
 
+import com.followfollowme.tripmarble.domainlayer.region.application.exception.RegionErrorCode;
+import com.followfollowme.tripmarble.domainlayer.region.application.exception.RegionException;
+import com.followfollowme.tripmarble.domainlayer.region.application.port.out.RegionRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.region.application.port.out.RepresentativeRegionSigunguMappingRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.region.application.port.out.SigunguRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.region.domain.model.Sigungu;
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.internal.dto.TripSpotRandomResponse;
-import com.followfollowme.tripmarble.domainlayer.trip.application.mapper.TripSpotMapper;
+import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.internal.presenter.TripSpotInternalPresenter;
 import com.followfollowme.tripmarble.domainlayer.trip.application.port.in.TripSpotInternalUseCase;
 import com.followfollowme.tripmarble.domainlayer.trip.application.port.out.TripSpotRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.trip.domain.model.TripSpot;
@@ -19,8 +22,9 @@ public class TripSpotInternalFacade implements TripSpotInternalUseCase {
 
     private final RepresentativeRegionSigunguMappingRepositoryPort representativeRegionSigunguMappingRepositoryPort;
     private final SigunguRepositoryPort sigunguRepositoryPort;
+    private final RegionRepositoryPort regionRepositoryPort;
     private final TripSpotRepositoryPort tripSpotRepositoryPort;
-    private final TripSpotMapper tripSpotMapper;
+    private final TripSpotInternalPresenter tripSpotInternalPresenter;
 
     @Override
     @Transactional(readOnly = true)
@@ -30,14 +34,24 @@ public class TripSpotInternalFacade implements TripSpotInternalUseCase {
         List<Long> sigunguIds = representativeRegionSigunguMappingRepositoryPort.findSigunguIdsByRepresentativeRegionId(
             representativeRegionId);
 
-        // 2. 시군구 ID -> 시군구 코드 변환
-        List<Integer> sigunguCodes = sigunguRepositoryPort.findAllByIdIn(sigunguIds).stream()
+        // 2. 시군구 도메인 조회
+        List<Sigungu> sigungus = sigunguRepositoryPort.findAllByIdIn(sigunguIds);
+
+        // 3. 시군구 코드 + 지역 코드 추출 (법정동 시도 코드, 시군구 코드 => 자연키)
+        List<Integer> sigunguCodes = sigungus.stream()
             .map(Sigungu::sigunguCode)
             .toList();
 
-        // 3. 랜덤 여행지 조회
+        // 4. 시군구 중 하나의 regionId -> regionCode 1개만 조회
+        long regionId = sigungus.getFirst().regionId();
+        int regionCode = regionRepositoryPort.findById(regionId)
+            .orElseThrow(() -> new RegionException(RegionErrorCode.REGION_NOT_FOUND))
+            .regionCode();
+
+        // 5. 랜덤 여행지 조회
         List<TripSpot> tripSpots = tripSpotRepositoryPort.findRandomTripSpotsBySigunguCodesAndContentTypeIds(
-            sigunguCodes, contentTypeIds, limit);
-        return tripSpotMapper.toRandomResponseListFromDomainList(tripSpots);
+            regionCode, sigunguCodes, contentTypeIds, limit);
+
+        return tripSpotInternalPresenter.toRandomResponseList(tripSpots);
     }
 }
