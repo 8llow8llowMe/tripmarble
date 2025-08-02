d6b0661f0d1c9cdd9fbf4cb178306941c70d0106
신동근 <tlsehdrms124@naver.com>
2025-08-02 15:27:33 +0900

    [BE] fix: 서비스 디스커버리에 등록된 애플리케이션 명칭 지정

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/client/TripContentTypeClient.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/client/TripContentTypeClient.java
@@ -1,13 +1,14 @@
 package com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.client;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.dto.TripContentTypeInternalResponse;
-import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @FeignClient(
-    name = "trip-service",
+    name = "${feign-client.target-services.trip-service}",
     contextId = "tripContentTypeClient",
     path = "/internal/v1/trip-content-types"
 )
--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/client/TripSpotClient.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/client/TripSpotClient.java
@@ -1,20 +1,21 @@
 package com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.client;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.dto.TripSpotRandomResponse;
-import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @FeignClient(
-    name = "trip-service",
+    name = "${feign-client.target-services.trip-service}",
     contextId = "tripSpotClient",
     path = "/internal/v1/trip-spots"
 )
 public interface TripSpotClient {
 
     @GetMapping("/by-representative-region/{representativeRegionId}/random")
-    List<TripSpotRandomResponse> getRandomTripSpots(@PathVariable long representativeRegionId,
-        @RequestParam List<Integer> contentTypeIds, @RequestParam(defaultValue = "10") int limit);
+    List<TripSpotRandomResponse> getRandomTripSpots(
+        @PathVariable long representativeRegionId, @RequestParam List<Integer> contentTypeIds, @RequestParam(defaultValue = "10") int limit);
 }
