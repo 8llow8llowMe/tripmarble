d54b136bcbb8876b17b1085596401ee810fcb9ce
seonghoho <chltjdgh3@naver.com>
2025-08-04 17:44:49 +0900

    [FE/web] fix: 프로필 정보 편집 로직 수정

--- frontend/web/src/entities/users/api/users.ts
+++ frontend/web/src/entities/users/api/users.ts
@@ -1,16 +1,9 @@
 import { authApiClient } from "@/shared/lib/api/client";
 
-export const getUserProfile = () => authApiClient.get("/users/me");
+export const getUserProfile = () => authApiClient.get("/members/me");
 
 export const updateUserProfile = (data: any) =>
-  authApiClient.put("/users/me", data);
-
-export const deleteUserAccount = () => authApiClient.delete("/users/me");
-
-export const login = (credentials: { email: string; password: string }) =>
-  authApiClient.post("/auth/login", credentials);
-
-export const logout = () => authApiClient.post("/auth/logout");
+  authApiClient.patch("members/me", data);
 
 export const signUp = (data: {
   email: string;
@@ -31,3 +24,10 @@ export const uploadTempProfileImage = (imageFile: File) => {
     },
   });
 };
+
+// export const deleteUserAccount = () => authApiClient.delete("/users/me");
+
+export const login = (credentials: { email: string; password: string }) =>
+  authApiClient.post("/auth/login", credentials);
+
+export const logout = () => authApiClient.post("/auth/logout");
