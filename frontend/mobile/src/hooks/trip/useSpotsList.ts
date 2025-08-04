6c3a82cb980f17d44b16e28c3792f8042b68f283
eestingz <tmdgus1761@gmail.com>
2025-08-04 18:04:38 +0900

    [FE/mobile] feat: 대표여행지에 따른 여행지 목록 type, api, hook 작성

---
+++ frontend/mobile/src/hooks/trip/useSpotsList.ts
@@ -0,0 +1,68 @@
+import { ApiResponseBase } from '@/apis/base';
+import { END_POINTS } from '@/constants/apis';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { apiClient } from '@/apis/axiosClient';
+import { QUERY_KEY } from '@/constants/keys';
+
+interface Props {
+  representativeRegionId: number;
+  size?: number;
+}
+
+export interface TripSpotListRequest {
+  representativeRegionId: number;
+  lastTripSpotId?: number;
+  size?: number;
+}
+
+export interface TripSpoListtResponse extends ApiResponseBase {
+  dataBody: {
+    contents: {
+      tripSpotId: number;
+      contentId: number;
+      tripSpotName: string;
+      thumbnailImageUrl: string | null;
+    }[];
+    hasNext: boolean;
+  };
+}
+
+export const getTripSpotList = async ({
+  representativeRegionId,
+  lastTripSpotId,
+  size = 10,
+}: TripSpotListRequest) => {
+  const params: any = {};
+  if (lastTripSpotId !== undefined) params.lastTripSpotId = lastTripSpotId;
+  if (size !== undefined) params.size = size;
+
+  const { data } = await apiClient.get<TripSpoListtResponse>(
+    END_POINTS.TRIP.LIST_SPOTS(representativeRegionId),
+    { params },
+  );
+
+  return data;
+};
+
+const useTripSpotListInfiniteQuery = ({ representativeRegionId, size = 10 }: Props) => {
+  return useInfiniteQuery({
+    queryFn: ({ pageParam }: { pageParam?: number }) =>
+      getTripSpotList({
+        representativeRegionId,
+        lastTripSpotId: pageParam,
+        size,
+      }),
+    queryKey: [QUERY_KEY.TRIP.SPOT, representativeRegionId],
+    enabled: !!representativeRegionId,
+    getNextPageParam: (lastPage) => {
+      const { dataBody } = lastPage || {};
+      if (dataBody?.hasNext && dataBody.contents.length > 0) {
+        return dataBody.contents[dataBody.contents.length - 1].tripSpotId;
+      }
+      return undefined;
+    },
+    initialPageParam: undefined,
+  });
+};
+
+export default useTripSpotListInfiniteQuery;
