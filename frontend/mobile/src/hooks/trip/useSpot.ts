c5f54d4d5eb108fdeb3fa5f169197bff895d4a31
eestingz <tmdgus1761@gmail.com>
2025-08-04 18:04:54 +0900

    [FE/mobile] feat: 여행지 정보 상세 조회  type, api, hook 작성

---
+++ frontend/mobile/src/hooks/trip/useSpot.ts
@@ -0,0 +1,54 @@
+import { ApiResponseBase } from '@/apis/base';
+import { END_POINTS } from '@/constants/apis';
+import { useQuery } from '@tanstack/react-query';
+import { apiClient } from '@/apis/axiosClient';
+import { QUERY_KEY } from '@/constants/keys';
+
+interface Props {
+  tripSpotId: number;
+}
+
+export interface TripSpotRequest {
+  tripSpotId: number;
+}
+
+export interface TripSpotResponse extends ApiResponseBase {
+  dataBody: {
+    tripSpotId: number;
+    tripSpotName: string;
+    contentTypeName: string;
+    description: string | null;
+    homepageUrl: string | null;
+    phoneNumber: string | null;
+    address: string | null;
+    addressDetail: string | null;
+    longitude: number;
+    latitude: number;
+    imageUrl: string | null;
+    thumbnailImageUrl: string | null;
+  };
+}
+
+export const getTripSpot = async ({ tripSpotId }: TripSpotRequest) => {
+  const { data } = await apiClient.get<TripSpotResponse>(END_POINTS.TRIP.SPOT(tripSpotId));
+
+  return data;
+};
+
+const useTripSpotQuery = ({ tripSpotId }: Props) => {
+  const { data, isLoading, isError, isSuccess, refetch } = useQuery({
+    queryFn: () => getTripSpot({ tripSpotId }),
+    queryKey: [QUERY_KEY.TRIP.SPOT, tripSpotId],
+    enabled: !!tripSpotId,
+  });
+
+  return {
+    tripSpot: data,
+    isLoading,
+    isError,
+    isSuccess,
+    refetch,
+  };
+};
+
+export default useTripSpotQuery;
