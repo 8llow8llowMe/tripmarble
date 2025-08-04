a42a9845aeb3f8aa65c6647d0e62fa6370a14acf
신동근 <tlsehdrms124@naver.com>
2025-08-04 17:37:55 +0900

    [BE] fix: 게임 상태를 나타내는 ENUM 클래스 관련 필드 설명 수정

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/domain/model/enums/Status.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/domain/model/enums/Status.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Status {
-    WAITING("시작 전"),
-    ONGOING("진행 중"),
-    ENDED("종료됨");
+    WAITING("게임 시작 전"),
+    ONGOING("게임 진행 중"),
+    ENDED("게임 종료됨");
 
     private final String description;
 }
