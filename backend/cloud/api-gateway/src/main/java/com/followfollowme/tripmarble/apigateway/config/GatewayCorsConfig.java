52bbf5f586aba5050a21348669f5005400a11278
신동근 <tlsehdrms124@naver.com>
2025-08-04 13:39:16 +0900

    [BE] feat: API Gateway CORS 설정 개발서버 관련 추가

--- backend/cloud/api-gateway/src/main/java/com/followfollowme/tripmarble/apigateway/config/GatewayCorsConfig.java
+++ backend/cloud/api-gateway/src/main/java/com/followfollowme/tripmarble/apigateway/config/GatewayCorsConfig.java
@@ -16,7 +16,9 @@ public class GatewayCorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of(
             "http://localhost:5173", // 프론트엔드 개발자가 로컬에서 실행한 Next.js 앱에서 백엔드로 API 요청할 수 있도록 허용
-            "http://localhost:8081", // 프론트엔드 개발자가 로컬에서 실행한 React-Native 앱에서 백엔드로 API 요청할 수 있도록 허용 
+            "http://localhost:8081", // 프론트엔드 개발자가 로컬에서 실행한 React-Native 앱에서 백엔드로 API 요청할 수 있도록 허용
+            // 개발서버 API Gateway에서 Swagger UI를 통해 각 마이크로서비스로 API 호출 시 CORS 허용 (Swagger Aggregation 패턴)
+            "http://www.tripmarble-dev.store:*",
             "https://www.tripmarble.com" // 운영 배포된 프론트가 실제 API 서버로 요청할 때 CORS 허용
         ));
 
