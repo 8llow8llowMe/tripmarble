2a45de2b529032b21f6878fbb17a0396b490c9ea
eestingz <tmdgus1761@gmail.com>
2025-08-04 18:03:19 +0900

    [FE/mobile] feat: 여행지 관련 api 엔드포인트 추가

--- frontend/mobile/src/constants/apis.ts
+++ frontend/mobile/src/constants/apis.ts
@@ -10,6 +10,26 @@ const END_POINTS = {
   USER: {
     INFO: `/members/me`,
   },
+
+  //TRIP
+  TRIP: {
+    // 여행지 정보
+    SPOT: (tripSpotId: number) => `/trip-spots/${tripSpotId}`,
+    LIST_SPOTS: (representativeRegionId: number) =>
+      `/trip-spots/by-representative-region/${representativeRegionId}`,
+
+    // 지역
+    LIST_REGIONS: '/regions',
+    LIST_SIGUNGUS_BY_REGION: (regionId: number) => `/regions/${regionId}/sigungus`,
+
+    // 대표 여행지
+    LIST_REPRESENTATIVE_REGIONS: '/regions/representative',
+    REPRESENTATIVE_REGION: (representativeRegionId: number) =>
+      `/regions/representative/${representativeRegionId}`,
+
+    // 여행 콘텐츠 타입
+    LIST_CONTENT_TYPES: '/trip-content-types',
+  },
 };
 
 export { END_POINTS };
