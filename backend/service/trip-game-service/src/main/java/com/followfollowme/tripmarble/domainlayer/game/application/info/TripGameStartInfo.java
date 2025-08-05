2ca170874fa2bf133704ce34d70787da4c96e7ae
신동근 <tlsehdrms124@naver.com>
2025-08-05 17:48:50 +0900

    [BE] fix: 여행 게임 시작 Info DTO에서 사용하는 클래스명 변경에 따른 수정 및 매핑 관련 메서드 수정

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/info/TripGameStartInfo.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/info/TripGameStartInfo.java
@@ -1,17 +1,16 @@
 package com.followfollowme.tripmarble.domainlayer.game.application.info;
 
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
-import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameMember;
 import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record TripGameStartInfo(
     TripGame tripGame,
-    List<TripGameMember> members
+    List<TripGameMemberWithProfileInfo> members
 ) {
 
-    public static TripGameStartInfo of(TripGame tripGame, List<TripGameMember> members) {
+    public static TripGameStartInfo of(TripGame tripGame, List<TripGameMemberWithProfileInfo> members) {
         return TripGameStartInfo.builder()
             .tripGame(tripGame)
             .members(members)
