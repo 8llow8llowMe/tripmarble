d32b622c5ad8be75f4ac8ac0ce7c5728f41883a7
신동근 <tlsehdrms124@naver.com>
2025-08-05 17:51:41 +0900

    [BE] feat: 여행 게임 시작 비즈니스 로직 정의 (메인 서비스단)

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/in/TripGameWebUseCase.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/in/TripGameWebUseCase.java
@@ -2,8 +2,8 @@ package com.followfollowme.tripmarble.domainlayer.game.application.port.in;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.DifficultyResponse;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameCreateResponse;
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameStartResponse;
 import com.followfollowme.tripmarble.domainlayer.game.application.command.TripGameCreateCommand;
-
 import java.util.List;
 
 public interface TripGameWebUseCase {
@@ -11,4 +11,6 @@ public interface TripGameWebUseCase {
     List<DifficultyResponse> getAllDifficulties();
 
     TripGameCreateResponse crateTripGame(TripGameCreateCommand command);
+
+    TripGameStartResponse startTripGame(long tripGameId, long hostMemberId);
 }
--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameFacade.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameFacade.java
@@ -2,11 +2,14 @@ package com.followfollowme.tripmarble.domainlayer.game.application.service;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.DifficultyResponse;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameCreateResponse;
-import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameInfo;
-import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameTileInfo;
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameStartResponse;
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameTileView;
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameView;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.presenter.TripGameCreatePresenter;
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.presenter.TripGameStartPresenter;
 import com.followfollowme.tripmarble.domainlayer.game.application.command.TripGameCreateCommand;
 import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameCreateInfo;
+import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameStartInfo;
 import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameTileCreateInfo;
 import com.followfollowme.tripmarble.domainlayer.game.application.port.in.TripGameWebUseCase;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Difficulty;
@@ -23,7 +26,9 @@ public class TripGameFacade implements TripGameWebUseCase {
 
     private final TripGameCreateProcessor tripGameCreateProcessor;
     private final TripGameTileCreateProcessor tripGameTileCreateProcessor;
+    private final TripGameStartProcessor tripGameStartProcessor;
     private final TripGameCreatePresenter tripGameCreatePresenter;
+    private final TripGameStartPresenter tripGameStartPresenter;
 
     @Override
     public List<DifficultyResponse> getAllDifficulties() {
@@ -39,7 +44,7 @@ public class TripGameFacade implements TripGameWebUseCase {
     @Transactional
     public TripGameCreateResponse crateTripGame(TripGameCreateCommand command) {
         // 1. 여행 게임(계획) 및 관련 엔티티 생성
-        TripGameCreateInfo tripGameCreateInfo = tripGameCreateProcessor.createTripGame(command);
+        TripGameCreateInfo tripGameCreateInfo = tripGameCreateProcessor.createGame(command);
 
         // 2. 게임 내 블록(말판) 생성
         TripGameTileCreateInfo tripGameTileCreateInfo = tripGameTileCreateProcessor.createTilesForGame(
@@ -48,11 +53,21 @@ public class TripGameFacade implements TripGameWebUseCase {
             tripGameCreateInfo.tripGame().difficulty()
         );
 
-        // 3. 도메인 객체들을 응답 전용 DTO로 매핑 (TripGameInfo, TripGameTileInfo)
-        TripGameInfo gameInfo = tripGameCreatePresenter.toGameInfo(tripGameCreateInfo);
-        List<TripGameTileInfo> tileInfos = tripGameCreatePresenter.toTileInfos(tripGameTileCreateInfo);
+        // 3. 도메인 객체들을 응답 전용 DTO로 매핑 (TripGameView, TripGameTileView)
+        TripGameView gameInfo = tripGameCreatePresenter.toGameInfo(tripGameCreateInfo);
+        List<TripGameTileView> tileInfos = tripGameCreatePresenter.toTileInfos(tripGameTileCreateInfo);
 
         // 4. 최종 응답 dto 조립 및 반환
         return tripGameCreatePresenter.toCreateResponseFrom(gameInfo, tileInfos);
+    }
+
+    @Override
+    @Transactional
+    public TripGameStartResponse startTripGame(long tripGameId, long hostMemberId) {
+        // 1. 게임 시작 처리 (게임 상태 변경 + 턴 순서 지정 + 멤버 정보 포함)
+        TripGameStartInfo tripGameStartInfo = tripGameStartProcessor.startGame(tripGameId, hostMemberId);
+
+        // 2. 응답용 DTO로 변환
+        return tripGameStartPresenter.toStartResponse(tripGameStartInfo);
     }
 }
