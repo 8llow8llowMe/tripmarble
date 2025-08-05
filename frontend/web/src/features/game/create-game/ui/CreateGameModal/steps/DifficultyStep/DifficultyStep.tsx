96bae9e4e48e039c354348dc34cde06e37814f79
seonghoho <chltjdgh3@naver.com>
2025-08-05 20:31:28 +0900

    [FE/web] fix: 게임 생성 난이도 부분 수정

--- frontend/web/src/features/game/create-game/ui/CreateGameModal/steps/DifficultyStep/DifficultyStep.tsx
+++ frontend/web/src/features/game/create-game/ui/CreateGameModal/steps/DifficultyStep/DifficultyStep.tsx
@@ -1,3 +1,4 @@
+import useTripGameDifficulties from "@/entities/games/hooks/useTripGameDifficulties";
 import styles from "./DifficultyStep.module.scss";
 
 interface DifficultyStepProps {
@@ -5,23 +6,35 @@ interface DifficultyStepProps {
   onChange: (value: string) => void;
 }
 
-const difficulties = ["EASY", "NORMAL", "HARD"];
+interface DifficultiesType {
+  code: string;
+  description: string;
+}
+
 export default function DifficultyStep({
   value,
   onChange,
 }: DifficultyStepProps) {
+  const { data } = useTripGameDifficulties();
+  const dummyData = [
+    { code: "EASY", description: "쉬움" },
+    { code: "NORMAL", description: "보통" },
+    { code: "HARD", description: "어려움" },
+  ];
+  const difficulties: DifficultiesType[] = data?.data?.dataBody || dummyData;
+
   return (
     <div className={styles.difficultyGroup}>
       {difficulties.map((diff) => (
         <button
-          key={diff}
+          key={diff.code}
           type="button"
           className={`${styles.button} ${
-            value === diff ? styles.selected : ""
+            value === diff.code ? styles.selected : ""
           }`}
-          onClick={() => onChange(diff)}
+          onClick={() => onChange(diff.code)}
         >
-          {diff}
+          {diff.description}
         </button>
       ))}
     </div>
