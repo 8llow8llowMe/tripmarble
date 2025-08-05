4fbec4b25912a083f5397a6fddf73ce951545cfc
신동근 <tlsehdrms124@naver.com>
2025-08-05 13:10:17 +0900

    [BE] feat: 여행 게임 (계획) 시작 및 종료 메서드 정의

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/domain/model/TripGame.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/domain/model/TripGame.java
@@ -2,9 +2,8 @@ package com.followfollowme.tripmarble.domainlayer.game.domain.model;
 
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Difficulty;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Status;
-import lombok.Builder;
-
 import java.time.LocalDate;
+import lombok.Builder;
 
 @Builder
 public record TripGame(
@@ -17,4 +16,30 @@ public record TripGame(
     long representativeRegionId
 ) {
 
+    public TripGame start() {
+        if (this.status != Status.WAITING) {
+            throw new IllegalStateException("게임은 대기 상태에서만 시작할 수 있습니다.");
+        }
+        return TripGame.builder()
+            .id(this.id)
+            .title(this.title)
+            .status(Status.ONGOING)
+            .difficulty(this.difficulty)
+            .startedAt(LocalDate.now())
+            .endedAt(null)
+            .representativeRegionId(this.representativeRegionId)
+            .build();
+    }
+
+    public TripGame end() {
+        return TripGame.builder()
+            .id(this.id)
+            .title(this.title)
+            .status(Status.ENDED)
+            .difficulty(this.difficulty)
+            .startedAt(this.startedAt)
+            .endedAt(LocalDate.now())
+            .representativeRegionId(this.representativeRegionId)
+            .build();
+    }
 }
