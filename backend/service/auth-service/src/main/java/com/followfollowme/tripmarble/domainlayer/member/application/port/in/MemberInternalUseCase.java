33fd1ebd7f2e3df59fecc26c06d9d76cf3ca3745
신동근 <tlsehdrms124@naver.com>
2025-08-05 16:50:47 +0900

    [BE] feat: 회원 프로필 목록 조회 비즈니스 로직 개발

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/port/in/MemberInternalUseCase.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/port/in/MemberInternalUseCase.java
@@ -1,5 +1,9 @@
 package com.followfollowme.tripmarble.domainlayer.member.application.port.in;
 
+import com.followfollowme.tripmarble.domainlayer.member.adapter.in.internal.dto.MemberProfileResponse;
+import java.util.List;
+
 public interface MemberInternalUseCase {
 
+    List<MemberProfileResponse> getMemberProfiles(List<Long> memberIds);
 }
