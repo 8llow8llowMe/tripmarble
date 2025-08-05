c84c7b747036ff7a1d9974a7e270f0b8c15253f3
신동근 <tlsehdrms124@naver.com>
2025-08-05 16:41:15 +0900

    [BE] feat: Mapper 관련 엔티티 리스트 -> 도메인 리스트 매핑 정의

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/mapper/MemberMapper.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/mapper/MemberMapper.java
@@ -3,6 +3,7 @@ package com.followfollowme.tripmarble.domainlayer.member.application.mapper;
 import com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.dto.MemberMyInfoResponse;
 import com.followfollowme.tripmarble.domainlayer.member.adapter.out.persistence.entity.MemberEntity;
 import com.followfollowme.tripmarble.domainlayer.member.domain.model.Member;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -11,6 +12,9 @@ public interface MemberMapper {
 
     // 엔티티 -> 도메인
     Member toDomainFromEntity(MemberEntity entity);
+
+    // 엔티티 리스트 -> 도메인 리스트
+    List<Member> toDomainListFromEntityList(List<MemberEntity> entities);
 
     // 도메인 -> 엔티티
     MemberEntity toEntityFromDomain(Member domain);
