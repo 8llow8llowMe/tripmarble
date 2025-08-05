f16832f6c5cf9a8e6d2c49dd5fd175db6119ecd9
신동근 <tlsehdrms124@naver.com>
2025-08-05 17:51:55 +0900

    [BE] feat: 여행 게임 시작 API 개발

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/in/web/controller/TripGameWebController.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/in/web/controller/TripGameWebController.java
@@ -4,23 +4,24 @@ import com.followfollowme.tripmarble.common.dto.Response;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.DifficultyResponse;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameCreateRequest;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameCreateResponse;
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameStartResponse;
 import com.followfollowme.tripmarble.domainlayer.game.application.command.TripGameCreateCommand;
 import com.followfollowme.tripmarble.domainlayer.game.application.port.in.TripGameWebUseCase;
 import com.followfollowme.tripmarble.security.common.dto.MemberLoginActive;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -51,6 +52,18 @@ public class TripGameWebController {
         @Valid @RequestBody TripGameCreateRequest request, @AuthenticationPrincipal MemberLoginActive loginActive) {
         TripGameCreateResponse response = tripGameWebUseCase.crateTripGame(
             TripGameCreateCommand.from(request, loginActive.id()));
+        return ResponseEntity.ok().body(Response.success(response));
+    }
+
+    @Operation(
+        summary = "여행 게임 시작",
+        description = "게임 방장이 모든 참여자가 준비된 상태에서 게임을 시작하는 기능입니다."
+    )
+    @PostMapping("/{tripGameId}/start")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Response<TripGameStartResponse>> startTripGame(
+        @PathVariable long tripGameId, @AuthenticationPrincipal MemberLoginActive loginActive) {
+        TripGameStartResponse response = tripGameWebUseCase.startTripGame(tripGameId, loginActive.id());
         return ResponseEntity.ok().body(Response.success(response));
     }
 }
