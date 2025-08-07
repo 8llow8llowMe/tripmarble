7c4eee680e3ef534480435eab514311737b00015
신동근 <tlsehdrms124@naver.com>
2025-08-07 14:03:19 +0900

    [BE] fix: FeignClient 각 서비스별 default 명칭 정의

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/client/TripContentTypeClient.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/client/TripContentTypeClient.java
@@ -1,14 +1,13 @@
 package com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.client;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.dto.TripContentTypeInternalResponse;
+import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @FeignClient(
-    name = "${feign-client.target-services.trip-service}",
+    name = "${feign-client.target-services.trip-service:trip-service}",
     contextId = "tripContentTypeClient",
     path = "/internal/v1/trip-content-types"
 )
--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/client/TripSpotClient.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/client/TripSpotClient.java
@@ -1,15 +1,14 @@
 package com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.client;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.dto.TripSpotRandomResponse;
+import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @FeignClient(
-    name = "${feign-client.target-services.trip-service}",
+    name = "${feign-client.target-services.trip-service:trip-service}",
     contextId = "tripSpotClient",
     path = "/internal/v1/trip-spots"
 )
@@ -17,5 +16,6 @@ public interface TripSpotClient {
 
     @GetMapping("/by-representative-region/{representativeRegionId}/random")
     List<TripSpotRandomResponse> getRandomTripSpots(
-        @PathVariable long representativeRegionId, @RequestParam List<Integer> contentTypeIds, @RequestParam(defaultValue = "10") int limit);
+        @PathVariable long representativeRegionId, @RequestParam List<Integer> contentTypeIds,
+        @RequestParam(defaultValue = "10") int limit);
 }
