a8066d1503efbded0e9a5dc8ca94e0f9d0286966
신동근 <tlsehdrms124@naver.com>
2025-08-08 17:54:21 +0900

    [BE] feat: 나의 여행 게임 목록 조회 기능 관련 초기 스켈레톤 코드 작성

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/in/TripGameWebUseCase.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/in/TripGameWebUseCase.java
@@ -1,9 +1,12 @@
 package com.followfollowme.tripmarble.domainlayer.game.application.port.in;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.DifficultyResponse;
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.MyTripGameCardView;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameCreateResponse;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameStartResponse;
 import com.followfollowme.tripmarble.domainlayer.game.application.command.TripGameCreateCommand;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Status;
+import com.followfollowme.tripmarble.persistence.dto.SliceResponse;
 import java.util.List;
 
 public interface TripGameWebUseCase {
@@ -11,6 +14,8 @@ public interface TripGameWebUseCase {
     List<DifficultyResponse> getAllDifficulties();
 
     TripGameCreateResponse crateTripGame(TripGameCreateCommand command);
+
+    SliceResponse<MyTripGameCardView> getMyTripGames(long memberId, long lastTripGameId, int size, Status status);
 
     TripGameStartResponse startTripGame(long tripGameId, long hostMemberId);
 }
