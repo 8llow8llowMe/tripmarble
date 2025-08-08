dd3da56a79e492ef0ef1f3cfff855bcf26e37509
신동근 <tlsehdrms124@naver.com>
2025-08-08 17:50:33 +0900

    [BE] feat: 여행 게임 Repositorty 커스텀 Repository 추가 상속

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/persistence/repository/TripGameRepository.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/persistence/repository/TripGameRepository.java
@@ -1,8 +1,9 @@
 package com.followfollowme.tripmarble.domainlayer.game.adapter.out.persistence.repository;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.out.persistence.entity.TripGameEntity;
+import com.followfollowme.tripmarble.domainlayer.game.adapter.out.persistence.repository.custom.TripGameCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TripGameRepository extends JpaRepository<TripGameEntity, Long> {
+public interface TripGameRepository extends JpaRepository<TripGameEntity, Long>, TripGameCustomRepository {
 
 }
