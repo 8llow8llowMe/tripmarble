40454bc232c691e3f3f419d019fe5a301bbd2e89
신동근 <tlsehdrms124@naver.com>
2025-08-08 17:53:44 +0900

    [BE] feat: 여행 게임 목록 조회 no offset 방식 조회 기능 개발 (Adapter, Port)

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/persistence/TripGameRepositoryAdapter.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/persistence/TripGameRepositoryAdapter.java
@@ -5,8 +5,10 @@ import com.followfollowme.tripmarble.domainlayer.game.adapter.out.persistence.re
 import com.followfollowme.tripmarble.domainlayer.game.application.mapper.TripGameMapper;
 import com.followfollowme.tripmarble.domainlayer.game.application.port.out.TripGameRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Status;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -27,5 +29,11 @@ public class TripGameRepositoryAdapter implements TripGameRepositoryPort {
     public Optional<TripGame> findById(long tripGameId) {
         return tripGameRepository.findById(tripGameId)
             .map(tripGameMapper::toDomainFromEntity);
+    }
+
+    @Override
+    public Slice<TripGame> findMyGameNoOffset(long memberId, long lastTripGameId, int size, Status status) {
+        Slice<TripGameEntity> entitySlice = tripGameRepository.findMyGamesNoOffset(memberId, lastTripGameId, size, status);
+        return entitySlice.map(tripGameMapper::toDomainFromEntity);
     }
 }
--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/out/TripGameRepositoryPort.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/out/TripGameRepositoryPort.java
@@ -1,11 +1,15 @@
 package com.followfollowme.tripmarble.domainlayer.game.application.port.out;
 
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Status;
 import java.util.Optional;
+import org.springframework.data.domain.Slice;
 
 public interface TripGameRepositoryPort {
 
     TripGame save(TripGame tripGame);
 
     Optional<TripGame> findById(long tripGameId);
+
+    Slice<TripGame> findMyGameNoOffset(long memberId, long lastTripGameId, int size, Status status);
 }
