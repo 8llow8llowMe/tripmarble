9e27e3122aaecf309c873fe5c808b97e3957a539
신동근 <tlsehdrms124@naver.com>
2025-08-08 17:18:58 +0900

    [BE] feat: 여행 게임 서비스 인프라 설정 QueryDSL 설정 추가

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/global/config/TripGameServiceInfraConfig.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/global/config/TripGameServiceInfraConfig.java
@@ -2,6 +2,7 @@ package com.followfollowme.tripmarble.global.config;
 
 import com.followfollowme.tripmarble.common.config.JasyptConfig;
 import com.followfollowme.tripmarble.persistence.config.JpaAuditConfig;
+import com.followfollowme.tripmarble.persistence.config.QuerydslConfig;
 import com.followfollowme.tripmarble.persistence.config.SnowflakeConfig;
 import com.followfollowme.tripmarble.security.resourceserver.config.ResourceServerSecurityConfig;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,8 @@ import org.springframework.context.annotation.Import;
     JasyptConfig.class,
     JpaAuditConfig.class,
     SnowflakeConfig.class,
-    ResourceServerSecurityConfig.class
+    ResourceServerSecurityConfig.class,
+    QuerydslConfig.class
 })
 public class TripGameServiceInfraConfig {
 
