815f16bb68d205902f1d4a4de9b61dd55ceaeda9
seonghoho <chltjdgh3@naver.com>
2025-08-12 23:36:41 +0900

    [FE/web] fix: dotenv 구조 변경

--- frontend/web/next.config.mjs
+++ frontend/web/next.config.mjs
@@ -1,23 +1,25 @@
+import path from "node:path";
 import dotenv from "dotenv";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const envPath =
   process.env.NODE_ENV === "development"
-    ? "./env/.env-frontend-dev"
-    : "./env/.env-frontend-prod";
+    ? path.resolve(__dirname, "./env/.env-frontend-dev")
+    : path.resolve(__dirname, "./env/.env-frontend-prod");
 
-dotenv.config({ path: envPath });
+dotenv.config({ path: envPath, override: true });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: Object.keys(process.env)
-    .filter((key) => key.startsWith("NEXT_PUBLIC_"))
-    .reduce((env, key) => {
-      env[key] = process.env[key];
-      return env;
-    }, {}),
-  images: {
-    domains: ["tong.visitkorea.or.kr", "k.kakaocdn.net"],
-  },
+  env: Object.fromEntries(
+    Object.keys(process.env)
+      .filter((k) => k.startsWith("NEXT_PUBLIC_"))
+      .map((k) => [k, process.env[k]])
+  ),
+  images: { domains: ["tong.visitkorea.or.kr", "k.kakaocdn.net"] },
 };
 
 export default nextConfig;
