fec23985438a1fbd7cb2db599b10e966b9b8a263
신동근 <tlsehdrms124@naver.com>
2025-08-12 23:50:44 +0900

    [BE] feat: 게임 타일 관련 미션 타입 ENUM값 정의

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/domain/model/enums/MissionType.java
@@ -0,0 +1,14 @@
+package com.followfollowme.tripmarble.domainlayer.game.domain.model.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MissionType {
+    PHOTO("사진 인증"),
+    REVIEW("후기 작성"),
+    CHECKIN_GPS("위치 인증");
+    
+    private final String description;
+}
