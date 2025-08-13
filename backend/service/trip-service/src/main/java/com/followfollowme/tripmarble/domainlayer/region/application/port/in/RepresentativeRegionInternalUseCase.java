edf32f79dd55f7b93e51d594e24884ac2e2e066c
신동근 <tlsehdrms124@naver.com>
2025-08-13 17:46:19 +0900

    [BE] feat: 여러 대표 여행지 정보 조회 기능 개발

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/in/RepresentativeRegionInternalUseCase.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/in/RepresentativeRegionInternalUseCase.java
@@ -1,8 +1,11 @@
 package com.followfollowme.tripmarble.domainlayer.region.application.port.in;
 
 import com.followfollowme.tripmarble.domainlayer.region.adapter.in.internal.dto.RepresentativeRegionInfoInternalResponse;
+import java.util.List;
 
 public interface RepresentativeRegionInternalUseCase {
 
     RepresentativeRegionInfoInternalResponse getRepresentativeRegionInfo(long representativeRegionId);
+
+    List<RepresentativeRegionInfoInternalResponse> getRepresentativeRegionsByIds(List<Long> representativeRegionIds);
 }
