4938cd484d40f30058ba6768f6b5458be1559ee9
신동근 <tlsehdrms124@naver.com>
2025-08-13 17:47:58 +0900

    [BE] feat: 내부 서비스 통신 전용 대표 여행지 관련 응답 DTO 정의

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/dto/RepresentativeRegionInfoInternalResponse.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/dto/RepresentativeRegionInfoInternalResponse.java
@@ -2,7 +2,8 @@ package com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.dto;
 
 public record RepresentativeRegionInfoInternalResponse(
     long representativeRegionId,
-    String representativeRegionName
+    String representativeRegionName,
+    String imageUrl
 ) {
 
 }
