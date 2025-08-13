30f83b516ae894ddf17ec8c852a64a0809750132
신동근 <tlsehdrms124@naver.com>
2025-08-13 17:46:33 +0900

    [BE] feat: 여러 대표 여행지 정보 조회 API 개발

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/in/internal/controller/RepresentativeRegionInternalController.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/adapter/in/internal/controller/RepresentativeRegionInternalController.java
@@ -5,10 +5,13 @@ import com.followfollowme.tripmarble.domainlayer.region.application.port.in.Repr
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,7 +28,21 @@ public class RepresentativeRegionInternalController {
         description = "대표 여행지 정보를 조회합니다."
     )
     @GetMapping("/{representativeRegionId}")
-    public RepresentativeRegionInfoInternalResponse getRepresentativeRegionInfo(@PathVariable long representativeRegionId) {
-        return representativeRegionInternalUseCase.getRepresentativeRegionInfo(representativeRegionId);
+    public ResponseEntity<RepresentativeRegionInfoInternalResponse> getRepresentativeRegionInfo(@PathVariable long representativeRegionId) {
+        RepresentativeRegionInfoInternalResponse response = representativeRegionInternalUseCase.getRepresentativeRegionInfo(
+            representativeRegionId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @Operation(
+        summary = "여러 대표 여행지 정보 조회",
+        description = "대표 여행지 ID 리스트를 기반으로 여러 정보를 조회합니다."
+    )
+    @GetMapping
+    public ResponseEntity<List<RepresentativeRegionInfoInternalResponse>> getRepresentativeRegionsByIds(
+        @RequestParam List<Long> representativeRegionIds) {
+        List<RepresentativeRegionInfoInternalResponse> responses = representativeRegionInternalUseCase.getRepresentativeRegionsByIds(
+            representativeRegionIds);
+        return ResponseEntity.ok().body(responses);
     }
 }
