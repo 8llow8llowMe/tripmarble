ba748cc2ec409a08f87a2f690d111e38f28e6064
seonghoho <seonghoho@codnut.com>
2025-08-15 23:49:37 +0900

    [FE/web] fix: 나의 게임 목록 props data 수정

--- frontend/web/src/widgets/my-games-horizontal/MyGamesHorizontal.tsx
+++ frontend/web/src/widgets/my-games-horizontal/MyGamesHorizontal.tsx
@@ -3,43 +3,55 @@ import Link from "next/link";
 import styles from "./MyGamesHorizontal.module.scss";
 
 export type MyGame = {
-  id: string;
+  tripGameId: number;
+  gameStatus: "WAITING" | "ONGOING" | "ENDED";
+  gameStatusDescription: "게임 시작 전" | "게임 진행 중" | "게임 종료";
+  difficultyCode: "EASY" | "NORMAL" | "HARD";
+  difficultyDescription: "쉬움" | "보통" | "어려움";
+  representativeRegionImageUrl: string | null;
+  representativeRegionName: string;
   title: string;
-  status: "playing" | "ended";
-  imageUrl?: string;
-  date: string;
+  startedAt: string;
+  endedAt: string;
+  tripThemeNames: string[];
+  isHost: boolean;
+  isReady: boolean;
 };
 
 export function MyGamesHorizontal({
   games,
-  status,
+  gameStatus,
 }: {
-  games: MyGame[];
-  status: "playing" | "ended";
+  games: MyGame[] | undefined;
+  gameStatus: "WAITING" | "ONGOING" | "ENDED";
 }) {
   return (
     <>
       <div className={styles.sectionTitle}>
-        {status === "playing" ? "진행중인 내 게임" : "진행 종료된 내 게임"}
+        {gameStatus !== "ENDED" ? "진행중인 내 게임" : "진행 종료된 내 게임"}
       </div>
       <div className={styles.horizontalList}>
-        {games
-          .filter((g) => g.status === status)
-          .map((game) => (
-            <div className={styles.hCard} key={game.id}>
-              <Link key={game.id} href={`/game/${game.id}`}>
-                <Image
-                  src={game.imageUrl || "/images/no-image.png"}
-                  alt="게임 이미지"
-                  width={220}
-                  height={200}
-                  style={{ borderRadius: "8px" }}
-                />
-                <div className={styles.hCardTitle}>{game.title}</div>
-                <div className={styles.hCardDate}>{game.date}</div>
-              </Link>
-            </div>
-          ))}
+        {games &&
+          games
+            // .filter((g) => g.gameStatus === gameStatus)
+            .map((game) => (
+              <div className={styles.hCard} key={game.tripGameId}>
+                <Link key={game.tripGameId} href={`/game/${game.tripGameId}`}>
+                  <Image
+                    src={
+                      game.representativeRegionImageUrl ||
+                      "/images/no-image.png"
+                    }
+                    alt="게임 이미지"
+                    width={220}
+                    height={200}
+                    style={{ borderRadius: "8px" }}
+                  />
+                  <div className={styles.hCardTitle}>{game.title}</div>
+                  <div className={styles.hCardDate}>{game.startedAt}</div>
+                </Link>
+              </div>
+            ))}
       </div>
     </>
   );
