c5a6e0450b42fca2823642404082443504474c5a
eestingz <tmdgus1761@gmail.com>
2025-08-21 01:17:29 +0900

    [FE/mobile] feat: game-play stack navigator 스크린 추가

--- frontend/mobile/src/navigations/app/GamePlayStackNavigator.tsx
+++ frontend/mobile/src/navigations/app/GamePlayStackNavigator.tsx
@@ -1,4 +1,6 @@
-import GameOverviewScreen from '@/screens/Game/GameOverviewScreen';
+import GameEndedScreen from '@/screens/Game/GameEndedScreen';
+import GameListScreen from '@/screens/Game/GameListScreen';
+import GameMissionAuthScreen from '@/screens/Game/GameMissionAuthScreen';
 import OngoingGameScreen from '@/screens/Game/OngoingGameScreen';
 import { GamePlayStackParamList } from '@/types/navigation/navigation';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -9,13 +11,23 @@ export default function GamePlayStackNavigator() {
   return (
     <GamePlayStack.Navigator>
       <GamePlayStack.Screen
-        name="GameOverviewScreen"
-        component={GameOverviewScreen}
+        name="GameEndedScreen"
+        component={GameEndedScreen}
         options={{ headerShown: false }}
       />
       <GamePlayStack.Screen
         name="OngoingGameScreen"
         component={OngoingGameScreen}
+        options={{ headerShown: false }}
+      />
+      <GamePlayStack.Screen
+        name="GameListScreen"
+        component={GameListScreen}
+        options={{ headerShown: false }}
+      />
+      <GamePlayStack.Screen
+        name="GameMissionAuthScreen"
+        component={GameMissionAuthScreen}
         options={{ headerShown: false }}
       />
     </GamePlayStack.Navigator>
