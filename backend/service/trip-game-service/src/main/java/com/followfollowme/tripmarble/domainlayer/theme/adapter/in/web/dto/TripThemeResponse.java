13ae78c8760802c71d7ef68730f7304adcfc28f9
신동근 <tlsehdrms124@naver.com>
2025-08-25 12:58:17 +0900

    [BE] fix: 여행 테마 조회 응답 DTO 테마 ID 부분 String으로 타입 변경

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/adapter/in/web/dto/TripThemeResponse.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/theme/adapter/in/web/dto/TripThemeResponse.java
@@ -1,10 +1,16 @@
 package com.followfollowme.tripmarble.domainlayer.theme.adapter.in.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "여행 테마 조회 응답 DTO")
 public record TripThemeResponse(
-    long tripThemeId,
+
+    @Schema(description = "여행 테마 ID", example = "1")
+    String tripThemeId,
+
+    @Schema(description = "여행 테마 이름", example = "관광")
     String tripThemeName
 ) {
 
