299d3305f958f366646514b906ac312099e59d35
seonghoho <chltjdgh3@naver.com>
2025-08-25 23:08:09 +0900

    [FE/web] fix: 게임 목록 컴포넌트 수정

--- frontend/web/src/widgets/my-games-horizontal/MyGamesHorizontal.tsx
+++ frontend/web/src/widgets/my-games-horizontal/MyGamesHorizontal.tsx
@@ -1,58 +1,60 @@
 import Image from "next/image";
-import Link from "next/link";
 import styles from "./MyGamesHorizontal.module.scss";
+import useMyGameList, {
+  GameStatus,
+} from "@/entities/games/hooks/useMyGameList";
+import { useRouter } from "next/navigation";
+import { fetchGameStart } from "@/entities/games/hooks/useGameStart";
 
-export type MyGame = {
-  tripGameId: number;
-  gameStatus: "WAITING" | "ONGOING" | "ENDED";
-  gameStatusDescription: "게임 시작 전" | "게임 진행 중" | "게임 종료";
-  difficultyCode: "EASY" | "NORMAL" | "HARD";
-  difficultyDescription: "쉬움" | "보통" | "어려움";
-  representativeRegionImageUrl: string | null;
-  representativeRegionName: string;
-  title: string;
-  startedAt: string;
-  endedAt: string;
-  tripThemeNames: string[];
-  isHost: boolean;
-  isReady: boolean;
-};
+export function MyGamesHorizontal({ type }: { type: GameStatus }) {
+  const router = useRouter();
+  const { data } = useMyGameList({ status: type });
+  const games = data?.data.dataBody.contents;
 
-export function MyGamesHorizontal({
-  games,
-  gameStatus,
-}: {
-  games: MyGame[] | undefined;
-  gameStatus: "WAITING" | "ONGOING" | "ENDED";
-}) {
+  const handleClick = async (gameId: string) => {
+    if (type === "WAITING") {
+      await fetchGameStart(gameId);
+    }
+    console.log(gameId);
+    router.push(`/game/${gameId}`);
+  };
+
   return (
     <>
-      <div className={styles.sectionTitle}>
-        {gameStatus !== "ENDED" ? "진행중인 내 게임" : "진행 종료된 내 게임"}
-      </div>
-      <div className={styles.horizontalList}>
-        {games &&
-          games
-            // .filter((g) => g.gameStatus === gameStatus)
-            .map((game) => (
-              <div className={styles.hCard} key={game.tripGameId}>
-                <Link key={game.tripGameId} href={`/game/${game.tripGameId}`}>
-                  <Image
-                    src={
-                      game.representativeRegionImageUrl ||
-                      "/images/no-image.png"
-                    }
-                    alt="게임 이미지"
-                    width={220}
-                    height={200}
-                    style={{ borderRadius: "8px" }}
-                  />
-                  <div className={styles.hCardTitle}>{game.title}</div>
-                  <div className={styles.hCardDate}>{game.startedAt}</div>
-                </Link>
+      {games && games.length > 0 ? (
+        <>
+          <div className={styles.sectionTitle}>
+            {type === "WAITING"
+              ? "시작 전 게임"
+              : type === "ONGOING"
+              ? "진행중인 내 게임"
+              : "진행 종료된 내 게임"}
+          </div>
+          <div className={styles.horizontalList}>
+            {games.map((game) => (
+              <div
+                className={styles.hCard}
+                key={game.tripGameId}
+                onClick={() => handleClick(game.tripGameId)}
+              >
+                <Image
+                  src={
+                    game.representativeRegionImageUrl || "/images/no-image.png"
+                  }
+                  alt="게임 이미지"
+                  width={220}
+                  height={200}
+                  style={{ borderRadius: "8px" }}
+                />
+                <div className={styles.hCardTitle}>{game.title}</div>
+                <div className={styles.hCardDate}>{game.startedAt}</div>
               </div>
             ))}
-      </div>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
     </>
   );
 }
