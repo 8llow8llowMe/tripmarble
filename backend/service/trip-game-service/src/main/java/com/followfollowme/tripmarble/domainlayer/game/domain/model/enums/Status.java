0eff523d54ca550569c9e6015854afdde9bc7cfc
신동근 <tlsehdrms124@naver.com>
2025-08-25 23:38:26 +0900

    [BE] feat: 게임 상태 관련 추상 메서드 구현 (게임 진행 및 시작, 종료 관련 검증)

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/domain/model/enums/Status.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/domain/model/enums/Status.java
@@ -1,14 +1,67 @@
 package com.followfollowme.tripmarble.domainlayer.game.domain.model.enums;
 
+import com.followfollowme.tripmarble.domainlayer.game.application.exception.TripGameErrorCode;
+import com.followfollowme.tripmarble.domainlayer.game.application.exception.TripGameException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum Status {
-    WAITING("게임 시작 전"),
-    ONGOING("게임 진행 중"),
-    ENDED("게임 종료됨");
+    WAITING("게임 시작 전") {
+        @Override
+        public void validatePlayable() {
+            throw new TripGameException(TripGameErrorCode.GAME_NOT_ONGOING);
+        }
+
+        @Override
+        public void validateStartable() {
+
+        }
+
+        @Override
+        public void validateEndable() {
+            throw new TripGameException(TripGameErrorCode.GAME_NOT_ONGOING);
+        }
+    },
+    ONGOING("게임 진행 중") {
+        @Override
+        public void validatePlayable() {
+
+        }
+
+        @Override
+        public void validateStartable() {
+            throw new TripGameException(TripGameErrorCode.GAME_NOT_WAITING);
+        }
+
+        @Override
+        public void validateEndable() {
+            
+        }
+    },
+    ENDED("게임 종료됨") {
+        @Override
+        public void validatePlayable() {
+            throw new TripGameException(TripGameErrorCode.GAME_ALREADY_ENDED);
+        }
+
+        @Override
+        public void validateStartable() {
+            throw new TripGameException(TripGameErrorCode.GAME_ALREADY_ENDED);
+        }
+
+        @Override
+        public void validateEndable() {
+            throw new TripGameException(TripGameErrorCode.GAME_ALREADY_ENDED);
+        }
+    };
 
     private final String description;
+
+    public abstract void validatePlayable();
+
+    public abstract void validateStartable();
+
+    public abstract void validateEndable();
 }
