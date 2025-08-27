de08ce4543d4e7d0088f7ed5420f61279e078244
신동근 <tlsehdrms124@naver.com>
2025-08-27 17:49:49 +0900

    [BE] feat: 여행 게임 종료 Info DTO 정의

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/info/TripGameEndInfo.java
@@ -0,0 +1,20 @@
+package com.followfollowme.tripmarble.domainlayer.game.application.info;
+
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Status;
+import lombok.Builder;
+
+@Builder
+public record TripGameEndInfo(
+    long tripGameId,
+    Status status
+
+) {
+
+    public static TripGameEndInfo of(TripGame game) {
+        return TripGameEndInfo.builder()
+            .tripGameId(game.id())
+            .status(game.status())
+            .build();
+    }
+}
