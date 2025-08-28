1adfff831e7699f613ef9b3acf93eb787b94a959
신동근 <tlsehdrms124@naver.com>
2025-08-28 17:49:49 +0900

    [BE] feat: 미션 결과 관련 enum validate 메서드 정의 (추상 메서드)

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/domain/model/enums/MissionResult.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/domain/model/enums/MissionResult.java
@@ -1,15 +1,40 @@
 package com.followfollowme.tripmarble.domainlayer.game.domain.model.enums;
 
+import com.followfollowme.tripmarble.domainlayer.game.application.exception.TripGameErrorCode;
+import com.followfollowme.tripmarble.domainlayer.game.application.exception.TripGameException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum MissionResult {
-    SUCCESS("성공"), // 미션 성공
-    SKIPPED("넘어감"), // 미션 건너뜀 (패스)
-    FAILED("실패"), // 미션 실패
-    PENDING("대기중"); // 아직 미션 미수행 (디폴트)
+    SUCCESS("성공") {
+        @Override
+        public void validateChangeable() {
+            throw new TripGameException(TripGameErrorCode.MISSION_ALREADY_PROCESSED);
+        }
+    },
+    SKIPPED("넘어감") {
+        @Override
+        public void validateChangeable() {
+            throw new TripGameException(TripGameErrorCode.MISSION_ALREADY_PROCESSED);
+        }
+    },
+    FAILED("실패") {
+        @Override
+        public void validateChangeable() {
+            throw new TripGameException(TripGameErrorCode.MISSION_ALREADY_PROCESSED);
+        }
+    },
+    PENDING("대기중") {
+        @Override
+        public void validateChangeable() {
+            // 가능 -> 아무것도 안함
+        }
+    };
 
     private final String description;
+
+    // 미션 상태 변경이 가능한지 검증
+    public abstract void validateChangeable();
 }
