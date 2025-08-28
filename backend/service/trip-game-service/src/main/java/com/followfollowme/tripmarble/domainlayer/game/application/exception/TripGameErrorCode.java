2693152e95914fa3b4160a556a71689cafb82e0c
신동근 <tlsehdrms124@naver.com>
2025-08-28 11:21:10 +0900

    [BE] fix: 여행 게임 관련 커스텀 에러코드 방장 관련 코드 메시지 수정

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/exception/TripGameErrorCode.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/exception/TripGameErrorCode.java
@@ -10,7 +10,7 @@ public enum TripGameErrorCode {
 
     GAME_NOT_FOUND("TRIP_GAME_001", "해당 게임 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     HOST_MEMBER_NOT_FOUND("TRIP_GAME_002", "해당 게임에서 방장 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    NOT_HOST_MEMBER("TRIP_GAME_003", "게임 시작 권한이 없습니다. 방장만 게임을 시작할 수 있습니다.", HttpStatus.FORBIDDEN),
+    NOT_HOST_MEMBER("TRIP_GAME_003", "게임 시작/강제 종료 권한이 없습니다. 방장만 게임을 시작/강제 종료 할 수 있습니다.", HttpStatus.FORBIDDEN),
     MEMBER_NOT_READY("TRIP_GAME_004", "모든 플레이어가 준비되어야 게임을 시작할 수 있습니다.", HttpStatus.BAD_REQUEST),
     MEMBER_PROFILE_FETCH_FAILED("TRIP_GAME_005", "회원 프로필 정보를 가져오는데 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     MEMBER_NOT_FOUND("TRIP_GAME_006", "해당 게임에 참여한 회원이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
