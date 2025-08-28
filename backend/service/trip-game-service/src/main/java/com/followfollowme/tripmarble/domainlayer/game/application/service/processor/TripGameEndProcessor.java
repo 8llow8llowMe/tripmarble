7bff5fb871c1ff318d0a9fbaa5f6bc03c4aca3a2
신동근 <tlsehdrms124@naver.com>
2025-08-28 11:30:20 +0900

    [BE] feat: 여행 게임 종료 프로세스 일반 종료 및 강제 종료 기능 정의

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/processor/TripGameEndProcessor.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/processor/TripGameEndProcessor.java
@@ -3,8 +3,10 @@ package com.followfollowme.tripmarble.domainlayer.game.application.service.proce
 import com.followfollowme.tripmarble.domainlayer.game.application.exception.TripGameErrorCode;
 import com.followfollowme.tripmarble.domainlayer.game.application.exception.TripGameException;
 import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameEndInfo;
+import com.followfollowme.tripmarble.domainlayer.game.application.port.out.TripGameMemberRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.game.application.port.out.TripGameRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameMember;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +15,30 @@ import org.springframework.stereotype.Service;
 public class TripGameEndProcessor {
 
     private final TripGameRepositoryPort tripGameRepositoryPort;
+    private final TripGameMemberRepositoryPort tripGameMemberRepositoryPort;
 
-    public TripGameEndInfo endTripGame(long tripGameId) {
+    public TripGameEndInfo normalEndTripGame(long tripGameId) {
         TripGame game = tripGameRepositoryPort.findById(tripGameId)
             .orElseThrow(() -> new TripGameException(TripGameErrorCode.GAME_NOT_FOUND));
 
-        // 게임 종료 -> 도메인에 위임
-        TripGame endedGame = game.end();
+        TripGame endedGame = game.normalEnd();
+
+        tripGameRepositoryPort.save(endedGame);
+
+        return TripGameEndInfo.of(endedGame);
+    }
+
+
+    public TripGameEndInfo forceEndTripGame(long tripGameId, long requesterId) {
+        TripGame game = tripGameRepositoryPort.findById(tripGameId)
+            .orElseThrow(() -> new TripGameException(TripGameErrorCode.GAME_NOT_FOUND));
+
+        // 방장 여부 검증
+        TripGameMember hostMember = tripGameMemberRepositoryPort.findHostMemberInGame(game.id(), requesterId)
+            .orElseThrow(() -> new TripGameException(TripGameErrorCode.NOT_HOST_MEMBER));
+
+        // 도메인에 위임 (강제 종료)
+        TripGame endedGame = game.forceEnd();
 
         tripGameRepositoryPort.save(endedGame);
 
