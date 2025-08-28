07dde23e6bf11869c053d93135a9b549762c46af
신동근 <tlsehdrms124@naver.com>
2025-08-28 17:51:32 +0900

    [BE] feat: 게임 이동 로그 ID로 해당 게임 이동 로그 찾기 메서드 정의 (Repository Adapter, Port)

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/out/TripGameMoveLogRepositoryPort.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/out/TripGameMoveLogRepositoryPort.java
@@ -2,8 +2,11 @@ package com.followfollowme.tripmarble.domainlayer.game.application.port.out;
 
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameMoveLog;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameTile;
+import java.util.Optional;
 
 public interface TripGameMoveLogRepositoryPort {
 
     TripGameMoveLog save(TripGameMoveLog tripGameMoveLog, TripGameTile tripGameTile);
+
+    Optional<TripGameMoveLog> findById(long tripGameMoveLogId);
 }
