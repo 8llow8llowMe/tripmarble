5208db3616561155ab1792f6adfbc254cc5b979d
신동근 <tlsehdrms124@naver.com>
2025-08-29 17:57:38 +0900

    [BE] feat: 게임 이동 로그 관련 컨트롤러 개발 진행중

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/in/web/controller/TripGameMoveLogWebController.java
@@ -0,0 +1,13 @@
+package com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/trip-games/{tripGameId}/move-logs")
+public class TripGameMoveLogWebController {
+
+    
+}
