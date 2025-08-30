36639c5b938ed478d10a88b18e16f5cd3a00ebb0
신동근 <tlsehdrms124@naver.com>
2025-08-30 16:20:35 +0900

    [BE] feat: 여행 게임 주사위 결과 INFO DTO 여행 게임 이동 로그 ID 필드 추가 및 매핑 관련 로직 추가 정의

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/info/TripGameDiceResultInfo.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/info/TripGameDiceResultInfo.java
@@ -1,11 +1,13 @@
 package com.followfollowme.tripmarble.domainlayer.game.application.info;
 
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameMoveLog;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameTile;
 import lombok.Builder;
 
 @Builder
 public record TripGameDiceResultInfo(
+    long tripGameMoveLogId,
     int diceValue,
     int finalStepNo,
     TripGame updatedGame,
@@ -13,8 +15,10 @@ public record TripGameDiceResultInfo(
     boolean isGameEnded
 ) {
 
-    public static TripGameDiceResultInfo of(int diceValue, int finalStepNo, TripGame game, TripGameTile tile, boolean isGameEnded) {
+    public static TripGameDiceResultInfo of(
+        TripGameMoveLog tripGameMoveLog, int diceValue, int finalStepNo, TripGame game, TripGameTile tile, boolean isGameEnded) {
         return TripGameDiceResultInfo.builder()
+            .tripGameMoveLogId(tripGameMoveLog.id())
             .diceValue(diceValue)
             .finalStepNo(finalStepNo)
             .updatedGame(game)
