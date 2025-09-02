cb908cfa03f609faf4d3cfa0164d64c87a4f42bb
신동근 <tlsehdrms124@naver.com>
2025-09-02 17:40:47 +0900

    [BE] feat: 여행 게임 상세 정보 Info DTO 정의

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/info/TripGameDetailInfo.java
@@ -0,0 +1,44 @@
+package com.followfollowme.tripmarble.domainlayer.game.application.info;
+
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Difficulty;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.EndType;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Status;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Builder;
+
+@Builder
+public record TripGameDetailInfo(
+    long tripGameId,
+    RepresentativeRegionInfo representativeRegionInfo,
+    List<String> tripThemeNames,
+    Status status,
+    Difficulty difficulty,
+    String title,
+    LocalDate startedAt,
+    LocalDate endedAt,
+    int currentTurnOrder,
+    int currentStepNo,
+    EndType endType,
+    List<TripGameMemberInfo> members
+) {
+
+    public static TripGameDetailInfo of(TripGame tripGame, RepresentativeRegionInfo representativeRegionInfo,
+        List<String> tripThemeNames, List<TripGameMemberInfo> members) {
+        return TripGameDetailInfo.builder()
+            .tripGameId(tripGame.id())
+            .representativeRegionInfo(representativeRegionInfo)
+            .tripThemeNames(tripThemeNames)
+            .status(tripGame.status())
+            .difficulty(tripGame.difficulty())
+            .title(tripGame.title())
+            .startedAt(tripGame.startedAt())
+            .endedAt(tripGame.endedAt())
+            .currentTurnOrder(tripGame.currentTurnOrder())
+            .currentStepNo(tripGame.currentStepNo())
+            .endType(tripGame.endType())
+            .members(members)
+            .build();
+    }
+}
