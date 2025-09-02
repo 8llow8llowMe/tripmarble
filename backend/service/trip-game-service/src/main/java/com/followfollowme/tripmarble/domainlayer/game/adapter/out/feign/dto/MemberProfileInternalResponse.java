adc7969c8db92cebf32dbf8de942fbad83d73ba8
신동근 <tlsehdrms124@naver.com>
2025-09-02 17:51:47 +0900

    [BE] fix: 회원 정보 프로필 관련 profileImageUrl로 필드명 변경

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/dto/MemberProfileInternalResponse.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/adapter/out/feign/dto/MemberProfileInternalResponse.java
@@ -3,7 +3,7 @@ package com.followfollowme.tripmarble.domainlayer.game.adapter.out.feign.dto;
 public record MemberProfileInternalResponse(
     long memberId,
     String nickname,
-    String profileImage
+    String profileImageUrl
 ) {
 
 }
