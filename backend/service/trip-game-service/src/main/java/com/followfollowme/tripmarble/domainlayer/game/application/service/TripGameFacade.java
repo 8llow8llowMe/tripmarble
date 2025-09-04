1b5c0366a4da15c7aaa24fa6688217bb0d53375d
신동근 <tlsehdrms124@naver.com>
2025-09-04 17:41:06 +0900

    [BE] fix: 여행 게임 생성 관련 삭제된 코드 긴급 추가

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameFacade.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameFacade.java
@@ -15,6 +15,7 @@ import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameD
 import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameEndInfo;
 import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameQueryInfo;
 import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameStartInfo;
+import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameTileCreateInfo;
 import com.followfollowme.tripmarble.domainlayer.game.application.port.in.TripGameWebUseCase;
 import com.followfollowme.tripmarble.domainlayer.game.application.service.processor.TripGameCreateProcessor;
 import com.followfollowme.tripmarble.domainlayer.game.application.service.processor.TripGameDetailProcessor;
@@ -25,6 +26,7 @@ import com.followfollowme.tripmarble.domainlayer.game.application.service.proces
 import com.followfollowme.tripmarble.domainlayer.game.application.service.processor.TripGameTileCreateProcessor;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Difficulty;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.enums.Status;
+import com.followfollowme.tripmarble.domainlayer.theme.domain.model.TripTheme;
 import com.followfollowme.tripmarble.persistence.dto.SliceResponse;
 import java.util.Arrays;
 import java.util.List;
@@ -61,8 +63,14 @@ public class TripGameFacade implements TripGameWebUseCase {
     public TripGameCreateResponse crateTripGame(TripGameCreateCommand command) {
         // 1. 여행 게임(계획) 및 관련 엔티티 생성
         TripGameCreateInfo tripGameCreateInfo = tripGameCreateProcessor.createGame(command);
+        // 2. 게임 내 블록(말판) 생성
+        TripGameTileCreateInfo tripGameTileCreateInfo = tripGameTileCreateProcessor.createTilesForGame(
+            tripGameCreateInfo.tripGame(),
+            tripGameCreateInfo.tripThemes().stream().map(TripTheme::id).toList(),
+            tripGameCreateInfo.tripGame().difficulty()
+        );
 
-        // 2. 응답용 DTO로 변환
+        // 3. 응답용 DTO로 변환
         return tripGamePresenter.toGameCreateResponse(tripGameCreateInfo);
     }
 
