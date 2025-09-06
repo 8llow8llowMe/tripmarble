c65e6bd95a5037fbddbdca7ad6aba09900bbd69a
신동근 <tlsehdrms124@naver.com>
2025-09-06 13:52:52 +0900

    [BE] fix: MinIO 버킷 관련 경로 통일성있게 수정

--- backend/core/storage-core/src/main/java/com/followfollowme/tripmarble/storage/bucket/MinioBucket.java
+++ backend/core/storage-core/src/main/java/com/followfollowme/tripmarble/storage/bucket/MinioBucket.java
@@ -7,10 +7,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum MinioBucket {
 
-    PROFILE_REAL_IMAGES("tripmarble", "profiles/real", "실제 프로필 이미지"),
-    PROFILE_TEMP_IMAGES("tripmarble", "profiles/temp", "임시 프로필 이미지"),
+    PROFILE_REAL_IMAGES("tripmarble", "profiles/real/images", "실제 프로필 이미지"),
+    PROFILE_TEMP_IMAGES("tripmarble", "profiles/temp/images", "임시 프로필 이미지"),
     REPRESENTATIVE_REGION_IMAGES("tripmarble", "representative-region/images", "대표 여행지 이미지"),
-    UPLOADS_TEMP("tripmarble", "uploads/temp", "임시 업로드 파일");
+    UPLOADS_TEMP("tripmarble", "uploads/temp/files", "임시 업로드 파일");
 
     private final String bucketName;
     private final String pathPrefix;
