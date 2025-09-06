0f4bbf033955f40ab166e0c3b14c3ea48be8f71b
신동근 <tlsehdrms124@naver.com>
2025-09-06 16:54:13 +0900

    [BE] feat: 여행 게임 이동 로그 목록 조회 프로세스 개발

---
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/processor/TripGameMoveLogQueryProcessor.java
@@ -0,0 +1,35 @@
+package com.followfollowme.tripmarble.domainlayer.game.application.service.processor;
+
+import com.followfollowme.tripmarble.domainlayer.game.application.exception.TripGameErrorCode;
+import com.followfollowme.tripmarble.domainlayer.game.application.exception.TripGameException;
+import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameMoveLogQueryInfo;
+import com.followfollowme.tripmarble.domainlayer.game.application.port.out.TripGameMoveLogRepositoryPort;
+import com.followfollowme.tripmarble.domainlayer.game.application.port.out.TripGameRepositoryPort;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
+import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameMoveLog;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TripGameMoveLogQueryProcessor {
+
+    private final TripGameRepositoryPort tripGameRepositoryPort;
+    private final TripGameMoveLogRepositoryPort tripGameMoveLogRepositoryPort;
+
+    public List<TripGameMoveLogQueryInfo> getMoveLogsByTripGameId(long tripGameId) {
+        // 1. 게임 존재 여부 검증
+        TripGame tripGame = tripGameRepositoryPort.findById(tripGameId)
+            .orElseThrow(() -> new TripGameException(TripGameErrorCode.GAME_NOT_FOUND));
+
+        // 2. 이동 로그 목록 조회
+        List<TripGameMoveLog> tripGameMoveLogs = tripGameMoveLogRepositoryPort.findAllByTripGameId(tripGameId);
+
+        // 3. Info 변환
+        return tripGameMoveLogs.stream()
+            .map(TripGameMoveLogQueryInfo::of)
+            .toList();
+    }
+}
