0b2ae25b9ffeb579da892c73c47110f2bf311ca9
신동근 <tlsehdrms124@naver.com>
2025-09-06 13:22:06 +0900

    [BE] feat: 회원 도메인에 회원 상태 필드 추가 및 회원 상태 관리 메서드들 추가 정의

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/domain/model/Member.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/domain/model/Member.java
@@ -1,6 +1,7 @@
 package com.followfollowme.tripmarble.domainlayer.member.domain.model;
 
 import com.followfollowme.tripmarble.domainlayer.auth.adapter.out.external.vendor.enums.OAuthProvider;
+import com.followfollowme.tripmarble.domainlayer.member.domain.model.enums.MemberStatus;
 import com.followfollowme.tripmarble.security.common.enums.SecurityRole;
 import lombok.Builder;
 
@@ -13,7 +14,51 @@ public record Member(
     String nickname,
     String profileImageUrl,
     SecurityRole role,
-    OAuthProvider provider
+    OAuthProvider provider,
+    MemberStatus status
 ) {
 
+    public Member withdraw() {
+        this.status.validateWithdrawable();
+        return Member.builder()
+            .id(this.id)
+            .email(this.email)
+            .password(this.password)
+            .name("탈퇴회원")
+            .nickname("탈퇴회원")
+            .profileImageUrl(null)
+            .role(this.role)
+            .provider(this.provider)
+            .status(MemberStatus.WITHDRAWN)
+            .build();
+    }
+
+    public Member restore(String newName, String newNickname) {
+        this.status.validateRestorable();
+        return Member.builder()
+            .id(this.id)
+            .email(this.email)
+            .password(this.password)
+            .name(newName)
+            .nickname(newNickname)
+            .profileImageUrl(this.profileImageUrl)
+            .role(this.role)
+            .provider(this.provider)
+            .status(MemberStatus.ACTIVE)
+            .build();
+    }
+
+    public Member suspend() {
+        return Member.builder()
+            .id(this.id)
+            .email(this.email)
+            .password(this.password)
+            .name(this.name)
+            .nickname(this.nickname)
+            .profileImageUrl(this.profileImageUrl)
+            .role(this.role)
+            .provider(this.provider)
+            .status(MemberStatus.SUSPENDED)
+            .build();
+    }
 }
