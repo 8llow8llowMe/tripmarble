8a07dfc4a6234131c66b59f24c8c4390f4dd2d6a
신동근 <tlsehdrms124@naver.com>
2025-09-06 15:23:22 +0900

    [BE] fix: MinIO 버킷 관련 대표 여행지 이미지 관련 수정

--- backend/core/storage-core/src/main/java/com/followfollowme/tripmarble/storage/bucket/MinioBucket.java
+++ backend/core/storage-core/src/main/java/com/followfollowme/tripmarble/storage/bucket/MinioBucket.java
@@ -9,7 +9,7 @@ public enum MinioBucket {
 
     PROFILE_REAL_IMAGES("tripmarble", "profiles/real/images", "실제 프로필 이미지"),
     PROFILE_TEMP_IMAGES("tripmarble", "profiles/temp/images", "임시 프로필 이미지"),
-    REPRESENTATIVE_REGION_IMAGES("tripmarble", "representative-region/images", "대표 여행지 이미지"),
+    REPRESENTATIVE_REGION_IMAGES("tripmarble", "representative-regions/images", "대표 여행지 이미지"),
     UPLOADS_TEMP("tripmarble", "uploads/temp/files", "임시 업로드 파일");
 
     private final String bucketName;
