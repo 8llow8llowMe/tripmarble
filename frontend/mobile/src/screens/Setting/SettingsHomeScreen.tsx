c012d6a7a898c54a6a60ed52c890bfeefee7bba1
eestingz <tmdgus1761@gmail.com>
2025-09-06 16:05:45 +0900

    [FE/mobile] feat: 이용약관, 개인정보처리방침 bottom-sheet 적용

--- frontend/mobile/src/screens/Setting/SettingsHomeScreen.tsx
+++ frontend/mobile/src/screens/Setting/SettingsHomeScreen.tsx
@@ -1,13 +1,38 @@
 import TextBox from '@/components/atom/TextBox';
+import PrivacyPolicySheet from '@/components/bottomSheet/PrivacyPolicySheet';
+import ServiceTermsSheet from '@/components/bottomSheet/ServiceTermsSheet';
 import Divider from '@/components/common/Divider';
 import { palette } from '@/constants/colors';
+import { useBottomSheetBase } from '@/hooks/useBottomSheetBase';
 import { Ionicons } from '@expo/vector-icons';
+import { BottomSheetBackdrop, BottomSheetModal } from '@gorhom/bottom-sheet';
 
 import React from 'react';
 import { SafeAreaView, TouchableOpacity } from 'react-native';
 import { View, StyleSheet, ScrollView } from 'react-native';
 
 const SettingsHomeScreen = () => {
+  const {
+    bottomSheetRef: serviceTermsBottomSheetRef,
+    openSheet: openServiceTermsSheet,
+    closeSheet: closeServiceTermsSheet,
+  } = useBottomSheetBase();
+
+  const {
+    bottomSheetRef: privacyPolicyBottomSheetRef,
+    openSheet: openPrivacyPolicySheet,
+    closeSheet: closePrivacyPolicySheet,
+  } = useBottomSheetBase();
+
+  const renderBackdrop = (props: any) => (
+    <BottomSheetBackdrop
+      {...props}
+      pressBehavior={'close'}
+      disappearsOnIndex={-1} // 기본적으로 항상 표시되도록 설정
+      appearsOnIndex={0} // 시작 지점에서도 배경이 나타나도록 설정
+    />
+  );
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <ScrollView contentContainerStyle={styles.container}>
@@ -29,24 +54,24 @@ const SettingsHomeScreen = () => {
           </TextBox>
 
           <View style={{ gap: 28 }}>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={openServiceTermsSheet}>
               <View style={styles.row}>
                 <TextBox size={15}>이용약관</TextBox>
                 <Ionicons name="chevron-forward" size={18} color={palette.gray300} />
               </View>
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={openPrivacyPolicySheet}>
               <View style={styles.row}>
                 <TextBox size={15}>개인정보처리방침</TextBox>
                 <Ionicons name="chevron-forward" size={18} color={palette.gray300} />
               </View>
             </TouchableOpacity>
-            <TouchableOpacity>
+            {/* <TouchableOpacity>
               <View style={styles.row}>
                 <TextBox size={15}>위치기반서비스 이용약관</TextBox>
                 <Ionicons name="chevron-forward" size={18} color={palette.gray300} />
               </View>
-            </TouchableOpacity>
+            </TouchableOpacity> */}
             <Divider />
           </View>
         </View>
@@ -71,6 +96,34 @@ const SettingsHomeScreen = () => {
           </TouchableOpacity>
         </View>
       </ScrollView>
+
+      <BottomSheetModal
+        ref={serviceTermsBottomSheetRef}
+        handleStyle={{
+          backgroundColor: palette.white,
+          borderTopLeftRadius: 12,
+          borderTopRightRadius: 12,
+        }}
+        index={0}
+        snapPoints={[605]}
+        backdropComponent={renderBackdrop}
+      >
+        <ServiceTermsSheet />
+      </BottomSheetModal>
+
+      <BottomSheetModal
+        ref={privacyPolicyBottomSheetRef}
+        handleStyle={{
+          backgroundColor: palette.white,
+          borderTopLeftRadius: 12,
+          borderTopRightRadius: 12,
+        }}
+        index={0}
+        snapPoints={[605]}
+        backdropComponent={renderBackdrop}
+      >
+        <PrivacyPolicySheet />
+      </BottomSheetModal>
     </SafeAreaView>
   );
 };
