18e880adc8e89455e5e57860193664f7e567f449
eestingz <tmdgus1761@gmail.com>
2025-09-06 16:04:15 +0900

    [FE/mobile] feat: create bottom-sheet hook

---
+++ frontend/mobile/src/hooks/useBottomSheetBase.ts
@@ -0,0 +1,51 @@
+import PrivacyPolicySheet from '@/components/bottomSheet/PrivacyPolicySheet';
+import ServiceTermsSheet from '@/components/bottomSheet/ServiceTermsSheet';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
+import { useFocusEffect } from '@react-navigation/native';
+
+import { useCallback, useRef, useState } from 'react';
+import { BackHandler } from 'react-native';
+
+export const BOTTOM_SHEET_COMPONENTS = {
+  ServiceTermsSheet: ServiceTermsSheet, // 이용약관
+  PrivacyPolicySheet: PrivacyPolicySheet, // 개인정보 처리방침
+} as const;
+
+type BottomSheetName = keyof typeof BOTTOM_SHEET_COMPONENTS;
+
+export const useBottomSheetBase = () => {
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openSheet = () => {
+    bottomSheetRef.current?.present();
+    setIsOpen(true);
+  };
+
+  const closeSheet = () => {
+    bottomSheetRef.current?.dismiss();
+    setIsOpen(false);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      const backAction = () => {
+        if (isOpen) {
+          closeSheet();
+          return true;
+        }
+        return false;
+      };
+
+      const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+      return () => backHandler.remove();
+    }, [isOpen]),
+  );
+
+  return {
+    bottomSheetRef,
+    openSheet,
+    closeSheet,
+  };
+};
