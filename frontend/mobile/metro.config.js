2a762ffbaaf85d35633d4a7eecfd214430e9b167
eestingz <tmdgus1761@gmail.com>
2025-09-06 16:02:49 +0900

    [FE/mobile] feat: metro config 추가

--- frontend/mobile/metro.config.js
+++ frontend/mobile/metro.config.js
@@ -1,2 +1,9 @@
 const { getDefaultConfig } = require('expo/metro-config');
-module.exports = getDefaultConfig(__dirname);
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+config.resolver.assetExts.push('md');
+
+module.exports = wrapWithReanimatedMetroConfig(config);
