c149ecd69689f3a9c1d2ea7a01bd988fd4d43b16
신동근 <tlsehdrms124@naver.com>
2025-09-06 16:54:59 +0900

    [BE] feat: 여행 게임 이동 로그 목록 조회 기능 개발 (메인 서비스단)

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/in/TripGameMoveLogWebUseCase.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/in/TripGameMoveLogWebUseCase.java
@@ -1,6 +1,9 @@
 package com.followfollowme.tripmarble.domainlayer.game.application.port.in;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.MissionResultResponse;
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameMoveLogResponse;
+
+import java.util.List;
 
 public interface TripGameMoveLogWebUseCase {
 
@@ -9,4 +12,6 @@ public interface TripGameMoveLogWebUseCase {
     MissionResultResponse successMission(long tripGameId, long tripGameMoveLogId, long memberId);
 
     MissionResultResponse failMission(long tripGameId, long tripGameMoveLogId, long memberId);
+
+    List<TripGameMoveLogResponse> getMoveLogsByTripGameId(long tripGameId);
 }
--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameMoveLogFacade.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameMoveLogFacade.java
@@ -1,19 +1,25 @@
 package com.followfollowme.tripmarble.domainlayer.game.application.service;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.MissionResultResponse;
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameMoveLogResponse;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.presenter.TripGameMoveLogPresenter;
 import com.followfollowme.tripmarble.domainlayer.game.application.info.MissionResultInfo;
+import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameMoveLogQueryInfo;
 import com.followfollowme.tripmarble.domainlayer.game.application.port.in.TripGameMoveLogWebUseCase;
 import com.followfollowme.tripmarble.domainlayer.game.application.service.processor.TripGameMissionProcessor;
+import com.followfollowme.tripmarble.domainlayer.game.application.service.processor.TripGameMoveLogQueryProcessor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TripGameMoveLogFacade implements TripGameMoveLogWebUseCase {
 
     private final TripGameMissionProcessor tripGameMissionProcessor;
+    private final TripGameMoveLogQueryProcessor tripGameMoveLogQueryProcessor;
     private final TripGameMoveLogPresenter tripGameMoveLogPresenter;
 
     @Override
@@ -37,5 +43,10 @@ public class TripGameMoveLogFacade implements TripGameMoveLogWebUseCase {
         return tripGameMoveLogPresenter.toMissionResultResponse(missionResultInfo);
     }
 
-
+    @Override
+    @Transactional(readOnly = true)
+    public List<TripGameMoveLogResponse> getMoveLogsByTripGameId(long tripGameId) {
+        List<TripGameMoveLogQueryInfo> tripGameMoveLogQueryInfos = tripGameMoveLogQueryProcessor.getMoveLogsByTripGameId(tripGameId);
+        return tripGameMoveLogPresenter.toMoveLogResponses(tripGameMoveLogQueryInfos);
+    }
 }
