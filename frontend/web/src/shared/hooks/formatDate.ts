eefd2787b508f448d5a4674865c143b55d57db1f
seonghoho <seonghoho@codnut.com>
2025-09-06 21:17:18 +0900

    [FE/web] fix: formatData 함수 재사용하도록 변경

--- frontend/web/src/shared/hooks/formatDate.ts
+++ frontend/web/src/shared/hooks/formatDate.ts
@@ -1,6 +1,6 @@
 const formatDate = (date: Date | null) => {
   if (!date) return "";
-  const year = date.getFullYear();
+  const year = String(date.getFullYear()).slice(2);
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
