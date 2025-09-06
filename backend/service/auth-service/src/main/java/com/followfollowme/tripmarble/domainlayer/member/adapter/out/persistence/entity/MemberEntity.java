2f77bde2f167df4dad6f5bcfee932f0a637712f8
신동근 <tlsehdrms124@naver.com>
2025-09-06 13:19:33 +0900

    [BE] feat: 회원 엔티티에 회원 상태 엔티티 추가

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/out/persistence/entity/MemberEntity.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/out/persistence/entity/MemberEntity.java
@@ -1,6 +1,7 @@
 package com.followfollowme.tripmarble.domainlayer.member.adapter.out.persistence.entity;
 
 import com.followfollowme.tripmarble.domainlayer.auth.adapter.out.external.vendor.enums.OAuthProvider;
+import com.followfollowme.tripmarble.domainlayer.member.domain.model.enums.MemberStatus;
 import com.followfollowme.tripmarble.persistence.entity.BaseEntity;
 import com.followfollowme.tripmarble.security.common.enums.SecurityRole;
 import jakarta.persistence.Column;
@@ -61,4 +62,8 @@ public class MemberEntity extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private OAuthProvider provider;
 
+    @Comment("회원 상태")
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
 }
