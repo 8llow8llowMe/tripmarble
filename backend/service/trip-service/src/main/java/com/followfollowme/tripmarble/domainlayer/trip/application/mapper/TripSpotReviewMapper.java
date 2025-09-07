74e82fd7c4122c87f5316d7f31b96654bd300013
신동근 <tlsehdrms124@naver.com>
2025-09-07 16:06:21 +0900

    [BE] feat: 여행지 리뷰 Mapper 정의 (엔티티 <-> 도메인)

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/mapper/TripSpotReviewMapper.java
@@ -0,0 +1,24 @@
+package com.followfollowme.tripmarble.domainlayer.trip.application.mapper;
+
+import com.followfollowme.tripmarble.domainlayer.trip.adapter.out.persistence.entity.TripSpotReviewEntity;
+import com.followfollowme.tripmarble.domainlayer.trip.domain.model.TripSpot;
+import com.followfollowme.tripmarble.domainlayer.trip.domain.model.TripSpotReview;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = TripSpotMapper.class)
+public interface TripSpotReviewMapper {
+
+    // 엔티티 -> 도메인
+    @Mapping(target = "tripSpotId", source = "tripSpot.id")
+    TripSpotReview toDomainFromEntity(TripSpotReviewEntity entity);
+
+    // 도메인 -> 엔티티
+    @Mapping(target = "id", source = "domain.id")
+    @Mapping(target = "tripSpot", source = "tripSpot")
+    @Mapping(target = "memberId", source = "domain.memberId")
+    @Mapping(target = "content", source = "domain.content")
+    @Mapping(target = "rating", source = "domain.rating")
+    @Mapping(target = "sourceType", source = "domain.sourceType")
+    TripSpotReviewEntity toEntityFromDomain(TripSpotReview domain, TripSpot tripSpot);
+}
