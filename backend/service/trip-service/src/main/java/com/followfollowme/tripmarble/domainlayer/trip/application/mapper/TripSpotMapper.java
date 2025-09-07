881eb2353ec16dc8004726fa8e3ebdec974744c1
신동근 <tlsehdrms124@naver.com>
2025-09-07 14:38:32 +0900

    [BE] feat: 여행지 정보 관련 Mapper (엔티티 <-> 도메인) 도메인 -> 엔티티 매핑 정의

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/mapper/TripSpotMapper.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/mapper/TripSpotMapper.java
@@ -2,14 +2,18 @@ package com.followfollowme.tripmarble.domainlayer.trip.application.mapper;
 
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.out.persistence.entity.TripSpotEntity;
 import com.followfollowme.tripmarble.domainlayer.trip.domain.model.TripSpot;
-import java.util.List;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TripSpotMapper {
 
     // 엔티티 -> 도메인
     TripSpot toDomainFromEntity(TripSpotEntity entity);
+
+    // 도메인 -> 엔티티
+    TripSpotEntity toEntityFromDomain(TripSpot domain);
 
     // 엔티티 리스트 -> 도메인 리스트
     List<TripSpot> toDomainListFromEntityList(List<TripSpotEntity> entities);
