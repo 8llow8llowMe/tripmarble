17bfa2c60790f12dc8e11d122884b788c642cefc
신동근 <tlsehdrms124@naver.com>
2025-09-08 17:07:10 +0900

    [BE] feat: 내부 서비스 통신 전용 리뷰 생성 요청 DTO 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/in/internal/dto/TripSpotReviewCreateInternalRequest.java
@@ -0,0 +1,13 @@
+package com.followfollowme.tripmarble.domainlayer.trip.adapter.in.internal.dto;
+
+import java.util.List;
+
+public record TripSpotReviewCreateInternalRequest(
+    long tripSpotId,
+    long memberId,
+    String content,
+    double rating,
+    List<String> photoUrls
+) {
+
+}
