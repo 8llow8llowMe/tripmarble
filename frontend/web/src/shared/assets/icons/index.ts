b4a1af779cf0f369ff5d641b649a367b3098305b
seonghoho <chltjdgh3@naver.com>
2025-09-09 00:16:09 +0900

    [FE/web] feat: 왼쪽, 오른쪽 화살 svg 추가

--- frontend/web/src/shared/assets/icons/index.ts
+++ frontend/web/src/shared/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as CheckIcon } from "./check";
 export { default as CloseIcon } from "./close";
+export { default as ArrowLeftIcon } from "./arrowLeft";
+export { default as ArrowRightIcon } from "./arrowRight";
