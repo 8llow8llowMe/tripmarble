c4c926eaefea8d5d2d430e9712f556519d7a3e02
신동근 <tlsehdrms124@naver.com>
2025-09-08 17:41:06 +0900

    [BE] feat: 여행 게임 이동 로그 관련 리뷰 미션 처리 비즈니스 로직 정의

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/in/TripGameMoveLogWebUseCase.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/in/TripGameMoveLogWebUseCase.java
@@ -2,14 +2,14 @@ package com.followfollowme.tripmarble.domainlayer.game.application.port.in;
 
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.MissionResultResponse;
 import com.followfollowme.tripmarble.domainlayer.game.adapter.in.web.dto.TripGameMoveLogResponse;
-
+import com.followfollowme.tripmarble.domainlayer.game.application.command.ReviewMissionCommand;
 import java.util.List;
 
 public interface TripGameMoveLogWebUseCase {
 
-    MissionResultResponse skipMission(long tripGameId, long tripGameMoveLogId, long memberId);
+    MissionResultResponse processReviewMission(long tripGameId, long tripGameMoveLogId, long memberId, ReviewMissionCommand command);
 
-    MissionResultResponse successMission(long tripGameId, long tripGameMoveLogId, long memberId);
+    MissionResultResponse skipMission(long tripGameId, long tripGameMoveLogId, long memberId);
 
     MissionResultResponse failMission(long tripGameId, long tripGameMoveLogId, long memberId);
 
