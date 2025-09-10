156202e3610d29bf8d59d5d889f42c2185dd8ccb
seonghoho <seonghoho@codnut.com>
2025-09-10 13:55:42 +0900

    [FE/web] feat: three dot 아이콘 추가

--- frontend/web/src/shared/assets/icons/index.ts
+++ frontend/web/src/shared/assets/icons/index.ts
@@ -2,3 +2,4 @@ export { default as CheckIcon } from "./check";
 export { default as CloseIcon } from "./close";
 export { default as ArrowLeftIcon } from "./arrowLeft";
 export { default as ArrowRightIcon } from "./arrowRight";
+export { default as DotThreeIcon } from "./dotThree";
