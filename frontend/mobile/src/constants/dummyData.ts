4eb8667ecc5875a5d16914d97c17789b71720e77
eestingz <tmdgus1761@gmail.com>
2025-09-15 03:45:38 +0900

    feat: dummy data 실제 데이터로 임시 저장

--- frontend/mobile/src/constants/dummyData.ts
+++ frontend/mobile/src/constants/dummyData.ts
@@ -5,28 +5,26 @@ export const DUMMY_RANDOM_PICK = {
   tripSpotName: '설악해수욕장',
 };
 
+//국립아시아문화전당, 협재해수욕장, 협재동굴, 추자군도
 export const DUMMY_PLACES = [
   {
-    id: '1',
-    name: '부산 해운대',
-    image:
-      'https://images.unsplash.com/photo-1517959105821-eaf2591984dd?q=80&w=1600&auto=format&fit=crop',
+    id: '44118',
+    name: '부산 해운대해수욕장',
+    image: 'http://tong.visitkorea.or.kr/cms/resource/34/3090534_image2_1.JPG',
     tags: ['바다', '야경'],
     score: 4.7,
   },
   {
-    id: '2',
-    name: '강릉 안목해변',
-    image:
-      'https://images.unsplash.com/photo-1504604792257-22ebeb14f00a?q=80&w=1600&auto=format&fit=crop',
+    id: '19366',
+    name: '강릉 사근진해변',
+    image: 'http://tong.visitkorea.or.kr/cms/resource/59/2921959_image2_1.jpg',
     tags: ['카페', '여유'],
     score: 4.5,
   },
   {
-    id: '3',
+    id: '21791',
     name: '제주 성산일출봉',
-    image:
-      'https://images.unsplash.com/photo-1607863680051-7e2d0d0e1a8f?q=80&w=1600&auto=format&fit=crop',
+    image: 'http://tong.visitkorea.or.kr/cms/resource/82/2944282_image2_1.bmp',
     tags: ['등산', '자연'],
     score: 4.8,
   },
