f5d03d013e94144012ecce1a551e8b58769dc54d
신동근 <tlsehdrms124@naver.com>
2025-09-15 18:50:56 +0900

    [BE] feat: 해당 여행지 리뷰 ID 목록에 따른 첨부된 사진 목록 조회 쿼리 기능 개발

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/out/persistence/repository/TripSpotReviewPhotoRepository.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/adapter/out/persistence/repository/TripSpotReviewPhotoRepository.java
@@ -1,8 +1,10 @@
 package com.followfollowme.tripmarble.domainlayer.trip.adapter.out.persistence.repository;
 
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.out.persistence.entity.TripSpotReviewPhotoEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TripSpotReviewPhotoRepository extends JpaRepository<TripSpotReviewPhotoEntity, Long> {
 
+    List<TripSpotReviewPhotoEntity> findByTripSpotReviewIdIn(List<Long> tripSpotReviewIds);
 }
