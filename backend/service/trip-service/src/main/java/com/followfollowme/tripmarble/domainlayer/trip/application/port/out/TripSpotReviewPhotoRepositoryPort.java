f39b945171d56b3487c8e5ea43c8f614df86953a
신동근 <tlsehdrms124@naver.com>
2025-09-15 18:54:21 +0900

    [BE] feat: 여행지 리뷰 ID 목록에 따른 여행지 리뷰 사진 목록 조회 기능 개발 (Repository)

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/port/out/TripSpotReviewPhotoRepositoryPort.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/port/out/TripSpotReviewPhotoRepositoryPort.java
@@ -2,10 +2,11 @@ package com.followfollowme.tripmarble.domainlayer.trip.application.port.out;
 
 import com.followfollowme.tripmarble.domainlayer.trip.domain.model.TripSpotReview;
 import com.followfollowme.tripmarble.domainlayer.trip.domain.model.TripSpotReviewPhoto;
-
 import java.util.List;
 
 public interface TripSpotReviewPhotoRepositoryPort {
 
     List<TripSpotReviewPhoto> saveAll(List<TripSpotReviewPhoto> tripSpotReviewPhotos, TripSpotReview tripSpotReview);
+
+    List<TripSpotReviewPhoto> findByTripSpotReviewIdIn(List<Long> tripSpotReviewIds);
 }
