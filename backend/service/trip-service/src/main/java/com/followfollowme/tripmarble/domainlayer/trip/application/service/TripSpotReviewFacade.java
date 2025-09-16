bb3ea6cd561c3ea382fa4542f60812a9e0a17156
신동근 <tlsehdrms124@naver.com>
2025-09-16 17:52:12 +0900

    [BE] feat: 여행지 리뷰 상세 조회 및 해당 여행지의 리뷰 사진 임시 업로드 비즈니스 로직 개발

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/service/TripSpotReviewFacade.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/service/TripSpotReviewFacade.java
@@ -2,16 +2,21 @@ package com.followfollowme.tripmarble.domainlayer.trip.application.service;
 
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto.TripSpotReviewAndPhotosResponse;
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto.TripSpotReviewCreateResponse;
+import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto.TripSpotReviewDetailResponse;
+import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto.TripSpotReviewPhotoUploadResponse;
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.dto.TripSpotReviewSummaryResponse;
 import com.followfollowme.tripmarble.domainlayer.trip.adapter.in.web.presenter.TripSpotReviewPresenter;
 import com.followfollowme.tripmarble.domainlayer.trip.application.command.TripSpotReviewCreateCommand;
 import com.followfollowme.tripmarble.domainlayer.trip.application.info.TripSpotReviewAndPhotosInfo;
 import com.followfollowme.tripmarble.domainlayer.trip.application.info.TripSpotReviewCreateInfo;
+import com.followfollowme.tripmarble.domainlayer.trip.application.info.TripSpotReviewDetailInfo;
 import com.followfollowme.tripmarble.domainlayer.trip.application.info.TripSpotReviewPhotoCreateInfo;
+import com.followfollowme.tripmarble.domainlayer.trip.application.info.TripSpotReviewPhotoUploadInfo;
 import com.followfollowme.tripmarble.domainlayer.trip.application.info.TripSpotReviewSummaryInfo;
 import com.followfollowme.tripmarble.domainlayer.trip.application.port.in.TripSpotReviewWebUseCase;
 import com.followfollowme.tripmarble.domainlayer.trip.application.service.processor.TripSpotReviewCreateProcessor;
 import com.followfollowme.tripmarble.domainlayer.trip.application.service.processor.TripSpotReviewPhotoCreateProcessor;
+import com.followfollowme.tripmarble.domainlayer.trip.application.service.processor.TripSpotReviewPhotoUploadProcessor;
 import com.followfollowme.tripmarble.domainlayer.trip.application.service.processor.TripSpotReviewQueryProcessor;
 import com.followfollowme.tripmarble.persistence.dto.SliceResponse;
 import java.util.List;
@@ -19,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +33,7 @@ public class TripSpotReviewFacade implements TripSpotReviewWebUseCase {
     private final TripSpotReviewCreateProcessor tripSpotReviewCreateProcessor;
     private final TripSpotReviewPhotoCreateProcessor tripSpotReviewPhotoCreateProcessor;
     private final TripSpotReviewQueryProcessor tripSpotReviewQueryProcessor;
+    private final TripSpotReviewPhotoUploadProcessor tripSpotReviewPhotoUploadProcessor;
     private final TripSpotReviewPresenter tripSpotReviewPresenter;
 
     @Override
@@ -36,10 +43,14 @@ public class TripSpotReviewFacade implements TripSpotReviewWebUseCase {
         TripSpotReviewCreateInfo reviewCreateInfo = tripSpotReviewCreateProcessor.createGeneralReview(tripSpotId, memberId,
             command.content(), command.rating());
 
-        // 2. 사진 저장
-        List<TripSpotReviewPhotoCreateInfo> photoCreateInfos = tripSpotReviewPhotoCreateProcessor.createPhotos(
-            reviewCreateInfo.tripSpotReviewId(), command.photoUrls());
+        // 2. temp -> real 변환
+        List<String> realPhotoUrls = tripSpotReviewPhotoUploadProcessor.promoteToReal(command.photoUrls());
 
+        // 3. 사진 저장
+        List<TripSpotReviewPhotoCreateInfo> photoCreateInfos = tripSpotReviewPhotoCreateProcessor.createPhotos(
+            reviewCreateInfo.tripSpotReviewId(), realPhotoUrls);
+
+        // 4. Presenter 변환
         return tripSpotReviewPresenter.toCreateResponse(reviewCreateInfo, photoCreateInfos);
     }
 
@@ -56,5 +67,21 @@ public class TripSpotReviewFacade implements TripSpotReviewWebUseCase {
         Slice<TripSpotReviewAndPhotosInfo> tripSpotReviewAndPhotosInfoSlice =
             tripSpotReviewQueryProcessor.getTripSpotReviews(tripSpotId, lastTripSpotReviewId, size);
         return tripSpotReviewPresenter.toReviewAndPhotosSliceResponse(tripSpotReviewAndPhotosInfoSlice);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public TripSpotReviewDetailResponse getTripSpotReviewDetail(long tripSpotId, long tripSpotReviewId) {
+        TripSpotReviewDetailInfo tripSpotReviewDetailInfo =
+            tripSpotReviewQueryProcessor.getTripSpotReviewDetail(tripSpotId, tripSpotReviewId);
+        return tripSpotReviewPresenter.toDetailResponse(tripSpotReviewDetailInfo);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<TripSpotReviewPhotoUploadResponse> uploadTempReviewPhotos(long tripSpotId, List<MultipartFile> imageFiles) {
+        List<TripSpotReviewPhotoUploadInfo> tripSpotReviewPhotoUploadInfos =
+            tripSpotReviewPhotoUploadProcessor.uploadTempReviewPhotos(tripSpotId, imageFiles);
+        return tripSpotReviewPresenter.toUploadResponses(tripSpotReviewPhotoUploadInfos);
     }
 }
