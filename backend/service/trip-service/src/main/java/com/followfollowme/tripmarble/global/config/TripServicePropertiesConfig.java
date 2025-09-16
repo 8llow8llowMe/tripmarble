7c794abdc98f4485399df48d47535fd7b13a588e
신동근 <tlsehdrms124@naver.com>
2025-09-16 17:43:27 +0900

    [BE] feat: trip-service 프로퍼티 설정 클래스에 Minio 프로퍼티 바인딩 설정 클래스 Import

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/config/TripServicePropertiesConfig.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/config/TripServicePropertiesConfig.java
@@ -4,6 +4,7 @@ import com.followfollowme.tripmarble.common.config.JasyptPropertiesConfig;
 import com.followfollowme.tripmarble.common.config.SwaggerPropertiesConfig;
 import com.followfollowme.tripmarble.persistence.config.SnowflakePropertiesConfig;
 import com.followfollowme.tripmarble.security.resourceserver.config.JwtResourceServerPropertiesConfig;
+import com.followfollowme.tripmarble.storage.config.MinioPropertiesConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -12,7 +13,8 @@ import org.springframework.context.annotation.Import;
     JasyptPropertiesConfig.class,
     SwaggerPropertiesConfig.class,
     SnowflakePropertiesConfig.class,
-    JwtResourceServerPropertiesConfig.class
+    JwtResourceServerPropertiesConfig.class,
+    MinioPropertiesConfig.class
 })
 public class TripServicePropertiesConfig {
 
