563376f25b3b35bc87374a6a9df6656ad16acdca
신동근 <tlsehdrms124@naver.com>
2025-09-16 17:45:54 +0900

    [BE] fix: 여행지 관련 커스텀 에러코드 여행지 리뷰 사진 업로드 관련 추가 및 문구 수정

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/exception/TripErrorCode.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/exception/TripErrorCode.java
@@ -12,7 +12,7 @@ public enum TripErrorCode {
     TRIP_CONTENT_TYPE_NOT_FOUND("TRIP_002", "해당 여행지 콘텐츠 타입을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     TRIP_SPOT_DETAIL_NOT_FOUND("TRIP_003", "해당 여행지 상세 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     TRIP_SPOT_REVIEW_NOT_FOUND("TRIP_004", "해당 여행지 리뷰를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    TRIP_SPOT_REVIEW_PHOTO_SAVE_FAILED("TRIP_005", "여행지 리뷰 사진 저장에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    UPLOAD_REVIEW_PHOTO_FAILED("TRIP_005", "여행지 리뷰 사진 업로드에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final String code;
     private final String errorMessage;
