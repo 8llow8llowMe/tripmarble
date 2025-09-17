45c87111fbe090fc30e5ac3ad8ddf1757e0d88ee
신동근 <tlsehdrms124@naver.com>
2025-09-17 17:15:08 +0900

    [BE] fix: auth service swagger 설정 전역적으로 설정한 Security 설정 (API) 해제

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/AuthServiceSwaggerConfig.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/AuthServiceSwaggerConfig.java
@@ -1,10 +1,10 @@
 package com.followfollowme.tripmarble.global.config;
 
-import com.followfollowme.tripmarble.common.property.SwaggerProperties;
+import com.followfollowme.tripmarble.common.properties.SwaggerProperties;
 import com.followfollowme.tripmarble.common.support.SwaggerSecurityCommon;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -22,11 +22,10 @@ public class AuthServiceSwaggerConfig {
     private final SwaggerProperties properties;
 
     @Bean
-    public OpenAPI authOpenAPI(Components components, SecurityRequirement securityRequirement) {
+    public OpenAPI authOpenAPI(Components components) {
         return new OpenAPI()
             .components(components)
-            .addSecurityItem(securityRequirement)
-            .info(new io.swagger.v3.oas.models.info.Info()
+            .info(new Info()
                 .title("트립마블(TripMarble) 회원 및 인증/인가 서비스 관련 API 명세서")
                 .description("Auth Service 전용")
                 .version("v1")
