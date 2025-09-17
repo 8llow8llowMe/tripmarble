8fcaed13457f7384069cd0294d988857dd68b7d4
신동근 <tlsehdrms124@naver.com>
2025-09-17 17:29:05 +0900

    [BE] feat: 해당 회원의 프로필 조회 기능 개발 (내부 서비스 통신 전용)

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/service/MemberInternalFacade.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/service/MemberInternalFacade.java
@@ -2,6 +2,8 @@ package com.followfollowme.tripmarble.domainlayer.member.application.service;
 
 import com.followfollowme.tripmarble.domainlayer.member.adapter.in.internal.dto.MemberProfileInternalResponse;
 import com.followfollowme.tripmarble.domainlayer.member.adapter.in.internal.presenter.MemberInternalPresenter;
+import com.followfollowme.tripmarble.domainlayer.member.application.exception.MemberErrorCode;
+import com.followfollowme.tripmarble.domainlayer.member.application.exception.MemberException;
 import com.followfollowme.tripmarble.domainlayer.member.application.port.in.MemberInternalUseCase;
 import com.followfollowme.tripmarble.domainlayer.member.application.port.out.MemberRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.member.domain.model.Member;
@@ -22,5 +24,13 @@ public class MemberInternalFacade implements MemberInternalUseCase {
     public List<MemberProfileInternalResponse> getMemberProfiles(List<Long> memberIds) {
         List<Member> members = memberRepositoryPort.findByIdIn(memberIds);
         return memberInternalPresenter.toProfileResonseList(members);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public MemberProfileInternalResponse getMemberProfiles(long memberId) {
+        Member member = memberRepositoryPort.findById(memberId)
+            .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_MEMBER));
+        return memberInternalPresenter.toProfileResponse(member);
     }
 }
