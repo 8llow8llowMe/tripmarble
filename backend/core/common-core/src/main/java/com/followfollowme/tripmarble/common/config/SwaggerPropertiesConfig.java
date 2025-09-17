17f89d365830b1ca5a075e885f36994b24a09a70
신동근 <tlsehdrms124@naver.com>
2025-09-17 17:14:06 +0900

    [BE] fix: 패키지명 변경에 따른 import 구문 수정

--- backend/core/common-core/src/main/java/com/followfollowme/tripmarble/common/config/SwaggerPropertiesConfig.java
+++ backend/core/common-core/src/main/java/com/followfollowme/tripmarble/common/config/SwaggerPropertiesConfig.java
@@ -1,8 +1,9 @@
 package com.followfollowme.tripmarble.common.config;
 
-import com.followfollowme.tripmarble.common.property.SwaggerProperties;
+import com.followfollowme.tripmarble.common.properties.SwaggerProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableConfigurationProperties(SwaggerProperties.class)
 public class SwaggerPropertiesConfig {
+
 }
