01a4463d9137c0587b35c572634dc0303073a664
seonghoho <seonghoho@codnut.com>
2025-09-18 14:11:38 +0900

    [FE/web] fix: 게임 만들기 모달 x 버튼 위치 수정

--- frontend/web/src/shared/ui/common/Modal/Modal.tsx
+++ frontend/web/src/shared/ui/common/Modal/Modal.tsx
@@ -19,10 +19,10 @@ const Modal = ({
   return (
     <div className={styles.overlay} onClick={onClose}>
       <div className={styles.content} onClick={(e) => e.stopPropagation()}>
-        {children}
         <button className={styles.closeButton} onClick={onClose}>
           <CloseIcon />
         </button>
+        {children}
       </div>
     </div>
   );
