7609a4a56d5221236dadf6683dbf16a1820f97fe
신동근 <tlsehdrms124@naver.com>
2025-09-19 16:27:43 +0900

    [BE] feat: 여행지 정보 및 상세 정보 관련 Info DTO 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/trip/application/info/TripSpotWithDetailViewInfo.java
@@ -0,0 +1,37 @@
+package com.followfollowme.tripmarble.domainlayer.trip.application.info;
+
+import com.followfollowme.tripmarble.domainlayer.trip.domain.model.TripSpot;
+import com.followfollowme.tripmarble.domainlayer.trip.domain.model.TripSpotDetail;
+import lombok.Builder;
+
+@Builder
+public record TripSpotWithDetailViewInfo(
+    long tripSpotId,
+    String tripSpotName,
+    String contentTypeName,
+    String description,
+    String homepageUrl,
+    String phoneNumber,
+    String address,
+    String addressDetail,
+    double longitude,
+    double latitude,
+    String originalImageUrl
+) {
+
+    public static TripSpotWithDetailViewInfo of(TripSpot tripSpot, TripSpotDetail tripSpotDetail, String contentTypeName) {
+        return TripSpotWithDetailViewInfo.builder()
+            .tripSpotId(tripSpot.id())
+            .tripSpotName(tripSpot.title())
+            .contentTypeName(contentTypeName)
+            .description(tripSpotDetail.overview())
+            .homepageUrl(tripSpotDetail.homepage())
+            .phoneNumber(tripSpot.tel())
+            .address(tripSpot.addr1())
+            .addressDetail(tripSpot.addr2())
+            .longitude(tripSpot.mapX())
+            .latitude(tripSpot.mapY())
+            .originalImageUrl(tripSpot.firstImage())
+            .build();
+    }
+}
