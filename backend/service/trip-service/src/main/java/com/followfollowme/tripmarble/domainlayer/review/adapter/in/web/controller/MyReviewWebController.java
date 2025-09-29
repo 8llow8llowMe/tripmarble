6eed1206933513f90608302960bfd03038422855
신동근 <tlsehdrms124@naver.com>
2025-09-29 17:30:44 +0900

    [BE] feat: 나의 리뷰 목록 조회 API 개발

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/review/adapter/in/web/controller/MyReviewWebController.java
@@ -0,0 +1,49 @@
+package com.followfollowme.tripmarble.domainlayer.review.adapter.in.web.controller;
+
+import com.followfollowme.tripmarble.common.dto.Response;
+import com.followfollowme.tripmarble.domainlayer.review.adapter.in.web.dto.TripSpotReviewAndPhotosResponse;
+import com.followfollowme.tripmarble.domainlayer.review.application.port.in.MyReviewWebUseCase;
+import com.followfollowme.tripmarble.persistence.dto.SliceResponse;
+import com.followfollowme.tripmarble.persistence.enums.OrderType;
+import com.followfollowme.tripmarble.security.common.dto.MemberLoginActive;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/reviews")
+@Tag(name = "나의 리뷰", description = "사용자가 작성한 리뷰 관련 클라이언트 전용 API 입니다.")
+public class MyReviewWebController {
+
+    private final MyReviewWebUseCase myReviewWebUseCase;
+
+    @Operation(
+        summary = "나의 리뷰 목록 조회 (무한 스크롤)",
+        description = "로그인 사용자가 작성한 여행지 리뷰 목록을 무한 스크롤 방식으로 조회합니다.",
+        security = {@SecurityRequirement(name = "bearerAuth")}
+    )
+    @ApiResponse(responseCode = "200", description = "나의 리뷰 목록 조회 성공")
+    @GetMapping
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Response<SliceResponse<TripSpotReviewAndPhotosResponse>>> getMyTripSpotReviews(
+        @AuthenticationPrincipal MemberLoginActive loginActive,
+        @RequestParam(required = false, defaultValue = "0") String lastTripSpotReviewId,
+        @RequestParam(required = false, defaultValue = "10") int size,
+        @RequestParam(required = false, defaultValue = "DESC") OrderType orderType
+    ) {
+        SliceResponse<TripSpotReviewAndPhotosResponse> responses =
+            myReviewWebUseCase.getMyTripSpotReviews(loginActive.id(), Long.parseLong(lastTripSpotReviewId), size, orderType);
+        return ResponseEntity.ok().body(Response.success(responses));
+    }
+}
+
