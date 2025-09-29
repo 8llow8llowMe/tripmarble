7bdaa8ab1a65130d1d060cd44e36b221d7c7f587
신동근 <tlsehdrms124@naver.com>
2025-09-29 17:51:57 +0900

    [BE] feat: 리뷰 관련 커스텀 에러코드 추가

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/review/application/exception/ReviewErrorCode.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/review/application/exception/ReviewErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ReviewErrorCode {
 
     TRIP_SPOT_REVIEW_NOT_FOUND("REVIEW_001", "해당 여행지 리뷰를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    UPLOAD_REVIEW_PHOTO_FAILED("REVIEW_002", "여행지 리뷰 사진 업로드에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    UPLOAD_REVIEW_PHOTO_FAILED("REVIEW_002", "여행지 리뷰 사진 업로드에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    REVIEW_FORBIDDEN("REVIEW_003", "본인 리뷰가 아니므로 삭제할 수 없습니다.", HttpStatus.FORBIDDEN);
 
     private final String code;
     private final String errorMessage;
