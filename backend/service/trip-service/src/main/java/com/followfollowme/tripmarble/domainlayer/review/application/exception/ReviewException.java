3c25ccf69c93cff52061d2cd367b639c224a3c54
신동근 <tlsehdrms124@naver.com>
2025-09-29 17:16:29 +0900

    [BE] feat: 리뷰 관련 커스텀 Exception 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/review/application/exception/ReviewException.java
@@ -0,0 +1,14 @@
+package com.followfollowme.tripmarble.domainlayer.review.application.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ReviewException extends RuntimeException {
+
+    private final ReviewErrorCode errorCode;
+
+    public ReviewException(ReviewErrorCode errorCode) {
+        super(errorCode.getErrorMessage());
+        this.errorCode = errorCode;
+    }
+}
