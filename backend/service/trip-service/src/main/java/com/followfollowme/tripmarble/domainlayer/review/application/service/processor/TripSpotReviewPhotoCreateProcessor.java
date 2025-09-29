901e74a03e24c4ecbb6f3468c0e62d64896fba2c
신동근 <tlsehdrms124@naver.com>
2025-09-29 17:20:05 +0900

    [BE] fix: 여행지 리뷰 사진 생성 프로세스 관련 리뷰 관련 커스텀 Exception 처리

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/review/application/service/processor/TripSpotReviewPhotoCreateProcessor.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/review/application/service/processor/TripSpotReviewPhotoCreateProcessor.java
@@ -1,12 +1,12 @@
 package com.followfollowme.tripmarble.domainlayer.review.application.service.processor;
 
+import com.followfollowme.tripmarble.domainlayer.review.application.exception.ReviewErrorCode;
+import com.followfollowme.tripmarble.domainlayer.review.application.exception.ReviewException;
 import com.followfollowme.tripmarble.domainlayer.review.application.info.TripSpotReviewPhotoCreateInfo;
 import com.followfollowme.tripmarble.domainlayer.review.application.port.out.TripSpotReviewPhotoRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.review.application.port.out.TripSpotReviewRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.review.domain.model.TripSpotReview;
 import com.followfollowme.tripmarble.domainlayer.review.domain.model.TripSpotReviewPhoto;
-import com.followfollowme.tripmarble.domainlayer.trip.application.exception.TripErrorCode;
-import com.followfollowme.tripmarble.domainlayer.trip.application.exception.TripException;
 import com.followfollowme.tripmarble.persistence.util.SnowflakeIdGenerator;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -28,7 +28,7 @@ public class TripSpotReviewPhotoCreateProcessor {
 
         // 1. 리뷰 존재 확인
         TripSpotReview review = tripSpotReviewRepositoryPort.findById(tripSpotReviewId)
-            .orElseThrow(() -> new TripException(TripErrorCode.TRIP_SPOT_REVIEW_NOT_FOUND));
+            .orElseThrow(() -> new ReviewException(ReviewErrorCode.TRIP_SPOT_REVIEW_NOT_FOUND));
 
         // 2. 도메인 객체 리스트 생성
         List<TripSpotReviewPhoto> photos = IntStream.range(0, photoUrls.size())
