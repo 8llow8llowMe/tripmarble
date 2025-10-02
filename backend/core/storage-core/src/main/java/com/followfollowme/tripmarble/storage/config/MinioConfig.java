c5c5c50633b601227971465f00a4c3f104de6daf
신동근 <tlsehdrms124@naver.com>
2025-10-02 17:40:27 +0900

    [BE] fix: MinIO 설정 관련 Bean 주입들 파라미터로 의존성 주입하게끔 수정

--- backend/core/storage-core/src/main/java/com/followfollowme/tripmarble/storage/config/MinioConfig.java
+++ backend/core/storage-core/src/main/java/com/followfollowme/tripmarble/storage/config/MinioConfig.java
@@ -3,19 +3,14 @@ package com.followfollowme.tripmarble.storage.config;
 import com.followfollowme.tripmarble.storage.properties.MinioProperties;
 import com.followfollowme.tripmarble.storage.util.MinioFileRemover;
 import io.minio.MinioClient;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class MinioConfig {
 
-    private final MinioClient minioClient;
-    private final MinioProperties minioProperties;
-
     @Bean
-    public MinioClient minioClient() {
+    public MinioClient minioClient(MinioProperties minioProperties) {
         return MinioClient.builder()
             .endpoint(minioProperties.internalUrl())
             .credentials(minioProperties.accessKey(), minioProperties.secretKey())
@@ -23,7 +18,7 @@ public class MinioConfig {
     }
 
     @Bean
-    public MinioFileRemover minioFileRemover() {
+    public MinioFileRemover minioFileRemover(MinioClient minioClient, MinioProperties minioProperties) {
         return new MinioFileRemover(minioClient, minioProperties);
     }
 }
