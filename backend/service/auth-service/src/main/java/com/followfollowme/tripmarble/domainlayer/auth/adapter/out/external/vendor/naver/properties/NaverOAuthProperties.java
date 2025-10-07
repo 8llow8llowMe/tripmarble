ea31357354ecb72990946d5f17cc08d9de3a925b
신동근 <tlsehdrms124@naver.com>
2025-10-07 15:39:07 +0900

    [BE] fix: 네이버 OAUTH 관련 프로퍼티 수정

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/adapter/out/external/vendor/naver/properties/NaverOAuthProperties.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/adapter/out/external/vendor/naver/properties/NaverOAuthProperties.java
@@ -1,4 +1,4 @@
-package com.followfollowme.tripmarble.domainlayer.auth.adapter.out.external.vendor.naver;
+package com.followfollowme.tripmarble.domainlayer.auth.adapter.out.external.vendor.naver.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -7,7 +7,7 @@ public record NaverOAuthProperties(
     String redirectUri,
     String clientId,
     String clientSecret,
-    String state
+    String[] scope // 없어도 되지만 명시
 ) {
 
 }
