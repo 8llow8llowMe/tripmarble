162b9355c96e3cd75b9199bef2ef46737df5b1a5
신동근 <tlsehdrms124@naver.com>
2025-10-07 15:41:26 +0900

    [BE] feat: 소셜 로그인 관련 OAUTH 프로퍼티 설정 클래스에 네이버 소셜 로그인 프로퍼티 클래스 추가

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/OAuthPropertiesConfig.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/global/config/OAuthPropertiesConfig.java
@@ -1,7 +1,7 @@
 package com.followfollowme.tripmarble.global.config;
 
 import com.followfollowme.tripmarble.domainlayer.auth.adapter.out.external.vendor.kakao.properties.KakaoOAuthProperties;
-import com.followfollowme.tripmarble.domainlayer.auth.adapter.out.external.vendor.naver.NaverOAuthProperties;
+import com.followfollowme.tripmarble.domainlayer.auth.adapter.out.external.vendor.naver.properties.NaverOAuthProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
