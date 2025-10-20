47870598b8a1ac7abe7e8c572c5d36893680d740
eestingz <tmdgus1761@gmail.com>
2025-10-20 12:16:55 +0900

    [FE/mobile] feat: add sheet base

--- frontend/mobile/src/hooks/useBottomSheetBase.ts
+++ frontend/mobile/src/hooks/useBottomSheetBase.ts
@@ -1,3 +1,5 @@
+import GameInfoSheet from '@/components/bottomSheet/game/GameInfoSheet';
+import MissionSelectSheet from '@/components/bottomSheet/game/MissionSelectSheet';
 import PrivacyPolicySheet from '@/components/bottomSheet/PrivacyPolicySheet';
 import SelectProfileImageSheet from '@/components/bottomSheet/SelectProfileImageSheet';
 import ServiceTermsSheet from '@/components/bottomSheet/ServiceTermsSheet';
@@ -11,6 +13,8 @@ export const BOTTOM_SHEET_COMPONENTS = {
   ServiceTermsSheet: ServiceTermsSheet, // 이용약관
   PrivacyPolicySheet: PrivacyPolicySheet, // 개인정보 처리방침
   SelectProfileImageSheet: SelectProfileImageSheet, // 프로필 이미지 선택
+  GameInfoSheet: GameInfoSheet, // 게임 보드 여행지 상세 정보
+  MissionSelectSheet: MissionSelectSheet, // 미션인증 방식 선택
 } as const;
 
 type BottomSheetName = keyof typeof BOTTOM_SHEET_COMPONENTS;
