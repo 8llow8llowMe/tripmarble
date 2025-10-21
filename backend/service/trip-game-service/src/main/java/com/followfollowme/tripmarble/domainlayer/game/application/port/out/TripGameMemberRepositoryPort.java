c38381593de452968048817e613f1efbcaaadba8
신동근 <tlsehdrms124@naver.com>
2025-10-21 17:57:19 +0900

    [BE] feat: 해당 회원의 여행 게임 개수 조회 쿼리 기능에 따른 Repository단 Port 및 Adapter 구현

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/out/TripGameMemberRepositoryPort.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/port/out/TripGameMemberRepositoryPort.java
@@ -3,7 +3,6 @@ package com.followfollowme.tripmarble.domainlayer.game.application.port.out;
 import com.followfollowme.tripmarble.domainlayer.game.application.readmodel.TripGameMemberCount;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGame;
 import com.followfollowme.tripmarble.domainlayer.game.domain.model.TripGameMember;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -24,4 +23,6 @@ public interface TripGameMemberRepositoryPort {
     List<TripGameMember> findAllByTripGameIdAndMemberId(List<Long> tripGameIds, long memberId);
 
     Optional<TripGameMember> findByTripGameIdAndMemberId(long tripGameId, long memberId);
+
+    int countTripGameByMemberId(long memberId);
 }
