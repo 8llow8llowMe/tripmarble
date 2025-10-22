a5950f59a0ab27e4c24d98af3a1193bdfad7625f
신동근 <tlsehdrms124@naver.com>
2025-10-22 15:41:54 +0900

    [BE] fix: 네이버 회원 정보 응답 DTO에서 도메인 매핑 코드 삭제

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/adapter/out/external/vendor/naver/dto/NaverMemberResponse.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/auth/adapter/out/external/vendor/naver/dto/NaverMemberResponse.java
@@ -2,9 +2,6 @@ package com.followfollowme.tripmarble.domainlayer.auth.adapter.out.external.vend
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.followfollowme.tripmarble.domainlayer.auth.adapter.out.external.vendor.enums.OAuthProvider;
-import com.followfollowme.tripmarble.domainlayer.member.domain.model.Member;
-import com.followfollowme.tripmarble.security.common.enums.SecurityRole;
 
 @JsonNaming(SnakeCaseStrategy.class)
 public record NaverMemberResponse(
@@ -13,14 +10,4 @@ public record NaverMemberResponse(
     NaverAccount response
 ) {
 
-    public Member toDomain() {
-        return Member.builder()
-            .email(response.email())
-            .name(response.name())
-            .nickname(response.nickname())
-            .profileImageUrl(response.profileImage())
-            .role(SecurityRole.USER)
-            .provider(OAuthProvider.NAVER)
-            .build();
-    }
 }
