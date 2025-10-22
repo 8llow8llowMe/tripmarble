084bae53894650c0388209e726857e5037375a1b
신동근 <tlsehdrms124@naver.com>
2025-10-22 17:31:56 +0900

    [BE] feat: 해당 회원의 여행 게임 개수 조회 기능 개발 (메인 서비스단)

--- backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameInternalFacade.java
+++ backend/service/trip-game-service/src/main/java/com/followfollowme/tripmarble/domainlayer/game/application/service/TripGameInternalFacade.java
@@ -1,5 +1,8 @@
 package com.followfollowme.tripmarble.domainlayer.game.application.service;
 
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.internal.dto.TripGameCountInternalResponse;
+import com.followfollowme.tripmarble.domainlayer.game.adapter.in.internal.presenter.TripGameInternalPresenter;
+import com.followfollowme.tripmarble.domainlayer.game.application.info.TripGameCountInfo;
 import com.followfollowme.tripmarble.domainlayer.game.application.port.in.TripGameInternalUseCase;
 import com.followfollowme.tripmarble.domainlayer.game.application.service.processor.TripGameQueryProcessor;
 import lombok.RequiredArgsConstructor;
@@ -11,10 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class TripGameInternalFacade implements TripGameInternalUseCase {
 
     private final TripGameQueryProcessor tripGameQueryProcessor;
+    private final TripGameInternalPresenter tripGameInternalPresenter;
 
     @Override
     @Transactional(readOnly = true)
-    public int getTripGameCountByMember(long memberId) {
-        return tripGameQueryProcessor.getTripGameCountByMember(memberId);
+    public TripGameCountInternalResponse getTripGameCountByMember(long memberId) {
+        TripGameCountInfo tripGameCountInfo = tripGameQueryProcessor.getTripGameCountByMember(memberId);
+        return tripGameInternalPresenter.toCountResponse(tripGameCountInfo);
     }
 }
