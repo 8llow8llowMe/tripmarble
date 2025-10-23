94c9c3c2c91ac8824c06674c443dce7772353bef
신동근 <tlsehdrms124@naver.com>
2025-10-23 21:16:56 +0900

    [BE] feat: 해당 회원이 작성한 리뷰 개수 및 리뷰 사진 개수 조회 API Feign Client 정의

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/out/feign/client/TripSpotReviewClient.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/out/feign/client/TripSpotReviewClient.java
@@ -1,6 +1,9 @@
 package com.followfollowme.tripmarble.domainlayer.member.adapter.out.feign.client;
 
+import com.followfollowme.tripmarble.domainlayer.member.adapter.out.feign.dto.TripSpotReviewCountInternalResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
     name = "${feign-client.target-services.trip-service:trip-service}",
@@ -9,4 +12,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 )
 public interface TripSpotReviewClient {
 
+    @GetMapping("/members/{memberId}/count")
+    TripSpotReviewCountInternalResponse getMyTripSpotReviewAndPhotoCount(@PathVariable long memberId);
 }
