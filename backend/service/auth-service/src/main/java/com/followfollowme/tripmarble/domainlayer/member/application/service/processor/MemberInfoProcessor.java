aa5331b9d67eabe3e9384598aa48a9c4654b050d
신동근 <tlsehdrms124@naver.com>
2025-10-23 21:20:02 +0900

    [BE] feat: 나의 활동 요약 기능 정의 (서브 서비스단)

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/service/processor/MemberInfoProcessor.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/service/processor/MemberInfoProcessor.java
@@ -1,10 +1,14 @@
 package com.followfollowme.tripmarble.domainlayer.member.application.service.processor;
 
-import com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.dto.MemberMyInfoResponse;
-import com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.presenter.MemberPresenter;
+import com.followfollowme.tripmarble.domainlayer.member.adapter.out.feign.dto.TripGameCountInternalResponse;
+import com.followfollowme.tripmarble.domainlayer.member.adapter.out.feign.dto.TripSpotReviewCountInternalResponse;
 import com.followfollowme.tripmarble.domainlayer.member.application.exception.MemberErrorCode;
 import com.followfollowme.tripmarble.domainlayer.member.application.exception.MemberException;
+import com.followfollowme.tripmarble.domainlayer.member.application.info.MemberActivitySummaryInfo;
+import com.followfollowme.tripmarble.domainlayer.member.application.info.MemberMyInfo;
 import com.followfollowme.tripmarble.domainlayer.member.application.port.out.MemberRepositoryPort;
+import com.followfollowme.tripmarble.domainlayer.member.application.port.out.TripGameClientPort;
+import com.followfollowme.tripmarble.domainlayer.member.application.port.out.TripSpotReviewClientPort;
 import com.followfollowme.tripmarble.domainlayer.member.domain.model.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,12 +18,29 @@ import org.springframework.stereotype.Service;
 public class MemberInfoProcessor {
 
     private final MemberRepositoryPort memberRepositoryPort;
-    private final MemberPresenter memberPresenter;
+    private final TripGameClientPort tripGameClientPort;
+    private final TripSpotReviewClientPort tripSpotReviewClientPort;
 
-    public MemberMyInfoResponse loadMyInfo(long memberId) {
+    public MemberMyInfo loadMyInfo(long memberId) {
         Member member = memberRepositoryPort.findById(memberId)
             .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_MEMBER));
 
-        return memberPresenter.toMyInfoResponse(member);
+        return MemberMyInfo.of(member);
+    }
+
+    public MemberActivitySummaryInfo getMemberActivitySummary(long memberId) {
+        // 1. 해당 회원 존재 검증
+        if (!memberRepositoryPort.existsById(memberId)) {
+            throw new MemberException(MemberErrorCode.NOT_FOUND_MEMBER);
+        }
+
+        // 2. 여행 게임 수 조회
+        TripGameCountInternalResponse gameCount = tripGameClientPort.getTripGameCountByMember(memberId);
+
+        // 3. 여행 리뷰 및 사진 수 조회
+        TripSpotReviewCountInternalResponse reviewCount = tripSpotReviewClientPort.getMyTripSpotReviewAndPhotoCount(memberId);
+
+        // 4. Info 조합
+        return MemberActivitySummaryInfo.of(gameCount, reviewCount);
     }
 }
