ffc7036b19e664b633539612cfde26529ecc92bc
신동근 <tlsehdrms124@naver.com>
2025-10-23 21:19:37 +0900

    [BE] feat: 해당 회원 ID를 통해 존재 여부 확인 기능 정의 (Repository)

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/out/persistence/MemberRepositoryAdapter.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/adapter/out/persistence/MemberRepositoryAdapter.java
@@ -5,10 +5,11 @@ import com.followfollowme.tripmarble.domainlayer.member.adapter.out.persistence.
 import com.followfollowme.tripmarble.domainlayer.member.application.mapper.MemberMapper;
 import com.followfollowme.tripmarble.domainlayer.member.application.port.out.MemberRepositoryPort;
 import com.followfollowme.tripmarble.domainlayer.member.domain.model.Member;
-import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -45,5 +46,10 @@ public class MemberRepositoryAdapter implements MemberRepositoryPort {
     public List<Member> findByIdIn(List<Long> memberIds) {
         List<MemberEntity> entities = memberRepository.findByIdIn(memberIds);
         return memberMapper.toDomainListFromEntityList(entities);
+    }
+
+    @Override
+    public boolean existsById(long memberId) {
+        return memberRepository.existsById(memberId);
     }
 }
