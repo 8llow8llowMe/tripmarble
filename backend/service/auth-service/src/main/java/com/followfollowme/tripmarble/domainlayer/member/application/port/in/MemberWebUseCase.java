772fa1d98a414777f2e63bf8d046d6b604d3027c
신동근 <tlsehdrms124@naver.com>
2025-10-23 21:23:14 +0900

    [BE] feat: 나의 활동 요약 조회 기능 정의 (메인 서비스단)

--- backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/port/in/MemberWebUseCase.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/tripmarble/domainlayer/member/application/port/in/MemberWebUseCase.java
@@ -1,5 +1,6 @@
 package com.followfollowme.tripmarble.domainlayer.member.application.port.in;
 
+import com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.dto.MemberActivitySummaryResponse;
 import com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.dto.MemberMyInfoResponse;
 import com.followfollowme.tripmarble.domainlayer.member.adapter.in.web.dto.MemberProfileUploadResponse;
 import com.followfollowme.tripmarble.domainlayer.member.application.command.MemberSignupCommand;
@@ -17,4 +18,6 @@ public interface MemberWebUseCase {
     void updateMyInfo(MemberUpdateCommand command);
 
     void withdrawMember(long memberId);
+
+    MemberActivitySummaryResponse getMemberActivitySummary(long memberId);
 }
