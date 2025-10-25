895e707050ce5125b5d83fdeeb71070aaffc9ff2
seonghoho <seonghoho@codnut.com>
2025-10-25 09:32:44 +0900

    [FE/web] fix: filter 디자인 변경

---
+++ frontend/web/src/shared/assets/icons/arrowUp.tsx
@@ -0,0 +1,30 @@
+const arrowUp = () => {
+  return (
+    <>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clipPath="url(#clip0_5_784)">
+          <path
+            d="M6 15L12 9L18 15"
+            stroke="#0F1B2A"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_5_784">
+            <rect width="24" height="24" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    </>
+  );
+};
+
+export default arrowUp;
