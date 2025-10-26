82407c219ec3fa02f57ae0e37bfe95fcf1bc88da
신동근 <tlsehdrms124@naver.com>
2025-10-26 15:00:58 +0900

    [BE] feat: 대표 여행지 자동 완성 검색 기능 추가 (메인 서비스단)

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/in/RepresentativeRegionWebUseCase.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/domainlayer/region/application/port/in/RepresentativeRegionWebUseCase.java
@@ -1,7 +1,9 @@
 package com.followfollowme.tripmarble.domainlayer.region.application.port.in;
 
 import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RepresentativeRegionDetailResponse;
+import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RepresentativeRegionSearchResponse;
 import com.followfollowme.tripmarble.domainlayer.region.adapter.in.web.dto.RepresentativeRegionSummaryResponse;
+
 import java.util.List;
 
 public interface RepresentativeRegionWebUseCase {
@@ -9,4 +11,6 @@ public interface RepresentativeRegionWebUseCase {
     List<RepresentativeRegionSummaryResponse> getAllRepresentativeRegions();
 
     RepresentativeRegionDetailResponse getRepresentativeRegionDetail(long representativeId);
+
+    List<RepresentativeRegionSearchResponse> getAutocompleteSuggestions(String keyword);
 }
