c3d9b77ccac311aac9e23a488ed0c6bc8f91d119
신동근 <tlsehdrms124@naver.com>
2025-10-26 14:54:35 +0900

    [BE] feat: trip-service 인프라 설정 클래스에 redis 설정 클래스 추가

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/config/TripServiceInfraConfig.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/config/TripServiceInfraConfig.java
@@ -4,6 +4,7 @@ import com.followfollowme.tripmarble.common.config.JasyptConfig;
 import com.followfollowme.tripmarble.persistence.config.JpaAuditConfig;
 import com.followfollowme.tripmarble.persistence.config.QuerydslConfig;
 import com.followfollowme.tripmarble.persistence.config.SnowflakeConfig;
+import com.followfollowme.tripmarble.redis.config.RedisConfig;
 import com.followfollowme.tripmarble.security.resourceserver.config.ResourceServerSecurityConfig;
 import com.followfollowme.tripmarble.storage.config.MinioConfig;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +17,8 @@ import org.springframework.context.annotation.Import;
     QuerydslConfig.class,
     SnowflakeConfig.class,
     ResourceServerSecurityConfig.class,
-    MinioConfig.class
+    MinioConfig.class,
+    RedisConfig.class
 })
 public class TripServiceInfraConfig {
 
