a2aecad7ef7608a625b06fed320cfd396ea333af
신동근 <tlsehdrms124@naver.com>
2025-10-26 14:54:17 +0900

    [BE] feat: trip-service 프로퍼티 바인딩 설정 클래스에 redis 프로퍼티 바인딩 설정 클래스 추가

--- backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/config/TripServicePropertiesConfig.java
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/config/TripServicePropertiesConfig.java
@@ -3,6 +3,7 @@ package com.followfollowme.tripmarble.global.config;
 import com.followfollowme.tripmarble.common.config.JasyptPropertiesConfig;
 import com.followfollowme.tripmarble.common.config.SwaggerPropertiesConfig;
 import com.followfollowme.tripmarble.persistence.config.SnowflakePropertiesConfig;
+import com.followfollowme.tripmarble.redis.config.RedisPropertiesConfig;
 import com.followfollowme.tripmarble.security.resourceserver.config.JwtResourceServerPropertiesConfig;
 import com.followfollowme.tripmarble.storage.config.MinioPropertiesConfig;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +15,8 @@ import org.springframework.context.annotation.Import;
     SwaggerPropertiesConfig.class,
     SnowflakePropertiesConfig.class,
     JwtResourceServerPropertiesConfig.class,
-    MinioPropertiesConfig.class
+    MinioPropertiesConfig.class,
+    RedisPropertiesConfig.class
 })
 public class TripServicePropertiesConfig {
 
