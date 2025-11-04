000005b5212e6ae215ca590067c0fd2c478a08c1
신동근 <tlsehdrms124@naver.com>
2025-11-04 15:44:22 +0900

    [BE] fix: Redis 프로퍼티에 redis sentinel 관련 설정 필드 추가

--- backend/core/redis-core/src/main/java/com/followfollowme/tripmarble/redis/properties/RedisProperties.java
+++ backend/core/redis-core/src/main/java/com/followfollowme/tripmarble/redis/properties/RedisProperties.java
@@ -1,11 +1,23 @@
 package com.followfollowme.tripmarble.redis.properties;
 
+import com.followfollowme.tripmarble.redis.properties.enums.RedisMode;
+import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "spring.data.redis")
 public record RedisProperties(
+    RedisMode mode,
     String host,
-    int port
+    Integer port,
+    String masterName,
+    String password,
+    List<SentinelNode> sentinels
 ) {
 
+    public record SentinelNode(
+        String host,
+        int port
+    ) {
+
+    }
 }
