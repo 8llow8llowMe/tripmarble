c9623d5d912a61f9b805ea57a34c5c0337b25cdd
신동근 <tlsehdrms124@naver.com>
2025-11-13 14:33:33 +0900

    [BE] fix: 커스텀 에러코드 에러 메시지 필드명 일관성 있게 통일 및 그에 따른 Exception 수정 작업

--- backend/cloud/api-gateway/src/main/java/com/followfollowme/tripmarble/apigateway/jwt/exception/JwtErrorCode.java
+++ backend/cloud/api-gateway/src/main/java/com/followfollowme/tripmarble/apigateway/jwt/exception/JwtErrorCode.java
@@ -7,11 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum JwtErrorCode {
-    
-    TOKEN_EXPIRED("JWT001", "토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
-    TOKEN_INVALID("JWT002", "토큰이 유효하지 않습니다.", HttpStatus.UNAUTHORIZED),
-    TOKEN_SIGNATURE_INVALID("JWT003", "토큰의 서명 검증에 실패하였습니다.", HttpStatus.UNAUTHORIZED),
-    TOKEN_MALFORMED("JWT004", "토큰 형식이 올바르지 않습니다.", HttpStatus.UNAUTHORIZED);
+
+    TOKEN_EXPIRED("JWT_001", "토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
+    TOKEN_INVALID("JWT_002", "토큰이 유효하지 않습니다.", HttpStatus.UNAUTHORIZED),
+    TOKEN_SIGNATURE_INVALID("JWT_003", "토큰의 서명 검증에 실패하였습니다.", HttpStatus.UNAUTHORIZED),
+    TOKEN_MALFORMED("JWT_004", "토큰 형식이 올바르지 않습니다.", HttpStatus.UNAUTHORIZED);
 
     private final String code;
     private final String message;
