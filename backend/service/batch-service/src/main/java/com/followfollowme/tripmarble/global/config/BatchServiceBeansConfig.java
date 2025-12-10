f9637120eb2bac202215e5e176f6564c40ba211f
신동근 <tlsehdrms124@naver.com>
2025-12-10 15:51:33 +0900

    [BE] fix: Batch Service 관련 Beans 관리 설정 클래스 수정

--- backend/service/batch-service/src/main/java/com/followfollowme/tripmarble/global/config/BatchServiceBeansConfig.java
+++ backend/service/batch-service/src/main/java/com/followfollowme/tripmarble/global/config/BatchServiceBeansConfig.java
@@ -1,12 +1,13 @@
 package com.followfollowme.tripmarble.global.config;
 
-import com.followfollowme.tripmarble.common.config.JasyptConfig;
+import com.followfollowme.tripmarble.common.config.JasyptConfigurer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-    JasyptConfig.class
+    JasyptConfigurer.class
 })
-public class BatchServiceInfraConfig {
+public class BatchServiceBeansConfig {
+
 }
