969f1dc275e568053467f9e1ea49b39f579573b9
신동근 <tlsehdrms124@naver.com>
2025-12-10 15:48:46 +0900

    [BE] feat: trip-service 전용 기능 활성화 설정 클래스 정의

---
+++ backend/service/trip-service/src/main/java/com/followfollowme/tripmarble/global/config/TripServiceFeaturesConfig.java
@@ -0,0 +1,13 @@
+package com.followfollowme.tripmarble.global.config;
+
+import com.followfollowme.tripmarble.persistence.config.JpaAuditConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({
+    JpaAuditConfig.class
+})
+public class TripServiceFeaturesConfig {
+
+}
