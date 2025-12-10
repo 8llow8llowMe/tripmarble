9687c7c37c80041a4d38b78f97e02c0669cb45d8
신동근 <tlsehdrms124@naver.com>
2025-12-10 15:51:00 +0900

    [BE] fix: api-gateway 전용 Bean 주입 설정 클래스 수정

--- backend/cloud/api-gateway/src/main/java/com/followfollowme/tripmarble/apigateway/config/ApiGatewayBeansConfig.java
+++ backend/cloud/api-gateway/src/main/java/com/followfollowme/tripmarble/apigateway/config/ApiGatewayBeansConfig.java
@@ -1,11 +1,13 @@
 package com.followfollowme.tripmarble.apigateway.config;
 
-import com.followfollowme.tripmarble.common.config.JasyptConfig;
+import com.followfollowme.tripmarble.common.config.JasyptConfigurer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(JasyptConfig.class)
-public class ApiGatewayInfraConfig {
+@Import({
+    JasyptConfigurer.class
+})
+public class ApiGatewayBeansConfig {
 
 }
