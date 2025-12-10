a5f6ae077caf742ff7a2d195fcc454637aafa51a
신동근 <tlsehdrms124@naver.com>
2025-12-10 15:42:48 +0900

    [BE] fix: Redis 설정 클래스 명칭 변경

--- backend/core/redis-core/src/main/java/com/followfollowme/tripmarble/redis/config/RedisConfigurer.java
+++ backend/core/redis-core/src/main/java/com/followfollowme/tripmarble/redis/config/RedisConfigurer.java
@@ -3,7 +3,6 @@ package com.followfollowme.tripmarble.redis.config;
 import com.followfollowme.tripmarble.redis.properties.RedisProperties;
 import com.followfollowme.tripmarble.redis.properties.enums.RedisMode;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
@@ -13,8 +12,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Configuration
-public class RedisConfig {
+public class RedisConfigurer {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
