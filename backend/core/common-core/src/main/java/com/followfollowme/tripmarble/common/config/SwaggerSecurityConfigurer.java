81e70ce87e3b1f5001734154679cb41cdd86fe3e
신동근 <tlsehdrms124@naver.com>
2025-12-10 15:44:03 +0900

    [BE] fix: Swagger Security 설정 클래스명 변경

--- backend/core/common-core/src/main/java/com/followfollowme/tripmarble/common/config/SwaggerSecurityConfigurer.java
+++ backend/core/common-core/src/main/java/com/followfollowme/tripmarble/common/config/SwaggerSecurityConfigurer.java
@@ -1,10 +1,10 @@
-package com.followfollowme.tripmarble.common.support;
+package com.followfollowme.tripmarble.common.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 
-public class SwaggerSecurityCommon {
+public class SwaggerSecurityConfigurer {
 
     @Bean
     public Components swaggerComponents() {
